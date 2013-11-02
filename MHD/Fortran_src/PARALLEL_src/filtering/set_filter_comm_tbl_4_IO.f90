5a24ae0d5aa7dfab006e1d9c0cf8e43dc8596e69
Hiroaki Matsui <h_kemono@mac.com>
2013-11-02 09:55:25 -0700

    Replace make command to valuable $(GMAKE)

--- MHD/Fortran_src/PARALLEL_src/filtering/set_filter_comm_tbl_4_IO.f90
+++ MHD/Fortran_src/PARALLEL_src/filtering/set_filter_comm_tbl_4_IO.f90
@@ -27,8 +27,6 @@
 !
       subroutine copy_filter_comm_tbl_from_IO
 !
-      integer(kind = kint) :: i
-!
 !
       num_neib_filter = num_neib_domain_IO
 !
@@ -84,8 +82,6 @@
 !-----------------------------------------------------------------------
 !
       subroutine copy_filter_import_from_IO
-!
-      integer(kind = kint) :: i
 !
 !
       num_neib_filter = num_neib_domain_IO
