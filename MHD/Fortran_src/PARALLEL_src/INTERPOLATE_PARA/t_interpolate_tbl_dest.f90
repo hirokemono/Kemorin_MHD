a4d26e3bbb4823361371e32a97a66a0dd633a93a
Hiroaki Matsui <h_kemono@mac.com>
2013-11-12 11:59:58 -0800

    Change location of interpolate table structures

---
+++ MHD/Fortran_src/PARALLEL_src/INTERPOLATE_PARA/t_interpolate_tbl_dest.f90
@@ -0,0 +1,120 @@
+!>@file   t_interpolate_tbl_dest.f90
+!!@brief  module t_interpolate_tbl_dest
+!!
+!!@author H. Matsui
+!!@date  Programmed by H. Matsui in Nov., 2008
+!
+!> @brief Structure of interpolation table for target mesh
+!!
+!!@verbatim
+!!      subroutine alloc_type_itp_num_dest(tbl_dest)
+!!      subroutine alloc_type_itp_table_dest(tbl_dest)
+!!      subroutine alloc_type_zero_itp_tbl_dest(tbl_dest)
+!!
+!!      subroutine dealloc_type_itp_num_dest(tbl_dest)
+!!      subroutine dealloc_type_itp_table_dest(tbl_dest)
+!!@endverbatim
+!
+      module t_interpolate_tbl_dest
+!
+      use m_precision
+!
+      implicit none
+!
+!
+!> Structure of interpolation table for target grid
+      type interpolate_table_dest
+!
+!>   number of subdomain to receive interpolated data
+        integer(kind = kint) :: num_org_domain
+!>   flag if original nodes have same prosess
+        integer(kind = kint) :: iflag_self_itp_recv
+!>   subdomain rank to receive interpolated data
+        integer(kind = kint), pointer :: id_org_domain(:)
+!>   end address to receive interpolated data
+        integer(kind = kint), pointer :: istack_nod_tbl_dest(:)
+!
+!>   total number of interpolated node in target subdomain
+        integer(kind = kint) :: ntot_table_dest
+!>   local node ID to set interpolated data (import)
+        integer(kind = kint), pointer :: inod_dest_4_dest(:)
+!>   Reverse ID to set interpolated data (import)
+        integer(kind = kint), pointer :: irev_dest_4_dest(:)
+      end type interpolate_table_dest
+!
+!-----------------------------------------------------------------------
+!
+      contains
+!
+!-----------------------------------------------------------------------
+!
+      subroutine alloc_type_itp_num_dest(tbl_dest)
+!
+      type(interpolate_table_dest), intent(inout) :: tbl_dest
+!
+!
+      allocate(tbl_dest%id_org_domain(tbl_dest%num_org_domain))
+      allocate(tbl_dest%istack_nod_tbl_dest(0:tbl_dest%num_org_domain))
+!
+      if (tbl_dest%num_org_domain .gt. 0) tbl_dest%id_org_domain = 0
+      tbl_dest%istack_nod_tbl_dest =       -1
+!
+      end subroutine alloc_type_itp_num_dest
+!
+!-----------------------------------------------------------------------
+!
+      subroutine alloc_type_itp_table_dest(tbl_dest)
+!
+      type(interpolate_table_dest), intent(inout) :: tbl_dest
+!
+!
+      allocate( tbl_dest%inod_dest_4_dest(tbl_dest%ntot_table_dest)  )
+      allocate( tbl_dest%irev_dest_4_dest(tbl_dest%ntot_table_dest) )
+      if (tbl_dest%ntot_table_dest .gt. 0) then
+        tbl_dest%inod_dest_4_dest = 0
+        tbl_dest%irev_dest_4_dest = 0
+      end if
+!
+      end subroutine alloc_type_itp_table_dest
+!
+!-----------------------------------------------------------------------
+!
+      subroutine alloc_type_zero_itp_tbl_dest(tbl_dest)
+!
+      type(interpolate_table_dest), intent(inout) :: tbl_dest
+!
+!
+      tbl_dest%num_org_domain =  0
+      tbl_dest%ntot_table_dest = 0
+      call alloc_type_itp_num_dest(tbl_dest)
+      call alloc_type_itp_table_dest(tbl_dest)
+!
+      end subroutine alloc_type_zero_itp_tbl_dest
+!
+!------------------------------------------------------------------
+!-----------------------------------------------------------------------
+!
+      subroutine dealloc_type_itp_num_dest(tbl_dest)
+!
+      type(interpolate_table_dest), intent(inout) :: tbl_dest
+!
+!
+      deallocate( tbl_dest%id_org_domain )
+      deallocate( tbl_dest%istack_nod_tbl_dest)
+!
+      end subroutine dealloc_type_itp_num_dest
+!
+!-----------------------------------------------------------------------
+!
+      subroutine dealloc_type_itp_table_dest(tbl_dest)
+!
+      type(interpolate_table_dest), intent(inout) :: tbl_dest
+!
+!
+      deallocate(tbl_dest%inod_dest_4_dest, tbl_dest%irev_dest_4_dest)
+!
+      end subroutine dealloc_type_itp_table_dest
+!
+!-----------------------------------------------------------------------
+!
+      end module t_interpolate_tbl_dest
---
+++ MHD/Fortran_src/PARALLEL_src/INTERPOLATE_PARA/t_interpolate_tbl_org.f90
@@ -0,0 +1,184 @@
+!>@file   t_interpolate_tbl_org.f90
+!!@brief  module t_interpolate_tbl_org
+!!
+!!@author H. Matsui
+!!@date  Programmed by H. Matsui in Nov., 2008
+!
+!> @brief Structure of interpolation table for source mesh
+!!
+!!@verbatim
+!!      subroutine alloc_type_itp_num_org(tbl_org)
+!!      subroutine alloc_type_itp_table_org(tbl_org)
+!!      subroutine alloc_type_zero_itp_tbl_org(np_smp, tbl_org)
+!!
+!!      subroutine dealloc_type_itp_num_org(tbl_org)
+!!      subroutine dealloc_type_itp_table_org(tbl_org)
+!!
+!!      subroutine set_stack_tbl_org_smp_type(tbl_org)
+!!@endverbatim
+!
+      module t_interpolate_tbl_org
+!
+      use m_precision
+!
+      implicit none
+!
+!
+!> Structure of interpolation table for source grid
+      type interpolate_table_org
+!
+!>   number of subdomain to send interpolated data
+        integer(kind = kint) :: num_dest_domain
+!>   flag if target nodes have same prosess
+        integer(kind = kint) :: iflag_self_itp_send
+!>   subdomain rank to send interpolated data
+        integer(kind = kint), pointer :: id_dest_domain(:)
+!>   end address to send interpolated data
+        integer(kind = kint), pointer :: istack_nod_tbl_org(:)
+!>   end address for interplation modes
+        integer(kind = kint), pointer :: istack_itp_type_org(:)
+!
+!>   total number of node to interpolate in original subdomain
+        integer(kind = kint) :: ntot_table_org
+!>   export table for interpolation
+        integer(kind = kint), pointer :: inod_itp_send(:)
+!>   global node ID for target domain
+        integer(kind = kint), pointer :: inod_gl_dest_4_org(:)
+!>   local element ID to make interpolation
+        integer(kind = kint), pointer :: iele_org_4_org(:)
+!>   interpolation type ID
+        integer(kind = kint), pointer :: itype_inter_org(:)
+!>   Coordinate of target node in element coordinate
+        real(kind = kreal), pointer :: coef_inter_org(:,:)
+!
+!>   end address of table to interpolation at original elements
+        integer(kind = kint), pointer :: istack_tbl_type_org_smp(:)
+!>   maximum number of interpolation at original elements
+        integer(kind = kint) :: imax_tbl_wtype_org_smp
+!
+      end type interpolate_table_org
+!
+!-----------------------------------------------------------------------
+!
+      contains
+!
+!-----------------------------------------------------------------------
+!
+      subroutine alloc_type_itp_num_org(np_smp, tbl_org)
+!
+      integer(kind = kint), intent(in) :: np_smp
+      type(interpolate_table_org), intent(inout) :: tbl_org
+      integer(kind = kint) :: num
+!
+      allocate( tbl_org%id_dest_domain(tbl_org%num_dest_domain) )
+      allocate( tbl_org%istack_nod_tbl_org(0:tbl_org%num_dest_domain) )
+!
+      num = 4*tbl_org%num_dest_domain
+      allocate( tbl_org%istack_itp_type_org(0:4) )
+!
+      allocate(tbl_org%istack_tbl_type_org_smp(0:4*np_smp))
+!
+      if (tbl_org%num_dest_domain .gt. 0) tbl_org%id_dest_domain = 0
+      tbl_org%istack_nod_tbl_org =      0
+      tbl_org%istack_itp_type_org =     0
+      tbl_org%istack_tbl_type_org_smp = 0
+!
+      end subroutine alloc_type_itp_num_org
+!
+!-----------------------------------------------------------------------
+!
+      subroutine alloc_type_itp_table_org(tbl_org)
+!
+      type(interpolate_table_org), intent(inout) :: tbl_org
+!
+!
+      allocate( tbl_org%inod_itp_send(tbl_org%ntot_table_org) )
+      allocate( tbl_org%inod_gl_dest_4_org(tbl_org%ntot_table_org) )
+      allocate( tbl_org%iele_org_4_org(tbl_org%ntot_table_org) )
+      allocate( tbl_org%itype_inter_org(tbl_org%ntot_table_org) )
+      allocate( tbl_org%coef_inter_org(tbl_org%ntot_table_org,3) )
+!
+      if (tbl_org%ntot_table_org .gt. 0) then
+        tbl_org%inod_itp_send =      0
+        tbl_org%inod_gl_dest_4_org = 0
+        tbl_org%iele_org_4_org =     0
+        tbl_org%itype_inter_org =   -1
+        tbl_org%coef_inter_org = 0.0d0
+      end if
+!
+      end subroutine alloc_type_itp_table_org
+!
+!-----------------------------------------------------------------------
+!
+      subroutine alloc_type_zero_itp_tbl_org(np_smp, tbl_org)
+!
+      use m_constants
+!
+      integer(kind = kint), intent(in) :: np_smp
+      type(interpolate_table_org), intent(inout) :: tbl_org
+!
+!
+      tbl_org%num_dest_domain = 0
+      tbl_org%ntot_table_org =  0
+      call alloc_type_itp_num_org(np_smp, tbl_org)
+      call alloc_type_itp_table_org(tbl_org)
+!
+      end subroutine alloc_type_zero_itp_tbl_org
+!
+!------------------------------------------------------------------
+!-----------------------------------------------------------------------
+!
+      subroutine dealloc_type_itp_num_org(tbl_org)
+!
+      type(interpolate_table_org), intent(inout) :: tbl_org
+!
+      deallocate( tbl_org%id_dest_domain )
+      deallocate( tbl_org%istack_nod_tbl_org )
+      deallocate( tbl_org%istack_itp_type_org )
+      deallocate( tbl_org%istack_tbl_type_org_smp )
+!
+      end subroutine dealloc_type_itp_num_org
+!
+!-----------------------------------------------------------------------
+!
+      subroutine dealloc_type_itp_table_org(tbl_org)
+!
+      type(interpolate_table_org), intent(inout) :: tbl_org
+!
+      deallocate( tbl_org%inod_itp_send )
+      deallocate( tbl_org%inod_gl_dest_4_org )
+      deallocate( tbl_org%iele_org_4_org )
+      deallocate( tbl_org%itype_inter_org )
+      deallocate( tbl_org%coef_inter_org )
+!
+      end subroutine dealloc_type_itp_table_org
+!
+!-----------------------------------------------------------------------
+!-----------------------------------------------------------------------
+!
+      subroutine set_stack_tbl_org_smp_type(tbl_org)
+!
+      use m_machine_parameter
+      use cal_minmax_and_stacks
+!
+      type(interpolate_table_org), intent(inout) :: tbl_org
+!
+      integer(kind = kint) :: itype, ist, ied
+      integer(kind = kint) :: ist_smp, ied_smp
+!
+!
+      do itype = 1, 4
+        ist = tbl_org%istack_itp_type_org(itype-1) + 1
+        ied = tbl_org%istack_itp_type_org(itype  )
+        ist_smp = np_smp * (itype-1)
+        ied_smp = np_smp *  itype
+        call count_number_4_smp( np_smp, ist, ied,                      &
+     &        tbl_org%istack_tbl_type_org_smp(ist_smp:ied_smp),         &
+     &        tbl_org%imax_tbl_wtype_org_smp)
+      end do
+!
+      end subroutine set_stack_tbl_org_smp_type
+!
+!-----------------------------------------------------------------------
+!
+      end module t_interpolate_tbl_org
