25adad9e7a0d17d59d5ca57f4994137c05b12b42
Hiroaki Matsui <h_kemono@mac.com>
2014-12-21 16:32:30 -0800

    Use structures for FFT wrappers

--- MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/m_sph_multi_FFTW.F90
+++ MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/m_sph_multi_FFTW.F90
@@ -66,50 +66,14 @@
       use m_precision
       use m_constants
       use m_machine_parameter
+      use t_sph_field_FFTW
 !
       implicit none
 !
-!>      plan ID for fftw
-      integer, parameter :: fftw_plan =    8
-!>      data size of complex for FFTW3
-      integer, parameter :: fftw_complex = 8
-!>      Unit imaginary number
-      complex(kind = fftw_complex), parameter :: iu = (0.0d0,1.0d0)
-!>      estimation flag for FFTW
-      integer(kind = 4), parameter :: FFTW_ESTIMATE = 64
-!>      Meajor flag for FFTW
-      integer(kind = 4), parameter :: FFTW_MEASURE = 0
+!>      Structure to use FFTW
+      type(work_for_sgl_FFTW), save :: sph_fld_FFTW
 !
-!
-!>      plan ID for multi backward transform
-      integer(kind = fftw_plan), allocatable :: plan_back_mul(:)
-!>      plan ID for multi forward transform
-      integer(kind = fftw_plan), allocatable :: plan_fowd_mul(:)
-!
-!
-!>      real data for multiple Fourier transform
-      real(kind = kreal), allocatable :: X_FFTW_mul(:,:)
-!>      spectrum data for multiple Fourier transform
-      complex(kind = fftw_complex), allocatable :: C_FFTW_mul(:,:)
-!>      flag for number of components for Fourier transform
-      integer(kind = kint) :: iflag_fft_mul_len =  -1
-!>      normalization parameter for FFTW (= 1 / Nfft)
-      real(kind = kreal) :: aNfft
-!
-!
-      real(kind = kreal) :: elapsed_fftw(3) = (/0.0,0.0,0.0/)
-!
-      integer, parameter :: IONE_4 = 1
-      integer, parameter :: inembed = 0
-      integer, parameter :: istride = 1
-!
-      private :: iu
-      private :: FFTW_ESTIMATE
-      private :: iflag_fft_mul_len
-      private :: plan_back_mul, plan_fowd_mul
-      private :: X_FFTW_mul, C_FFTW_mul, aNfft
-!
-      private :: allocate_mul_FFTW_plan, deallocate_mul_FFTW_plan
+      private :: sph_fld_FFTW
 !
 ! ------------------------------------------------------------------
 !
@@ -122,46 +86,9 @@
       use m_spheric_parameter
       use m_spheric_param_smp
 !
-      integer(kind = kint) :: ip, ist
-      integer(kind = 4) :: Nfft4, howmany, idist_r, idist_c
 !
-!
-      Nfft4 = int(nidx_rtp(3))
-      call allocate_mul_FFTW_plan                                       &
-     &   (irt_rtp_smp_stack(np_smp), nidx_rtp(3))
-!
-      do ip = 1, np_smp
-        ist = irt_rtp_smp_stack(ip-1) + 1
-        howmany = int(irt_rtp_smp_stack(ip  )                           &
-     &           - irt_rtp_smp_stack(ip-1))
-        idist_r = int(nidx_rtp(3))
-        idist_c = int(nidx_rtp(3)/2+1)
-!
-#ifdef FFTW3_C
-        call kemo_fftw_plan_many_dft_r2c                                &
-     &     (plan_fowd_mul(ip), IONE_4, Nfft4, howmany,                  &
-     &      X_FFTW_mul(1,ist), inembed, istride, idist_r,               &
-     &      C_FFTW_mul(1,ist), inembed, istride, idist_c,               &
-     &      FFTW_ESTIMATE)
-        call kemo_fftw_plan_many_dft_c2r                                &
-     &     (plan_back_mul(ip), IONE_4, Nfft4, howmany,                  &
-     &      C_FFTW_mul(1,ist), inembed, istride, idist_c,               &
-     &      X_FFTW_mul(1,ist), inembed, istride, idist_r,               &
-     &      FFTW_ESTIMATE)
-#else
-        call dfftw_plan_many_dft_r2c                                    &
-     &     (plan_fowd_mul(ip), IONE_4, Nfft4, howmany,                  &
-     &      X_FFTW_mul(1,ist), inembed, istride, idist_r,               &
-     &      C_FFTW_mul(1,ist), inembed, istride, idist_c,               &
-     &      FFTW_ESTIMATE)
-        call dfftw_plan_many_dft_c2r                                    &
-     &     (plan_back_mul(ip), IONE_4, Nfft4, howmany,                  &
-     &      C_FFTW_mul(1,ist), inembed, istride, idist_c,               &
-     &      X_FFTW_mul(1,ist), inembed, istride, idist_r,               &
-     &      FFTW_ESTIMATE)
-#endif
-      end do
-      aNfft = one / dble(nidx_rtp(3))
+      call init_sph_multi_FFTW_t(nidx_rtp, irt_rtp_smp_stack,           &
+     &    sph_fld_FFTW)
 !
       end subroutine init_sph_multi_FFTW
 !
@@ -169,24 +96,8 @@
 !
       subroutine finalize_sph_multi_FFTW
 !
-      integer(kind = kint) :: j
 !
-!
-#ifdef FFTW3_C
-      do j = 1, np_smp
-        call kemo_fftw_destroy_plan(plan_fowd_mul(j))
-        call kemo_fftw_destroy_plan(plan_back_mul(j))
-        call kemo_fftw_cleanup
-      end do
-#else
-      do j = 1, np_smp
-        call dfftw_destroy_plan(plan_fowd_mul(j))
-        call dfftw_destroy_plan(plan_back_mul(j))
-        call dfftw_cleanup
-      end do
-#endif
-!
-      call deallocate_mul_FFTW_plan
+      call finalize_sph_multi_FFTW_t(sph_fld_FFTW)
 !
       end subroutine finalize_sph_multi_FFTW
 !
@@ -195,17 +106,11 @@
       subroutine verify_sph_multi_FFTW
 !
       use m_spheric_parameter
+      use m_spheric_param_smp
 !
 !
-      if( iflag_fft_mul_len .lt. 0) then
-        call init_sph_multi_FFTW
-        return
-      end if
-!
-      if( iflag_fft_mul_len .ne. nnod_rtp) then
-        call finalize_sph_multi_FFTW
-        call init_sph_multi_FFTW
-      end if
+      call verify_sph_multi_FFTW_t(nnod_rtp, nidx_rtp,                  &
+     &    irt_rtp_smp_stack, sph_fld_FFTW)
 !
       end subroutine verify_sph_multi_FFTW
 !
@@ -226,56 +131,9 @@
       integer(kind = kint), intent(in) :: irev_sr_rtp(nnod_rtp)
       real (kind=kreal), intent(inout):: WS(n_WS)
 !
-      integer(kind = kint) ::  m, j, ip, ist, ied, nd
-      integer(kind = kint) :: ic_rtp, is_rtp, ic_send, is_send
-      real :: dummy(3), rtmp(3)
-!
-!
-!      call cpu_time(dummy(1))
-!$omp parallel do schedule(static)                                      &
-!$omp&         private(nd,m,j,ip,ist,ied,ic_rtp,is_rtp,ic_send,is_send)
-      do ip = 1, np_smp
-        ist = irt_rtp_smp_stack(ip-1) + 1
-        ied = irt_rtp_smp_stack(ip)
-        do nd = 1, ncomp
-          do j = ist, ied
-            X_FFTW_mul(1:nidx_rtp(3),j) = X_rtp(j,1:nidx_rtp(3),nd)
-          end do
-!        call cpu_time(rtmp(1))
-!
-!        call cpu_time(dummy(2))
-#ifdef FFTW3_C
-          call kemo_fftw_execute_dft_r2c(plan_fowd_mul(ip),             &
-     &        X_FFTW_mul(1,ist), C_FFTW_mul(1,ist))
-#else
-          call dfftw_execute_dft_r2c(plan_fowd_mul(ip),                 &
-     &        X_FFTW_mul(1,ist), C_FFTW_mul(1,ist))
-#endif
-!
-!      call cpu_time(rtmp(2))
-!   normalization
-!      call cpu_time(dummy(3))
-          do j = ist, ied
-            ic_send = nd + (irev_sr_rtp(j) - 1) * ncomp
-            WS(ic_send) = aNfft * real(C_FFTW_mul(1,j))
-            do m = 2, (nidx_rtp(3)+1)/2
-              ic_rtp = j + (2*m-2) * irt_rtp_smp_stack(np_smp)
-              is_rtp = j + (2*m-1) * irt_rtp_smp_stack(np_smp)
-              ic_send = nd + (irev_sr_rtp(ic_rtp) - 1) * ncomp
-              is_send = nd + (irev_sr_rtp(is_rtp) - 1) * ncomp
-              WS(ic_send) = two * aNfft * real(C_FFTW_mul(m,j))
-              WS(is_send) = two * aNfft * real(C_FFTW_mul(m,j)*iu)
-            end do 
-            m = (nidx_rtp(3)+1)/2 + 1
-            ic_rtp = j + irt_rtp_smp_stack(np_smp)
-            ic_send = nd + (irev_sr_rtp(ic_rtp) - 1) * ncomp
-            WS(ic_send) = two * aNfft * real(C_FFTW_mul(m,j))
-          end do
-!      call cpu_time(rtmp(3))
-        end do
-      end do
-!$omp end parallel do
-!      elapsed_fftw(1:3) = elapsed_fftw(1:3) + rtmp(1:3) - dummy(1:3)
+      call sph_fld_fwd_FFTW_to_send_t(ncomp, nnod_rtp, nidx_rtp,        &
+     &    irt_rtp_smp_stack, n_WS, irev_sr_rtp, X_rtp, WS,              &
+     &    sph_fld_FFTW)
 !
       end subroutine sph_multi_fwd_FFTW_to_send
 !
@@ -295,89 +153,11 @@
       real(kind = kreal), intent(inout)                                 &
      &          :: X_rtp(irt_rtp_smp_stack(np_smp),nidx_rtp(3),ncomp)
 !
-      integer(kind = kint) :: m, j, ip, ist, ied, nd
-      integer(kind = kint) :: ic_rtp, is_rtp, ic_recv, is_recv
-      real :: dummy(3), rtmp(3)
-!
-!
-!      call cpu_time(dummy(3))
-!$omp parallel do schedule(static)                                      &
-!$omp&         private(nd,m,j,ip,ist,ied,ic_rtp,is_rtp,ic_recv,is_recv)
-      do ip = 1, np_smp
-        ist = irt_rtp_smp_stack(ip-1) + 1
-        ied = irt_rtp_smp_stack(ip)
-        do nd = 1, ncomp
-!   normalization
-          do j = ist, ied
-            ic_recv = nd + (irev_sr_rtp(j) - 1) * ncomp
-            C_FFTW_mul(1,j) = cmplx(WR(ic_recv), zero, kind(0d0))
-            do m = 2, (nidx_rtp(3)+1)/2
-              ic_rtp = j + (2*m-2) * irt_rtp_smp_stack(np_smp)
-              is_rtp = j + (2*m-1) * irt_rtp_smp_stack(np_smp)
-              ic_recv = nd + (irev_sr_rtp(ic_rtp) - 1) * ncomp
-              is_recv = nd + (irev_sr_rtp(is_rtp) - 1) * ncomp
-              C_FFTW_mul(m,j)                                           &
-     &            = half * cmplx(WR(ic_recv), -WR(is_recv),kind(0d0))
-            end do
-            m = (nidx_rtp(3)+1)/2 + 1
-            ic_rtp = j + irt_rtp_smp_stack(np_smp)
-            ic_recv = nd + (irev_sr_rtp(ic_rtp) - 1) * ncomp
-            C_FFTW_mul(m,j) = half * cmplx(WR(ic_recv), zero, kind(0d0))
-          end do
-!        call cpu_time(rtmp(3))
-!
-!        call cpu_time(dummy(2))
-#ifdef FFTW3_C
-          call kemo_fftw_execute_dft_c2r(plan_back_mul(ip),             &
-     &        C_FFTW_mul(1,ist), X_FFTW_mul(1,ist))
-#else
-          call dfftw_execute_dft_c2r(plan_back_mul(ip),                 &
-     &        C_FFTW_mul(1,ist), X_FFTW_mul(1,ist))
-#endif
-!        call cpu_time(rtmp(2))
-!
-!        call cpu_time(dummy(1))
-          do j = ist, ied
-            X_rtp(j,1:nidx_rtp(3),nd) = X_FFTW_mul(1:nidx_rtp(3),j)
-          end do
-        end do
-!      call cpu_time(rtmp(1))
-      end do
-!$omp end parallel do
-!
-!      elapsed_fftw(1:3) = elapsed_fftw(1:3) + rtmp(1:3) - dummy(1:3)
+      call sph_fld_back_FFTW_from_recv_t(ncomp, nnod_rtp, nidx_rtp,     &
+     &    irt_rtp_smp_stack, n_WR, irev_sr_rtp, WR, X_rtp,              &
+     &    sph_fld_FFTW)
 !
       end subroutine sph_multi_back_FFTW_from_recv
-!
-! ------------------------------------------------------------------
-! ------------------------------------------------------------------
-!
-      subroutine allocate_mul_FFTW_plan(nnod_rt, Nfft)
-!
-      integer(kind = kint), intent(in) :: nnod_rt, Nfft
-!
-!
-      allocate(plan_back_mul(np_smp))
-      allocate(plan_fowd_mul(np_smp))
-!
-      iflag_fft_mul_len = nnod_rt*Nfft
-      allocate(X_FFTW_mul(Nfft,nnod_rt))
-      allocate(C_FFTW_mul(Nfft/2+1,nnod_rt))
-      X_FFTW_mul = 0.0d0
-      C_FFTW_mul = 0.0d0
-!
-      end subroutine allocate_mul_FFTW_plan
-!
-! ------------------------------------------------------------------
-!
-      subroutine deallocate_mul_FFTW_plan
-!
-!
-      deallocate(plan_back_mul, plan_fowd_mul)
-      deallocate(X_FFTW_mul, C_FFTW_mul)
-      iflag_fft_mul_len = 0
-!
-      end subroutine deallocate_mul_FFTW_plan
 !
 ! ------------------------------------------------------------------
 !
