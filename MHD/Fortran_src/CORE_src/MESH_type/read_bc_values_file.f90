bf7507513aeb18b8610df7281383611fbf06244f
Hiroaki Matsui <h_kemono@mac.com>
2015-07-22 16:26:04 +0900

    Same as before

--- MHD/Fortran_src/CORE_src/MESH_type/read_bc_values_file.f90
+++ MHD/Fortran_src/CORE_src/MESH_type/read_bc_values_file.f90
@@ -1,5 +1,5 @@
-!read_bc_values_file_type.f90
-!     module read_bc_values_file_type
+!read_bc_values_file.f90
+!     module read_bc_values_file
 !
 !
 !     programmed by H.Matsui and H.Okuda
@@ -7,25 +7,26 @@
 !     modified by H. Matsui on Aug., 2007
 !
 !
-!    format for data file for boundary data
+!!    format for data file for boundary data
+!!
+!!      line   :  number of type of boundary
+!!      line...:  name of group type (node or surface) and group name
+!!      line   :  name of physical values
+!!               ( temperature, heat_flux, 
+!!                 velocity_x, velocity_y, velocity_z,
+!!                 torque_x, torque_y, torque_z, normal_velocity
+!!                 pressure,
+!!                 magnetic_x, magne_y, magne_z,
+!!                 magnetic_potential, infinity
+!!                 dunny_scalar )
+!!      line...:   values
+!!
+!!      subroutine s_read_bc_values_file(my_rank, nod_grp, sf_grp)
+!!        type(group_data), intent(in) :: nod_grp
+!!        type(surface_group_data), intent(in) :: sf_grp
+!!        integer (kind=kint), intent(in) :: my_rank
 !
-!      line   :  number of type of boundary
-!      line...:  name of group type (node or surface) and group name
-!      line   :  name of physical values
-!               ( temperature, heat_flux, 
-!                 velocity_x, velocity_y, velocity_z,
-!                 torque_x, torque_y, torque_z, normal_velocity
-!                 pressure,
-!                 magnetic_x, magne_y, magne_z,
-!                 magnetic_potential, infinity
-!                 dunny_scalar )
-!      line...:   values
-!
-!      subroutine read_boundary_values_file_t(my_rank, group)
-!        type(mesh_groups), intent(in) ::   group
-!        integer (kind=kint), intent(in) :: my_rank
-!
-      module read_bc_values_file_type
+      module read_bc_values_file
 !
       use m_precision
 !
@@ -37,22 +38,22 @@
 !
 !-----------------------------------------------------------------------
 !
-      subroutine read_boundary_values_file_t(my_rank, group)
+      subroutine s_read_bc_values_file(my_rank, nod_grp, sf_grp)
 !
-      use t_mesh_data
+      use t_group_data
       use boundary_field_file_IO
 !
-      type(mesh_groups), intent(in) ::   group
+      type(group_data), intent(in) :: nod_grp
+      type(surface_group_data), intent(in) :: sf_grp
       integer (kind=kint), intent(in) :: my_rank
 !
 !
-      call read_boundary_values_file(my_rank, group%nod_grp%num_grp,    &
-     &    group%nod_grp%istack_grp, group%nod_grp%grp_name,             &
-     &    group%surf_grp%num_grp, group%surf_grp%istack_grp,            &
-     &    group%surf_grp%grp_name)
+      call read_boundary_values_file(my_rank,                           &
+     &    nod_grp%num_grp, nod_grp%istack_grp, nod_grp%grp_name,        &
+     &    sf_grp%num_grp, sf_grp%istack_grp, sf_grp%grp_name)
 !
-      end subroutine read_boundary_values_file_t
+      end subroutine s_read_bc_values_file
 !
 !-----------------------------------------------------------------------
 !
-      end module read_bc_values_file_type
+      end module read_bc_values_file
