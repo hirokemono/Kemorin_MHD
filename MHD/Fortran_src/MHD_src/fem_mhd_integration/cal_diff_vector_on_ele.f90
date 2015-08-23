5be322f2a8a07fa7f3960614ca67ce8dea7244c3
Hiroaki Matsui <h_kemono@mac.com>
2015-08-23 13:16:28 -0700

    update cal_diff_vector_on_ele

--- MHD/Fortran_src/MHD_src/fem_mhd_integration/cal_diff_vector_on_ele.f90
+++ MHD/Fortran_src/MHD_src/fem_mhd_integration/cal_diff_vector_on_ele.f90
@@ -19,6 +19,7 @@
       use m_machine_parameter
 !
       use m_control_parameter
+      use m_geometry_constants
       use m_geometry_data
       use m_geometry_data_MHD
       use m_node_phys_address
@@ -38,11 +39,19 @@
       subroutine diff_velocity_on_ele
 !
 !
-      call int_diff_vector_on_ele                                       &
+      if (ele1%nnod_4_ele .eq. num_t_quad) then
+        call int_diff_vector_on_ele                                     &
      &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
      &    ele1%a_vol_ele, iele_fl_smp_stack, jac1_3d_q%ntot_int,        &
      &    intg_point_t_evo, jac1_3d_q%xjac, dwx,                        &
      &    d_nod(1,iphys%i_velo), dvx(1,i_dvx))
+      else
+        call int_diff_vector_on_ele                                     &
+     &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
+     &    ele1%a_vol_ele, iele_fl_smp_stack, jac1_3d_l%ntot_int,        &
+     &    intg_point_t_evo, jac1_3d_l%xjac, dnx,                        &
+     &    d_nod(1,iphys%i_velo), dvx(1,i_dvx))
+      end if
 !
       end subroutine diff_velocity_on_ele
 !
@@ -51,11 +60,19 @@
       subroutine diff_magne_on_ele
 !
 !
-      call int_diff_vector_on_ele                                       &
+      if (ele1%nnod_4_ele .eq. num_t_quad) then
+        call int_diff_vector_on_ele                                     &
      &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
      &    ele1%a_vol_ele, iele_cd_smp_stack, jac1_3d_q%ntot_int,        &
      &    intg_point_t_evo, jac1_3d_q%xjac, dwx,                        &
      &    d_nod(1,iphys%i_magne), dvx(1,i_dbx))
+      else
+        call int_diff_vector_on_ele                                     &
+     &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
+     &    ele1%a_vol_ele, iele_cd_smp_stack, jac1_3d_l%ntot_int,        &
+     &    intg_point_t_evo, jac1_3d_l%xjac, dnx,                        &
+     &    d_nod(1,iphys%i_magne), dvx(1,i_dbx))
+      end if
 !
       end subroutine diff_magne_on_ele
 !
@@ -65,11 +82,19 @@
       subroutine diff_filter_v_on_ele
 !
 !
-      call int_diff_vector_on_ele                                       &
+      if (ele1%nnod_4_ele .eq. num_t_quad) then
+        call int_diff_vector_on_ele                                     &
      &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
      &    ele1%a_vol_ele, iele_fl_smp_stack, jac1_3d_q%ntot_int,        &
      &    intg_point_t_evo, jac1_3d_q%xjac, dwx,                        &
      &    d_nod(1,iphys%i_filter_velo), dvx(1,i_dfvx))
+      else
+        call int_diff_vector_on_ele                                     &
+     &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
+     &    ele1%a_vol_ele, iele_fl_smp_stack, jac1_3d_l%ntot_int,        &
+     &    intg_point_t_evo, jac1_3d_l%xjac, dnx,                        &
+     &    d_nod(1,iphys%i_filter_velo), dvx(1,i_dfvx))
+      end if
 !
       end subroutine diff_filter_v_on_ele
 !
@@ -78,11 +103,19 @@
       subroutine diff_filter_b_on_ele
 !
 !
-      call int_diff_vector_on_ele                                       &
+      if (ele1%nnod_4_ele .eq. num_t_quad) then
+        call int_diff_vector_on_ele                                     &
      &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
      &    ele1%a_vol_ele, iele_fl_smp_stack, jac1_3d_q%ntot_int,        &
      &    intg_point_t_evo, jac1_3d_q%xjac, dwx,                        &
      &    d_nod(1,iphys%i_filter_magne), dvx(1,i_dfbx))
+      else
+        call int_diff_vector_on_ele                                     &
+     &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
+     &    ele1%a_vol_ele, iele_fl_smp_stack, jac1_3d_l%ntot_int,        &
+     &    intg_point_t_evo, jac1_3d_l%xjac, dnx,                        &
+     &    d_nod(1,iphys%i_filter_magne), dvx(1,i_dfbx))
+      end if
 !
       return
       end subroutine diff_filter_b_on_ele
@@ -93,11 +126,19 @@
       subroutine diff_temp_on_ele
 !
 !
-      call int_diff_scalar_on_ele                                       &
+      if (ele1%nnod_4_ele .eq. num_t_quad) then
+        call int_diff_scalar_on_ele                                     &
      &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
      &    ele1%a_vol_ele, ele1%istack_ele_smp, jac1_3d_q%ntot_int,      &
      &    intg_point_t_evo, jac1_3d_q%xjac, dwx,                        &
      &    d_nod(1,iphys%i_temp), dvx(1,i_dtx))
+      else
+        call int_diff_scalar_on_ele                                     &
+     &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
+     &    ele1%a_vol_ele, ele1%istack_ele_smp, jac1_3d_l%ntot_int,      &
+     &    intg_point_t_evo, jac1_3d_l%xjac, dnx,                        &
+     &    d_nod(1,iphys%i_temp), dvx(1,i_dtx))
+      end if
 !
       return
       end subroutine diff_temp_on_ele
@@ -107,11 +148,19 @@
       subroutine diff_composition_on_ele
 !
 !
-      call int_diff_scalar_on_ele                                       &
+      if (ele1%nnod_4_ele .eq. num_t_quad) then
+        call int_diff_scalar_on_ele                                     &
      &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
      &    ele1%a_vol_ele, ele1%istack_ele_smp, jac1_3d_q%ntot_int,      &
      &    intg_point_t_evo, jac1_3d_q%xjac, dwx,                        &
      &    d_nod(1,iphys%i_light), dvx(1,i_dcx))
+      else
+        call int_diff_scalar_on_ele                                     &
+     &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
+     &    ele1%a_vol_ele, ele1%istack_ele_smp, jac1_3d_l%ntot_int,      &
+     &    intg_point_t_evo, jac1_3d_l%xjac, dnx,                        &
+     &    d_nod(1,iphys%i_light), dvx(1,i_dcx))
+      end if
 !
       return
       end subroutine diff_composition_on_ele
@@ -122,11 +171,19 @@
       subroutine diff_filter_t_on_ele
 !
 !
-      call int_diff_scalar_on_ele                                       &
+      if (ele1%nnod_4_ele .eq. num_t_quad) then
+        call int_diff_scalar_on_ele                                     &
      &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
      &    ele1%a_vol_ele, ele1%istack_ele_smp, jac1_3d_q%ntot_int,      &
      &    intg_point_t_evo, jac1_3d_q%xjac, dwx,                        &
      &    d_nod(1,iphys%i_filter_temp), dvx(1,i_dftx))
+      else
+        call int_diff_scalar_on_ele                                     &
+     &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
+     &    ele1%a_vol_ele, ele1%istack_ele_smp, jac1_3d_l%ntot_int,      &
+     &    intg_point_t_evo, jac1_3d_l%xjac, dnx,                        &
+     &    d_nod(1,iphys%i_filter_temp), dvx(1,i_dftx))
+      end if
 !
       return
       end subroutine diff_filter_t_on_ele
@@ -136,11 +193,19 @@
       subroutine diff_filter_c_on_ele
 !
 !
-      call int_diff_scalar_on_ele                                       &
+      if (ele1%nnod_4_ele .eq. num_t_quad) then
+        call int_diff_scalar_on_ele                                     &
      &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
      &    ele1%a_vol_ele, ele1%istack_ele_smp, jac1_3d_q%ntot_int,      &
      &    intg_point_t_evo, jac1_3d_q%xjac, dwx,                        &
      &    d_nod(1,iphys%i_filter_comp), dvx(1,i_dfcx))
+      else
+        call int_diff_scalar_on_ele                                     &
+     &   (node1%numnod, ele1%numele, ele1%nnod_4_ele, ele1%ie,          &
+     &    ele1%a_vol_ele, ele1%istack_ele_smp, jac1_3d_l%ntot_int,      &
+     &    intg_point_t_evo, jac1_3d_l%xjac, dnx,                        &
+     &    d_nod(1,iphys%i_filter_comp), dvx(1,i_dfcx))
+      end if
 !
       return
       end subroutine diff_filter_c_on_ele
