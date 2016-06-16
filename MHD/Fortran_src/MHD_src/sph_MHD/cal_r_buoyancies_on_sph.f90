dcef1f340119cbcc914c9c4ffcda9b3f52dabec6
Hiroaki Matsui <h_kemono@mac.com>
2016-06-16 00:57:22 -0700

    temporal 3

--- MHD/Fortran_src/MHD_src/sph_MHD/cal_r_buoyancies_on_sph.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/cal_r_buoyancies_on_sph.f90
@@ -9,6 +9,7 @@
 !!@verbatim
 !!      subroutine s_cal_r_buoyancies_on_sph(kr, sph_rj, rj_fld)
 !!        type(sph_rj_grid), intent(in) ::  sph_rj
+!!        type(phys_address), intent(in) :: ipol
 !!        type(phys_data), intent(inout) :: rj_fld
 !!@endverbatim
 !!
@@ -20,7 +21,6 @@
 !
       use m_constants
       use m_control_parameter
-      use m_sph_phys_address
 !
       implicit  none
 !
@@ -32,16 +32,18 @@
 !
 !-----------------------------------------------------------------------
 !
-      subroutine s_cal_r_buoyancies_on_sph(kr, sph_rj, rj_fld)
+      subroutine s_cal_r_buoyancies_on_sph(kr, sph_rj, ipol, rj_fld)
 !
       use m_machine_parameter
       use m_physical_property
 !
       use t_spheric_rj_data
+      use t_phys_address
       use t_phys_data
 !
       integer(kind= kint), intent(in) :: kr
       type(sph_rj_grid), intent(in) ::  sph_rj
+      type(phys_address), intent(in) :: ipol
       type(phys_data), intent(inout) :: rj_fld
 !
 !
@@ -51,14 +53,14 @@
           if (iflag_debug.eq.1)                                         &
      &      write(*,*)'cal_r_double_buoyancy_on_sph', ipol%i_temp
           call cal_r_double_buoyancy_on_sph                             &
-     &       (kr, ipol%i_temp, ipol%i_div_buoyancy,                     &
+     &       (kr, ipol%i_temp, ipol%i_light, ipol%i_div_buoyancy,       &
      &        sph_rj%nidx_rj, sph_rj%radius_1d_rj_r,                    &
      &        rj_fld%n_point, rj_fld%ntot_phys, rj_fld%d_fld)
         else
           if (iflag_debug.eq.1)                                         &
      &      write(*,*)'cal_r_double_buoyancy_on_sph', ipol%i_par_temp
           call cal_r_double_buoyancy_on_sph                             &
-     &       (kr, ipol%i_par_temp,ipol%i_div_buoyancy,                  &
+     &       (kr, ipol%i_par_temp, ipol%i_light, ipol%i_div_buoyancy,   &
      &        sph_rj%nidx_rj, sph_rj%radius_1d_rj_r,                    &
      &        rj_fld%n_point, rj_fld%ntot_phys, rj_fld%d_fld)
         end if
@@ -99,12 +101,12 @@
 !-----------------------------------------------------------------------
 !-----------------------------------------------------------------------
 !
-      subroutine cal_r_double_buoyancy_on_sph(kr, is_t, is_fr,          &
+      subroutine cal_r_double_buoyancy_on_sph(kr, is_t, is_c, is_fr,    &
      &          nidx_rj, radius_1d_rj_r, nnod_rj, ntot_phys_rj, d_rj)
 !
       use m_physical_property
 !
-      integer(kind= kint), intent(in) :: is_t, is_fr, kr
+      integer(kind= kint), intent(in) :: is_t, is_c, is_fr, kr
       integer(kind = kint), intent(in) :: nidx_rj(2)
       integer(kind = kint), intent(in) :: nnod_rj, ntot_phys_rj
       real(kind = kreal), intent(in) :: radius_1d_rj_r(nidx_rj(1))
@@ -117,7 +119,7 @@
       do j = 1, nidx_rj(2)
         inod = j + (kr-1) * nidx_rj(2)
         d_rj(inod,is_fr) = (coef_buo*d_rj(inod,is_t)                    &
-     &                     + coef_comp_buo*d_rj(inod,ipol%i_light))     &
+     &                     + coef_comp_buo*d_rj(inod,is_c))             &
      &                     * radius_1d_rj_r(kr)
       end do
 !$omp end parallel do
