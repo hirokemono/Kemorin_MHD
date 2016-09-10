a2593828761e2d11523ed8d67998c3ef1c28f38e
Hiroaki Matsui <h_kemono@mac.com>
2016-09-10 12:14:11 -0700

    Fix number of time monitoring item in FEM_MHD

--- MHD/programs/FEM_MHD/analyzer_MHD.f90
+++ MHD/programs/FEM_MHD/analyzer_MHD.f90
@@ -39,7 +39,7 @@
 !
       write(*,*) 'Simulation start: PE. ', my_rank
 !
-      num_elapsed = 7
+      num_elapsed = 68
       call allocate_elapsed_times
 !
       elapse_labels(1) = 'Total time                 '
@@ -49,6 +49,16 @@
       elapse_labels(5) = 'Linear solver time         '
       elapse_labels(6) = 'Communication for RHS      '
       elapse_labels(7) = 'Communication time         '
+!
+      elapse_labels(60) = 'Sectioning initialization.    '
+      elapse_labels(61) = 'Isosurfaceing initialization.    '
+      elapse_labels(62) = 'Volume rendering initialization.    '
+      elapse_labels(63) = 'fieldline initialization.    '
+!
+      elapse_labels(65) = 'Sectioning.    '
+      elapse_labels(66) = 'Isosurfaceing.    '
+      elapse_labels(67) = 'Volume rendering.    '
+      elapse_labels(68) = 'fieldline.    '
 !
 !     --------------------- 
 !
