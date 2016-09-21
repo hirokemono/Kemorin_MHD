4e128940c0abbf5a0330983ffb5d29be2fe253e9
Hiroaki Matsui <h_kemono@mac.com>
2016-09-20 21:47:55 -0700

    Bug fix

--- MHD/Fortran_src/PARALLEL_src/COMM_src/gz_MPI_mesh_data_IO.f90
+++ MHD/Fortran_src/PARALLEL_src/COMM_src/gz_MPI_mesh_data_IO.f90
@@ -115,16 +115,24 @@
 !
 !
       call gz_mpi_read_num_node_ele(IO_param, mesh_IO)
+      call calypso_mpi_barrier
+      write(*,*) 'gz_mpi_read_num_node_ele end'
 !
       call gz_mpi_read_element_info(IO_param, mesh_IO%ele)
+      call calypso_mpi_barrier
+      write(*,*) 'gz_mpi_read_element_info end'
 !
 ! ----  import & export
 !
       call gz_mpi_skip_header(IO_param, len(hd_fem_import()))
       call gz_mpi_read_import_data(IO_param, mesh_IO%nod_comm)
+      call calypso_mpi_barrier
+      write(*,*) 'gz_mpi_read_import_data end'
 !
       call gz_mpi_skip_header(IO_param, len(hd_fem_export()))
       call gz_mpi_read_export_data(IO_param, mesh_IO%nod_comm)
+      call calypso_mpi_barrier
+      write(*,*) 'gz_mpi_read_export_data end'
 !
       end subroutine gz_mpi_read_geometry_data
 !
@@ -142,14 +150,20 @@
       call gz_mpi_skip_header(IO_param, len(hd_fem_nodgrp()))
       call gz_mpi_read_group_data                                       &
      &   (IO_param, mesh_group_IO%nod_grp)
+      call calypso_mpi_barrier
+      write(*,*) 'nod_grp end'
 !  read element group
       call gz_mpi_skip_header(IO_param, len(hd_fem_elegrp()))
       call gz_mpi_read_group_data                                       &
      &   (IO_param, mesh_group_IO%ele_grp)
+      call calypso_mpi_barrier
+      write(*,*) 'ele_grp end'
 !  read surface group
       call gz_mpi_skip_header(IO_param, len(hd_fem_sfgrp()))
       call gz_mpi_read_surf_grp_data                                    &
      &   (IO_param, mesh_group_IO%surf_grp)
+      call calypso_mpi_barrier
+      write(*,*) 'surf_grp end'
 !
       end subroutine gz_mpi_read_mesh_groups
 !
@@ -167,11 +181,15 @@
       call gz_mpi_read_node_position(IO_param,                          &
      &    mesh_IO%node%numnod, ithree, mesh_IO%node%inod_global,        &
      &    mesh_IO%node%xx)
+      call calypso_mpi_barrier
+      write(*,*) 'gz_mpi_read_node_position end'
 !
 !  ----  read element data -------
 !
       call gz_mpi_skip_header(IO_param, len(hd_fem_elem()))
       call gz_mpi_read_num_of_data(IO_param, mesh_IO%ele%numele)
+      call calypso_mpi_barrier
+      write(*,*) 'gz_mpi_read_num_of_data end'
 !
       end subroutine gz_mpi_read_num_node_ele
 !
@@ -185,11 +203,17 @@
 !
       call gz_mpi_skip_header(IO_param, len(hd_fem_para()))
       call gz_mpi_read_domain_info(IO_param, mesh_IO%nod_comm)
+      call calypso_mpi_barrier
+      write(*,*) 'gz_mpi_read_domain_info end'
 !
       call gz_mpi_skip_header(IO_param, len(hd_fem_node()))
       call gz_mpi_read_num_of_data                                      &
      &   (IO_param, mesh_IO%node%internal_node)
+      call calypso_mpi_barrier
+      write(*,*) 'mesh_IO%node%internal_node end', mesh_IO%node%internal_node
       call gz_mpi_read_num_of_data(IO_param, mesh_IO%node%numnod)
+      call calypso_mpi_barrier
+      write(*,*) 'mesh_IO%node%numnod end', mesh_IO%node%numnod
 !
       end subroutine gz_mpi_read_num_node
 !
@@ -203,11 +227,6 @@
 !
 !
       call gz_mpi_write_num_of_data(IO_param, nod_IO%internal_node)
-      call gz_mpi_write_charahead(IO_param,                             &
-     &    len_multi_int_textline(IO_param%nprocs_in),                   &
-     &    int_stack8_textline(IO_param%nprocs_in,                       &
-     &    IO_param%istack_merged))
-!
       call gz_mpi_write_node_position(IO_param,                         &
      &   nod_IO%numnod, ithree, nod_IO%inod_global, nod_IO%xx)
 !
