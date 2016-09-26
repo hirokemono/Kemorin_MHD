5bc03f905206b7d45aaad03730428cbbd06d136a
Hiroaki Matsui <h_kemono@mac.com>
2016-09-25 21:00:17 -0700

    Recover missing files (Why??)

---
+++ MHD/Fortran_src/VIZ_src/volume_rendering/pvr_surface_enhancement.f90
@@ -0,0 +1,181 @@
+!>@file  pvr_surface_enhancement.f90
+!!       module pvr_surface_enhancement
+!!
+!!@author H. Matsui
+!!@date   Programmed in Aug., 2011
+!
+!> @brief Set flag and opacities to enhance surfaces
+!!
+!!@verbatim
+!!      subroutine set_pvr_bc_enhanse_flag                              &
+!!     &         (surf_grp, num_enhanse_grp, enhanse_grp, draw_type,    &
+!!     &          fixed_opacity, iflag_enhanse, enhansed_opacity)
+!!      subroutine set_opacity_for_boundaries(surf_grp, sf_grp_v,       &
+!!     &          view_param, iflag_enhanse, enhansed_opacity,          &
+!!     &          numele, numsurf, isf_4_ele, arccos_sf)
+!!        type(surface_group_data), intent(in) :: surf_grp
+!!@endverbatim
+!
+      module pvr_surface_enhancement
+!
+      use m_precision
+      use m_constants
+      use m_machine_parameter
+      use m_geometry_constants
+      use t_group_data
+      use t_surface_group_geometry
+      use t_surface_group_connect
+      use t_control_params_4_pvr
+!
+      use calypso_mpi
+!
+      implicit  none
+!
+      character(len = kchara), parameter                                &
+     &                        :: LABEL_INCREASE = 'increase'
+      character(len = kchara), parameter                                &
+     &                        :: LABEL_DECREASE = 'decrease'
+!
+      character(len = kchara), parameter :: LABEL_EDGE = 'boarder'
+      character(len = kchara), parameter                                &
+     &                        :: LABEL_FORWARD = 'forward_surface'
+      character(len = kchara), parameter                                &
+     &                        :: LABEL_REVERSE = 'reverse_surface'
+!
+      integer(kind = kint), parameter :: IFLAG_NONE =          0
+      integer(kind = kint), parameter :: IFLAG_SHOW_EDGE =     2
+      integer(kind = kint), parameter :: IFLAG_SHOW_FORWARD =  1
+      integer(kind = kint), parameter :: IFLAG_SHOW_REVERSE = -1
+!
+      real(kind = kreal), parameter :: coef_op = 3e-2
+!
+!  ---------------------------------------------------------------------
+!
+      contains
+!
+!  ---------------------------------------------------------------------
+!
+      subroutine set_pvr_bc_enhanse_flag                                &
+     &         (surf_grp, num_enhanse_grp, enhanse_grp, draw_type,      &
+     &          fixed_opacity, iflag_enhanse, enhansed_opacity)
+!
+      use t_control_params_4_pvr
+      use skip_comment_f
+!
+      type(surface_group_data), intent(in) :: surf_grp
+      integer(kind = kint), intent(in) :: num_enhanse_grp
+      character(len=kchara), intent(in) :: enhanse_grp(num_enhanse_grp)
+      character(len=kchara), intent(in) :: draw_type(num_enhanse_grp)
+      real(kind = kreal), intent(in) :: fixed_opacity(num_enhanse_grp)
+      integer(kind = kint), intent(inout)                               &
+     &                     :: iflag_enhanse(surf_grp%num_grp)
+      real(kind = kreal), intent(inout)                                 &
+     &                     :: enhansed_opacity(surf_grp%num_grp)
+!
+      integer(kind = kint) :: igrp, jgrp
+!
+!
+      iflag_enhanse(1:surf_grp%num_grp) = IFLAG_NONE
+      do jgrp = 1, num_enhanse_grp
+        do igrp = 1, surf_grp%num_grp
+          if(cmp_no_case(enhanse_grp(jgrp),                             &
+     &                   surf_grp%grp_name(igrp))) then
+            if(cmp_no_case(draw_type(jgrp), LABEL_EDGE)) then
+              iflag_enhanse(igrp) = IFLAG_SHOW_EDGE
+            else if(cmp_no_case(draw_type(jgrp), LABEL_FORWARD)) then
+              iflag_enhanse(igrp) = IFLAG_SHOW_EDGE
+            else if(cmp_no_case(draw_type(jgrp), LABEL_REVERSE)) then
+              iflag_enhanse(igrp) = IFLAG_SHOW_EDGE
+            end if
+            enhansed_opacity(igrp) = fixed_opacity(jgrp)
+            exit
+          end if
+        end do
+      end do
+!
+      end subroutine set_pvr_bc_enhanse_flag
+!
+!  ---------------------------------------------------------------------
+!
+      subroutine set_opacity_for_boundaries(surf_grp, sf_grp_v,         &
+     &          view_param, iflag_enhanse, enhansed_opacity,            &
+     &          numele, numsurf, isf_4_ele, arccos_sf)
+!
+      use set_position_pvr_screen
+!
+      type(surface_group_data), intent(in) :: surf_grp
+      type(surface_group_geometry), intent(in) :: sf_grp_v
+      type(pvr_view_parameter), intent(in) :: view_param
+      integer(kind = kint), intent(in) :: numele, numsurf
+      integer(kind = kint), intent(in) :: isf_4_ele(numele,nsurf_4_ele)
+      integer(kind = kint), intent(in)                                  &
+     &                     :: iflag_enhanse(surf_grp%num_grp)
+      real(kind = kreal), intent(in)                                    &
+     &                     :: enhansed_opacity(surf_grp%num_grp)
+!
+      real(kind = kreal), intent(inout) :: arccos_sf(numsurf)
+!
+      integer(kind = kint) :: igrp
+      integer(kind = kint) :: ist, ied, inum, isurf, iele, k1
+      real(kind = kreal) :: size_v, ratio
+      real(kind = kreal), allocatable :: norm_sf_model(:,:)
+!
+!
+      allocate(norm_sf_model(surf_grp%num_item,4))
+!
+!$omp parallel workshare
+      norm_sf_model = zero
+!$omp end parallel workshare
+!
+      call chenge_direction_pvr_modelview(view_param%modelview_mat,     &
+     &    surf_grp%num_item, sf_grp_v%vnorm_sf_grp, norm_sf_model)
+!
+!$omp parallel do
+      do inum = 1, surf_grp%num_item
+        if(norm_sf_model(inum,3) .eq. zero) then
+          norm_sf_model(inum,3) =  1e-6
+        end if
+      end do
+!$omp end parallel do
+!
+      do igrp = 1, surf_grp%num_grp
+        ist = surf_grp%istack_grp(igrp-1)+1
+        ied = surf_grp%istack_grp(igrp)
+        do inum = ist, ied
+          iele = surf_grp%item_sf_grp(1,inum)
+          k1 =   surf_grp%item_sf_grp(2,inum)
+          isurf = abs(isf_4_ele(iele,k1))
+          size_v = sqrt(norm_sf_model(inum,1)*norm_sf_model(inum,1)     &
+     &              + norm_sf_model(inum,2)*norm_sf_model(inum,2)       &
+     &              + norm_sf_model(inum,3)*norm_sf_model(inum,3))
+          ratio = coef_op * size_v / norm_sf_model(inum,3)
+!
+          if(iflag_enhanse(igrp) .eq. IFLAG_SHOW_EDGE) then
+            if(abs(ratio) .gt. ONE) then
+              arccos_sf(isurf) = zero
+            else
+              arccos_sf(isurf) = enhansed_opacity(igrp)
+            end if
+          else if(iflag_enhanse(igrp) .eq. IFLAG_SHOW_FORWARD) then
+            if(ratio .gt. zero) then
+              arccos_sf(isurf) = zero
+            else
+              arccos_sf(isurf) = enhansed_opacity(igrp)
+            end if
+          else if(iflag_enhanse(igrp) .eq. IFLAG_SHOW_REVERSE) then
+            if(ratio .lt. zero) then
+              arccos_sf(isurf) = zero
+            else
+              arccos_sf(isurf) = enhansed_opacity(igrp)
+            end if
+          else
+            arccos_sf(isurf) = zero
+          end if
+        end do
+      end do
+!
+      end subroutine set_opacity_for_boundaries
+!
+!  ---------------------------------------------------------------------
+!
+      end module pvr_surface_enhancement
---
+++ MHD/Fortran_src/VIZ_src/volume_rendering/set_control_each_pvr.f90
@@ -0,0 +1,273 @@
+!set_control_each_pvr
+!      module set_control_each_pvr
+!
+!        programmed by H.Matsui on May. 2006
+!
+!!      subroutine set_pvr_file_control                                 &
+!!     &         (pvr, num_nod_phys, phys_nod_name, file_param)
+!!      subroutine set_control_pvr(pvr, ele_grp, surf_grp,              &
+!!     &          num_nod_phys, phys_nod_name, fld_param, view_param,   &
+!!     &          field_pvr, color_param, cbar_param)
+!
+      module set_control_each_pvr
+!
+      use m_precision
+!
+      use m_constants
+      use m_error_IDs
+      use m_control_data_4_pvr
+      use calypso_mpi
+!
+      use set_field_comp_for_viz
+!
+      implicit  none
+!
+      private :: set_control_pvr_movie
+!
+!  ---------------------------------------------------------------------
+!
+      contains
+!
+!  ---------------------------------------------------------------------
+!
+      subroutine set_pvr_file_control                                   &
+     &         (pvr, num_nod_phys, phys_nod_name, file_param)
+!
+      use t_control_params_4_pvr
+      use set_area_4_viz
+      use skip_comment_f
+!
+      integer(kind = kint), intent(in) :: num_nod_phys
+      character(len=kchara), intent(in) :: phys_nod_name(num_nod_phys)
+!
+      type(pvr_ctl), intent(in) :: pvr
+      type(pvr_output_parameter), intent(inout) :: file_param
+!
+!
+      integer(kind = kint) :: num_field, num_phys_viz
+      character(len = kchara) :: tmpchara
+!
+!
+      if(pvr%file_head_ctl%iflag .gt. 0) then
+        file_param%pvr_prefix = pvr%file_head_ctl%charavalue
+      else 
+        file_param%pvr_prefix = 'pvr'
+      end if
+!
+      tmpchara = pvr%file_fmt_ctl%charavalue
+      if     (cmp_no_case(tmpchara, 'ucd')                              &
+     &   .or. cmp_no_case(tmpchara, 'udt')) then
+        file_param%id_pvr_file_type = 0
+      else if(cmp_no_case(tmpchara, 'png')) then
+        file_param%id_pvr_file_type = 12
+      else if(cmp_no_case(tmpchara, 'bmp')) then
+        file_param%id_pvr_file_type = 11
+      else
+        file_param%id_pvr_file_type = 11
+      end if
+!
+!
+      tmpchara = pvr%transparent_ctl%charavalue
+      if     (cmp_no_case(tmpchara, 'rgba')                             &
+     &   .or. cmp_no_case(tmpchara, 'transparent')) then
+        file_param%id_pvr_transparent = 1
+      else if(cmp_no_case(tmpchara, 'rgb')                              &
+     &   .or. cmp_no_case(tmpchara, 'solid')) then
+        file_param%id_pvr_transparent = 0
+      else
+        file_param%id_pvr_transparent = 0
+      end if
+!
+      file_param%iflag_monitoring = 0
+      if(yes_flag(pvr%monitoring_ctl%charavalue)) then
+        file_param%iflag_monitoring = 1
+      end if
+!
+      call check_field_4_viz(num_nod_phys, phys_nod_name,               &
+     &    ione, pvr%pvr_field_ctl, num_field, num_phys_viz)
+      if(num_field .eq. 0) then
+        call calypso_MPI_abort(ierr_PVR,'set correct field name')
+      end if
+!
+!
+      if(iflag_debug .gt. 0) then
+        write(*,*) 'pvr_prefix', file_param%pvr_prefix
+        write(*,*) 'id_pvr_file_type', file_param%id_pvr_file_type
+        write(*,*) 'id_pvr_transparent', file_param%id_pvr_transparent
+      end if
+!
+      end subroutine set_pvr_file_control
+!
+!  ---------------------------------------------------------------------
+!  ---------------------------------------------------------------------
+!
+      subroutine set_control_pvr(pvr, ele_grp, surf_grp,                &
+     &          num_nod_phys, phys_nod_name, fld_param, view_param,     &
+     &          field_pvr, color_param, cbar_param)
+!
+      use t_group_data
+      use t_control_params_4_pvr
+      use t_geometries_in_pvr_screen
+      use set_pvr_modelview_matrix
+      use set_area_4_viz
+      use set_color_4_pvr
+      use set_rgba_4_each_pixel
+      use pvr_surface_enhancement
+      use set_control_pvr_color
+      use skip_comment_f
+!
+      type(group_data), intent(in) :: ele_grp
+      type(surface_group_data), intent(in) :: surf_grp
+!
+      integer(kind = kint), intent(in) :: num_nod_phys
+      character(len=kchara), intent(in) :: phys_nod_name(num_nod_phys)
+!
+      type(pvr_ctl), intent(inout) :: pvr
+      type(pvr_field_parameter), intent(inout) :: fld_param
+      type(pvr_view_parameter), intent(inout) :: view_param
+      type(pvr_projected_field), intent(inout) :: field_pvr
+      type(pvr_colormap_parameter), intent(inout) :: color_param
+      type(pvr_colorbar_parameter), intent(inout) :: cbar_param
+!
+      integer(kind = kint) :: icheck_ncomp(1), i
+      integer(kind = kint) :: ifld_tmp(1), icomp_tmp(1), ncomp_tmp(1)
+      character(len = kchara) :: fldname_tmp(1)
+      character(len = kchara) :: tmpchara
+!
+!
+      call set_components_4_viz                                         &
+     &   (num_nod_phys, phys_nod_name, ione, pvr%pvr_field_ctl,         &
+     &    pvr%pvr_comp_ctl, ione, ifld_tmp, icomp_tmp,                  &
+     &    icheck_ncomp, ncomp_tmp, fldname_tmp)
+      fld_param%id_pvr_output =    ifld_tmp(1)
+      fld_param%icomp_pvr_output = icomp_tmp(1)
+      fld_param%ncomp_pvr_org =    ncomp_tmp(1)
+      fld_param%name_pvr_output =  fldname_tmp(1)
+!
+      if (icheck_ncomp(1) .gt. 1)                                       &
+     &     call calypso_MPI_abort(ierr_PVR, 'set scalar for rendering')
+!
+!
+      call count_area_4_viz(ele_grp%num_grp, ele_grp%grp_name,          &
+     &    pvr%pvr_area_ctl%num, pvr%pvr_area_ctl%c_tbl,                 &
+     &    fld_param%nele_grp_area_pvr)
+!
+      if (fld_param%nele_grp_area_pvr .le. 0) then
+        call calypso_MPI_abort(ierr_PVR, 'set correct element group')
+      else
+        call alloc_pvr_element_group(fld_param)
+      end if
+!
+!
+      call s_set_area_4_viz(ele_grp%num_grp, ele_grp%grp_name,          &
+     &    pvr%pvr_area_ctl%num, pvr%pvr_area_ctl%c_tbl,                 &
+     &    fld_param%nele_grp_area_pvr, fld_param%id_ele_grp_area_pvr)
+!
+!
+      if (pvr%surf_enhanse_ctl%num .gt. 0) then
+        call set_pvr_bc_enhanse_flag(surf_grp,                          &
+     &      pvr%surf_enhanse_ctl%num, pvr%surf_enhanse_ctl%c1_tbl,      &
+     &      pvr%surf_enhanse_ctl%c2_tbl, pvr%surf_enhanse_ctl%vect,     &
+     &      field_pvr%iflag_enhanse, field_pvr%enhansed_opacity)
+      else
+         field_pvr%iflag_enhanse = IFLAG_NONE
+      end if
+!
+!
+      field_pvr%num_isosurf = pvr%num_pvr_iso_ctl
+      if(field_pvr%num_isosurf .gt. 0) then
+        call alloc_pvr_isosurfaces(field_pvr)
+!
+        do i = 1, field_pvr%num_isosurf
+          if(pvr%pvr_iso_ctl(i)%isosurf_value_ctl%iflag .gt. 0) then
+            field_pvr%iso_value(i)                                      &
+     &        = pvr%pvr_iso_ctl(i)%isosurf_value_ctl%realvalue
+          end if
+          if(pvr%pvr_iso_ctl(i)%opacity_ctl%iflag .gt. 0) then
+            field_pvr%iso_opacity(i)                                    &
+     &        = pvr%pvr_iso_ctl(i)%opacity_ctl%realvalue
+          end if
+          if(pvr%pvr_iso_ctl(i)%isosurf_type_ctl%iflag .gt. 0) then
+            tmpchara = pvr%pvr_iso_ctl(i)%isosurf_type_ctl%charavalue
+            if(cmp_no_case(tmpchara, LABEL_DECREASE)) then
+              field_pvr%itype_isosurf(i) = IFLAG_SHOW_REVERSE
+            else if(cmp_no_case(tmpchara, LABEL_DECREASE)) then
+              field_pvr%itype_isosurf(i) = IFLAG_SHOW_FORWARD
+            else
+              field_pvr%itype_isosurf(i) = IFLAG_SHOW_FORWARD
+            end if
+          end if
+        end do
+        write(*,*) 'iso_value',   field_pvr%iso_value
+        write(*,*) 'iso_opacity', field_pvr%iso_opacity
+        write(*,*) 'itype_isosurf', field_pvr%itype_isosurf
+      end if
+!
+      call set_control_pvr_movie(pvr%movie, view_param)
+!
+!    set colormap setting
+      call set_control_pvr_lighting(pvr%color, color_param)
+      call set_control_pvr_colormap(pvr%color, color_param)
+!
+!    set colorbar setting
+      call set_control_pvr_colorbar(pvr%colorbar, cbar_param)
+!
+!   set transfer matrix
+!
+      call s_set_pvr_modelview_matrix(pvr%mat, view_param)
+!
+      end subroutine set_control_pvr
+!
+!  ---------------------------------------------------------------------
+!
+      subroutine set_control_pvr_movie(movie, view_param)
+!
+      use t_control_data_pvr_misc
+      use t_control_params_4_pvr
+      use t_geometries_in_pvr_screen
+      use skip_comment_f
+!
+      type(pvr_movie_ctl), intent(in) :: movie
+      type(pvr_view_parameter), intent(inout) :: view_param
+!
+      character(len = kchara) :: tmpchara
+!
+!
+      if      (movie%num_frames_ctl%iflag .gt.    0                     &
+     &   .and. movie%rotation_axis_ctl%iflag .gt. 0) then
+        tmpchara = movie%rotation_axis_ctl%charavalue
+        if     (cmp_no_case(tmpchara, 'x')) then
+          view_param%iprm_pvr_rot(1) = 1
+          view_param%iflag_rotate_snap = 1
+        else if(cmp_no_case(tmpchara, 'y')) then
+          view_param%iprm_pvr_rot(1) = 2
+          view_param%iflag_rotate_snap = 1
+        else if(cmp_no_case(tmpchara, 'z')) then
+          view_param%iprm_pvr_rot(1) = 3
+          view_param%iflag_rotate_snap = 1
+        else
+          view_param%iprm_pvr_rot(1) =   0
+          view_param%iflag_rotate_snap = 0
+        end if
+!
+        view_param%iprm_pvr_rot(2) = movie%num_frames_ctl%intvalue
+      else
+        view_param%iflag_rotate_snap = 0
+        view_param%iprm_pvr_rot(1) = 0
+        view_param%iprm_pvr_rot(2) = 1
+        view_param%iflag_rotate_snap = 0
+      end if
+!
+      if(view_param%iflag_rotate_snap .eq. 0) then
+        view_param%istart_rot = 0
+        view_param%iend_rot =   0
+      else
+        view_param%istart_rot = 1
+        view_param%iend_rot =   view_param%iprm_pvr_rot(2)
+      end if
+!
+      end subroutine set_control_pvr_movie
+!
+!  ---------------------------------------------------------------------
+!
+      end module set_control_each_pvr
