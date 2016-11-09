ab4f0a060a0629428e7eb4e1e52aef7c9324dabf
Hiroaki Matsui <h_kemono@mac.com>
2016-11-09 15:10:47 -0800

    Fix module reference

--- MHD/Fortran_src/MHD_src/sph_MHD/FEM_analyzer_sph_MHD.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/FEM_analyzer_sph_MHD.f90
@@ -176,6 +176,7 @@
       use t_phys_data
       use t_phys_address
       use t_sph_trans_arrays_MHD
+      use m_control_parameter
 !
       use output_viz_file_control
       use copy_snap_4_sph_trans
