af8cafc887229e4fb11844f9ff141e535631bf3d
Hiroaki Matsui <h_kemono@mac.com>
2017-04-23 08:41:02 -0700

    Move subroutine

--- MHD/Fortran_src/MHD_src/IO/t_bc_data_list.f90
+++ MHD/Fortran_src/MHD_src/IO/t_bc_data_list.f90
@@ -16,6 +16,8 @@
 !!      subroutine dealloc_nod_bc_list(fld_BC)
 !!      subroutine dealloc_surf_bc_list(fld_BC)
 !!        type(boundary_condition_lists), intent(inout) :: fld_BC
+!!
+!!      subroutine deallocate_surf_bc_lists(MHD_prop, MHD_BC)
 !!@endverbatim
 !
       module t_bc_data_list
@@ -103,6 +105,52 @@
       deallocate(bc_list%bc_name)
 !
       end subroutine dealloc_bc_type_ctl
+!
+!-----------------------------------------------------------------------
+!-----------------------------------------------------------------------
+!
+      subroutine deallocate_surf_bc_lists(MHD_prop, MHD_BC)
+!
+      use t_control_parameter
+!
+      type(MHD_evolution_param), intent(in) :: MHD_prop
+      type(MHD_BC_lists), intent(inout) :: MHD_BC
+!
+!
+      if (MHD_prop%ht_prop%iflag_scheme .gt. id_no_evolution) then
+        if(MHD_BC%temp_BC%surf_BC%num_bc .gt. 0)                        &
+     &      call dealloc_surf_bc_list(MHD_BC%temp_BC)
+      end if
+!
+      if (MHD_prop%fl_prop%iflag_scheme .gt. id_no_evolution) then
+        if(MHD_BC%velo_BC%surf_BC%num_bc.gt.0)                          &
+     &      call dealloc_surf_bc_list(MHD_BC%velo_BC)
+        if(MHD_BC%press_BC%surf_BC%num_bc.gt.0)                         &
+     &      call dealloc_surf_bc_list(MHD_BC%press_BC)
+      end if
+!
+      if    (MHD_prop%cd_prop%iflag_Bevo_scheme .gt. id_no_evolution    &
+     &  .or. MHD_prop%cd_prop%iflag_Aevo_scheme .gt. id_no_evolution)   &
+     & then
+        if(MHD_BC%magne_BC%surf_BC%num_bc .gt. 0)                       &
+     &        call dealloc_surf_bc_list(MHD_BC%magne_BC)
+        if(MHD_BC%current_BC%surf_BC%num_bc .gt. 0)                     &
+     &        call dealloc_surf_bc_list(MHD_BC%current_BC)
+        if(MHD_BC%e_potential_BC%surf_BC%num_bc.gt.0)                   &
+     &        call dealloc_surf_bc_list(MHD_BC%e_potential_BC)
+      end if
+!
+      if (MHD_prop%cd_prop%iflag_Aevo_scheme .gt. id_no_evolution) then
+        if(MHD_BC%a_potential_BC%surf_BC%num_bc.gt.0)                   &
+     &        call dealloc_surf_bc_list(MHD_BC%a_potential_BC)
+      end if
+! 
+      if (MHD_prop%cp_prop%iflag_scheme .gt. id_no_evolution) then
+        if(MHD_BC%light_BC%surf_BC%num_bc.gt.0)                         &
+     &     call dealloc_surf_bc_list(MHD_BC%light_BC)
+      end if
+!
+      end subroutine deallocate_surf_bc_lists
 !
 !-----------------------------------------------------------------------
 !-----------------------------------------------------------------------
