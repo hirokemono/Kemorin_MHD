29fd2771c2daa91afdea1c1f4a68124a3cc5e51a
Hiroaki Matsui <h_kemono@mac.com>
2017-05-08 15:45:17 +0900

    Turn on Coriolis force calculation for field data output

--- MHD/Fortran_src/MHD_src/sph_MHD/cal_energy_flux_rtp.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/cal_energy_flux_rtp.f90
@@ -109,11 +109,11 @@
      &      frc_pl(1,f_trns%i_c_flux) )
       end if
 !
-!      if( (f_trns%i_Coriolis*fl_prop%iflag_4_coriolis) .gt. 0) then
-!        call cal_wz_coriolis_rtp                                       &
-!     &     (nnod_pole, sph_rtp%nidx_rtp, fl_prop%coef_cor,             &
-!     &      fls_pl(1,bs_trns%i_velo), frc_pl(1,f_trns%i_Coriolis))
-!      end if
+      if( (f_trns%i_Coriolis*fl_prop%iflag_4_coriolis) .gt. 0) then
+        call cal_wz_coriolis_rtp                                        &
+     &     (nnod_pole, sph_rtp%nidx_rtp, fl_prop%coef_cor,              &
+     &      fls_pl(1,bs_trns%i_velo), frc_pl(1,f_trns%i_Coriolis))
+      end if
 !$omp end parallel
 !
       end subroutine cal_nonlinear_pole_MHD
@@ -152,11 +152,11 @@
 !
 !
 !$omp parallel
-!      if(fs_trns%i_coriolis .gt. 0) then
-!        call cal_wz_coriolis_rtp                                       &
-!     &     (sph_rtp%nnod_rtp, sph_rtp%nidx_rtp, fl_prop%coef_cor,      &
-!     &      fls_rtp(1,bs_trns%i_velo), frs_rtp(1,fs_trns%i_Coriolis))
-!      end if
+      if(fs_trns%i_coriolis .gt. 0) then
+        call cal_wz_coriolis_rtp                                        &
+     &     (sph_rtp%nnod_rtp, sph_rtp%nidx_rtp, fl_prop%coef_cor,       &
+     &      fls_rtp(1,bs_trns%i_velo), frs_rtp(1,fs_trns%i_Coriolis))
+      end if
 !
       if(fs_trns%i_ujb .gt. 0) then
         call cal_dot_prod_no_coef_smp(sph_rtp%nnod_rtp,                 &
