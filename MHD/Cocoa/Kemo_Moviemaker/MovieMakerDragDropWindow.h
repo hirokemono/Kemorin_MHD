3a50bdbd0ba14e908ed8839f0526319832d2f549
Hiroaki Matsui <h_kemono@mac.com>
2018-02-16 14:08:33 -0800

    Update

--- MHD/Cocoa/Kemo_Moviemaker/MovieMakerDragDropWindow.h
+++ MHD/Cocoa/Kemo_Moviemaker/MovieMakerDragDropWindow.h
@@ -9,5 +9,25 @@
 #ifndef MovieMakerDragDropWindow_h
 #define MovieMakerDragDropWindow_h
 
+#import <Cocoa/Cocoa.h>
+#import "KemoMovieControll.h"
+
+@interface KemoMovieDragDropWindow : NSWindow {
+    IBOutlet KemoMovieControll* _kemoMovieIOControl;
+    int id_window;
+}
+
+
+- (id)init;
+- (void) awakeFromNib;
+- (BOOL)performDragOperation:(id<NSDraggingInfo>)sender;
+
+- (unsigned int)draggingEntered:(id <NSDraggingInfo>)sender;
+- (void)draggingExited:(id <NSDraggingInfo>)sender;
+
+- (void)windowWillClose:(NSNotification *)aNotofication;
+- (void)windowDidBecomeKey:(NSNotification *)aNotofication;
+@end
+
 
 #endif /* MovieMakerDragDropWindow_h */
--- MHD/Cocoa/Kemo_Moviemaker/MovieMakerDragDropWindow.m
+++ MHD/Cocoa/Kemo_Moviemaker/MovieMakerDragDropWindow.m
@@ -7,3 +7,64 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "MovieMakerDragDropWindow.h"
+
+
+@implementation KemoMovieDragDropWindow
+
+- (id)init
+{
+    return(self);
+}
+
+- (void) awakeFromNib
+{
+    [self registerForDraggedTypes:[NSArray arrayWithObjects:
+                                   NSFilenamesPboardType, nil]];
+}
+
+
+- (BOOL)performDragOperation:(id<NSDraggingInfo>)sender
+{
+    NSPasteboard *pbd = [sender draggingPasteboard];
+    
+    if ( [[pbd types] containsObject:NSFilenamesPboardType] ) {
+        NSArray *filearray = [pbd propertyListForType:NSFilenamesPboardType];
+        //       int numberOfFiles = [filearray count];
+        NSString *filename = [filearray objectAtIndex:0];
+        
+        [self setAlphaValue:1.0];
+        [_kemoMovieIOControl OpenReferenceImageFile:filename];
+    };
+    return YES;    
+}
+
+- (unsigned int)draggingEntered:(id <NSDraggingInfo>)sender
+{
+    [self makeKeyAndOrderFront:nil];
+    [self setAlphaValue:0.7];
+    [self displayIfNeeded];
+    
+    return NSDragOperationMove;
+}
+
+- (void)draggingExited:(id <NSDraggingInfo>)sender
+{
+    [self setAlphaValue:1.0];
+    [self displayIfNeeded];
+}
+
+- (void)windowWillClose:(NSNotification *)aNotofication;
+{
+    //	NSLog(@"Window %d is closing", id_window);
+    //    kemoview_set_single_viewer_id(id_window);
+    //	[self release];
+}
+
+- (void)windowDidBecomeKey:(NSNotification *)aNotofication;
+{
+    //	NSLog(@"Window %d is front", id_window);
+    //    kemoview_set_single_viewer_id(id_window);
+}
+
+@end
