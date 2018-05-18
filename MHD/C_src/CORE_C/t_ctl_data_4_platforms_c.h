f2a11c9f3ae53634e2325843cd194416a19d4aa2
Hiroaki Matsui <h_kemono@mac.com>
2018-05-18 14:09:48 +0900

    Add update

--- MHD/C_src/CORE_C/t_ctl_data_4_platforms_c.h
+++ MHD/C_src/CORE_C/t_ctl_data_4_platforms_c.h
@@ -5,9 +5,61 @@
 //  Created by Hiroaki Matsui on 2018/05/18.
 //
 
-#ifndef t_ctl_data_4_platforms_c_h
-#define t_ctl_data_4_platforms_c_h
+#ifndef t_ctl_data_4_platforms_c_h__
+#define t_ctl_data_4_platforms_c_h__
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "kemosrc_param_c.h"
+#include "control_elements_IO_c.h"
+
+struct platform_data_control_c{
+    int maxlen;
+    
+    struct int_ctl_item *ndomain_c;
+    struct int_ctl_item *num_smp_c;
+
+    struct chara_ctl_item *debug_flag_c;
+
+    struct chara_ctl_item *mesh_file_prefix_c;
+    struct chara_ctl_item *field_file_prefix_c;
+    struct chara_ctl_item *restart_file_prefix_c;
+    struct chara_ctl_item *spectr_field_file_prefix_c;
+    struct chara_ctl_item *sph_file_prefix_c;
+
+    struct chara_ctl_item *coriolis_int_file_name_c;
+    struct chara_ctl_item *bc_data_file_name_c;
+    struct chara_ctl_item *interpolate_sph_to_fem_c;
+    struct chara_ctl_item *interpolate_fem_to_sph_c;
+    
+    struct chara_ctl_item *mesh_file_fmt_c;
+    struct chara_ctl_item *sph_file_fmt_c;
+    struct chara_ctl_item *restart_file_fmt_c;
+    struct chara_ctl_item *field_file_fmt_c;
+    struct chara_ctl_item *itp_file_fmt_c;
+    struct chara_ctl_item *spectr_field_fmt_c;
+    struct chara_ctl_item *coriolis_file_fmt_c;
+    
+    struct chara_ctl_item *memory_conservation_c;
+    struct chara_ctl_item *FEM_mesh_output_switch_c;
+    struct chara_ctl_item *FEM_surface_output_switch_c;
+    struct chara_ctl_item *FEM_viewer_output_switch_c;
+    struct chara_ctl_item *del_org_data_ctl_c;
+    
+    struct chara_ctl_item *excluding_FEM_mesh_c;
+};
+
+    
+/* prototypes */
+
+void alloc_platform_data_control_c(struct platform_data_control_c *files);
+void dealloc_platform_data_control_c(struct platform_data_control_c *files);
+
+int read_platform_data_control_c(FILE *fp, char buf[LENGTHBUF], const char *label, 
+			struct platform_data_control_c *files);
+int write_platform_data_control_c(FILE *fp, int level, int *iflag, 
+			const char *label, struct platform_data_control_c *files);
+
 
 #endif /* t_ctl_data_4_platforms_c_h */
