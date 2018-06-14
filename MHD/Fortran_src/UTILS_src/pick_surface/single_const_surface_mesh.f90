d0bba23c92347bd09894227d9281d7a48c4cbf82
Hiroaki Matsui <h_kemono@mac.com>
2018-06-14 12:35:17 -0700

    temporal commit

--- MHD/Fortran_src/UTILS_src/pick_surface/single_const_surface_mesh.f90
+++ MHD/Fortran_src/UTILS_src/pick_surface/single_const_surface_mesh.f90
@@ -52,6 +52,14 @@
       type(merged_mesh) :: mgd_mesh1
       type(group_data_merged_surf) :: mgd_sf_grp1
       type(merged_viewer_mesh) :: mgd_view_mesh1
+      type(element_data), save :: ele_p
+      type(surface_data), save :: surf_p
+      type(edge_data), save :: edge_p
+      type(merged_mesh), save :: mgd_mesh_p
+      type(group_data_merged_surf), save :: mgd_sf_grp_p
+      type(merged_viewer_mesh), allocatable :: mgd_view_mesh_p(:)
+!
+      integer(kind = kint) :: ip, id_rank
 !
 !
       write(*,*) 'find_mesh_format_4_viewer'
@@ -61,14 +69,35 @@
 !
 !  set mesh_information
 !
-      call const_merged_mesh_data                                       &
-     &   (mesh_file, ele_v, surf_v, edge_v, mgd_mesh1, mgd_sf_grp1)
-      write(*,*) 'const_surf_mesh_4_viewer'
-      call const_surf_mesh_4_viewer                                     &
-     &   (surf_v, edge_v, mgd_mesh1, mgd_sf_grp1, mgd_view_mesh1)
+      allocate(mgd_view_mesh_p(mgd_mesh1%num_pe))
+      do ip = 1, mgd_mesh1%num_pe
+        id_rank = ip - 1
+        write(*,*) 'const_merged_mesh_sgl', ip
+        call const_merged_mesh_sgl                                      &
+     &    (id_rank, mesh_file, ele_p, surf_p, edge_p, mgd_mesh_p, mgd_sf_grp_p)
 !
-      call sel_output_surface_grid(mesh_file,                           &
-     &    surf_v%nnod_4_surf, edge_v%nnod_4_edge, mgd_view_mesh1)
+        call const_surf_mesh_4_viewer                                   &
+     &     (surf_p, edge_p, mgd_mesh_p, mgd_sf_grp_p, mgd_view_mesh_p(ip))
+!
+        call sel_output_single_surface_grid(id_rank, mesh_file,         &
+     &    surf_p%nnod_4_surf, edge_p%nnod_4_edge,             &
+     &    mgd_view_mesh_p(ip)%view_mesh, mgd_view_mesh_p(ip)%domain_grps,       &
+     &    mgd_view_mesh_p(ip)%view_nod_grps,    &
+     &    mgd_view_mesh_p(ip)%view_ele_grps, &
+     &    mgd_view_mesh_p(ip)%view_sf_grps)
+!
+        call dealloc_number_of_mesh(mgd_mesh_p)
+!!
+!        call dealloc_iso_surf_4_egrp_m(mgd_sf_grp_p)
+!        call dealloc_n_iso_surf_4_ele_grp(mgd_sf_grp_p)
+      end do
+!
+!      call const_merged_mesh_data                                       &
+!     &   (mesh_file, ele_v, surf_v, edge_v, mgd_mesh1, mgd_sf_grp1)
+!      write(*,*) 'const_surf_mesh_4_viewer'
+!      call const_surf_mesh_4_viewer                                     &
+!     &   (surf_v, edge_v, mgd_mesh1, mgd_sf_grp1, mgd_view_mesh1)
+!
 !
       end subroutine choose_surface_mesh_sgl
 !
@@ -265,6 +294,76 @@
       end if
 !
       end subroutine set_surf_domain_id_viewer
+!
+!------------------------------------------------------------------
+!------------------------------------------------------------------
+!
+      subroutine const_merged_mesh_sgl                                  &
+     &         (id_rank, mesh_file, ele, surf, edge,                    &
+     &          mgd_mesh, mgd_sf_grp)
+!
+      use t_file_IO_parameter
+      use load_mesh_data
+      use set_group_types_4_IO
+      use count_number_with_overlap
+      use set_merged_geometry
+      use mesh_IO_select
+      use const_merged_surf_data
+      use const_merged_surf_4_group
+!
+      integer(kind = kint) :: id_rank
+      type(field_IO_params), intent(in) :: mesh_file
+      type(element_data), intent(inout) :: ele
+      type(surface_data), intent(inout) :: surf
+      type(edge_data), intent(inout) :: edge
+      type(merged_mesh), intent(inout) :: mgd_mesh
+      type(group_data_merged_surf), intent(inout) :: mgd_sf_grp
+!
+      type(mesh_data) :: fem_IO_p
+      integer(kind = kint) :: ierr
+!
+!
+      mgd_mesh%num_pe = ione
+      call alloc_number_of_mesh(mgd_mesh)
+      call alloc_subdomain_groups(mgd_mesh)
+!
+      if (iflag_debug.gt.0) write(*,*) 'mpi_input_mesh'
+      call sel_read_mesh(mesh_file, id_rank, fem_IO_p, ierr)
+!
+      call set_mesh_geometry_data(fem_IO_p%mesh,                        &
+     &    mgd_mesh%subdomain(1)%nod_comm, mgd_mesh%subdomain(1)%node,   &
+     &    mgd_mesh%subdomain(1)%ele)
+      call set_grp_data_from_IO(fem_IO_p%group,                         &
+     &    mgd_mesh%sub_nod_grp(1), mgd_mesh%sub_ele_grp(1),             &
+     &    mgd_mesh%sub_surf_grp(1))
+      call dealloc_groups_data(fem_IO_p%group)
+      ele%nnod_4_ele = fem_IO_p%mesh%ele%nnod_4_ele
+!
+      call count_num_overlap_geom_type                                  &
+     &   (mgd_mesh%num_pe, mgd_mesh%subdomain, mgd_mesh%merge_tbl)
+      call count_num_geometry_w_overlap                                 &
+     &   (mgd_mesh%num_pe, mgd_mesh%subdomain, mgd_mesh%merge_tbl,      &
+     &    mgd_mesh%merged)
+!
+      call count_overlapped_mesh_groups(mgd_mesh)
+!
+!
+       write(*,*) 'set_source_mesh_parameter'
+       call set_source_mesh_parameter                                   &
+     &    (ele, surf, edge, mgd_mesh%merged_surf)
+!
+       write(*,*) 's_const_merged_surf_data'
+       call s_const_merged_surf_data(mgd_mesh)
+!
+       write(*,*) 'const_merged_surface_4_ele_grp'
+       call const_merged_surface_4_ele_grp                              &
+     &    (mgd_mesh%merged, mgd_mesh%merged_grp, mgd_mesh%merged_surf,  &
+     &     mgd_sf_grp)
+       write(*,*) 'const_merged_surface_4_sf_grp'
+       call const_merged_surface_4_sf_grp                               &
+     &    (mgd_mesh%merged_grp, mgd_mesh%merged_surf, mgd_sf_grp)
+!
+      end subroutine const_merged_mesh_sgl
 !
 !------------------------------------------------------------------
 !
