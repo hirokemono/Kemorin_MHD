df7a7270f68ff9e623fdbf7b81a4f31c8d5b1630
Hiroaki Matsui <h_kemono@mac.com>
2018-06-18 13:16:58 -0700

    Start constructing parallel assemble_udt

---
+++ MHD/Fortran_src/UTILS_src/MERGE/load_mesh_data_4_merge.f90
@@ -0,0 +1,140 @@
+!>@file   load_mesh_data_4_merge.f90
+!!@brief  module load_mesh_data_4_merge
+!!
+!!@author H. Matsui
+!!@date Programmed in Feb., 2011
+!
+!>@brief Construct spectrum data for new spectrum domain
+!!
+!!@verbatim
+!!      subroutine load_local_mesh_4_merge                              &
+!!     &         (mesh_file, nprocs_in, mesh, group, ele_mesh)
+!!        type(field_IO_params), intent(in) ::  mesh_file
+!!        type(mesh_geometry), intent(inout) :: mesh(nprocs_in)
+!!        type(mesh_groups), intent(inout) :: group(nprocs_in)
+!!        type(element_geometry), intent(inout) :: ele_mesh(nprocs_in)
+!!      subroutine load_local_node_ele_4_merge                          &
+!!     &         (mesh_file, nprocs_in, mesh)
+!!        type(field_IO_params), intent(in) ::  mesh_file
+!!        type(mesh_geometry), intent(inout) :: mesh(nprocs_in)
+!!        type(mesh_geometry) :: mesh_IO_m
+!!      subroutine load_local_node_4_merge                              &
+!!     &         (mesh_file, nprocs_in, mesh)
+!!        type(field_IO_params), intent(in) ::  mesh_file
+!!        type(mesh_geometry), intent(inout) :: mesh(nprocs_in)
+!!@endverbatim
+!
+      module load_mesh_data_4_merge
+!
+      use m_precision
+!
+      use calypso_mpi
+      use m_constants
+      use t_mesh_data
+      use t_geometry_data
+      use t_comm_table
+      use t_group_data
+!
+      implicit none
+!
+!
+! -----------------------------------------------------------------------
+!
+      contains
+!
+! -----------------------------------------------------------------------
+!
+      subroutine load_local_mesh_4_merge                                &
+     &         (mesh_file, nprocs_in, mesh, group, ele_mesh)
+!
+      use mesh_MPI_IO_select
+      use load_mesh_data
+!
+      integer(kind = kint), intent(in) ::  nprocs_in
+      type(field_IO_params), intent(in) ::  mesh_file
+      type(mesh_geometry), intent(inout) :: mesh(nprocs_in)
+      type(mesh_groups), intent(inout) :: group(nprocs_in)
+      type(element_geometry), intent(inout) :: ele_mesh(nprocs_in)
+!
+      type(mesh_data) :: fem_IO_m
+      integer(kind = kint) :: id_rank, iloop, ip
+!
+!
+      do iloop = 0, (nprocs_in-1) / nprocs
+        id_rank = my_rank + iloop * nprocs
+        ip = id_rank + 1
+        call sel_mpi_read_mesh                                          &
+     &     (nprocs_in, id_rank, mesh_file, fem_IO_m)
+!
+        if(id_rank .lt. nprocs_in) then
+          call set_mesh(fem_IO_m, mesh(ip), group(ip),                  &
+     &        ele_mesh(ip)%surf%nnod_4_surf,                            &
+     &        ele_mesh(ip)%edge%nnod_4_edge)
+        end if
+      end do
+!
+      end subroutine load_local_mesh_4_merge
+!
+! -----------------------------------------------------------------------
+!
+      subroutine load_local_node_ele_4_merge                            &
+     &         (mesh_file, nprocs_in, mesh)
+!
+      use mesh_MPI_IO_select
+      use load_mesh_data
+!
+      integer(kind = kint), intent(in) ::  nprocs_in
+      type(field_IO_params), intent(in) ::  mesh_file
+      type(mesh_geometry), intent(inout) :: mesh(nprocs_in)
+!
+      type(mesh_geometry) :: mesh_IO_m
+      integer(kind = kint) :: id_rank, iloop, ip
+!
+!
+      do iloop = 0, (nprocs_in-1) / nprocs
+        id_rank = my_rank + iloop * nprocs
+        ip = id_rank + 1
+        call sel_mpi_read_mesh_geometry                                 &
+     &     (nprocs_in, id_rank, mesh_file, mesh_IO_m)
+!
+        if(id_rank .lt. nprocs_in) then
+          call set_mesh_geometry_data(mesh_IO_m,                        &
+     &        mesh(ip)%nod_comm, mesh(ip)%node, mesh(ip)%ele)
+        end if
+      end do
+!
+      end subroutine load_local_node_ele_4_merge
+!
+! -----------------------------------------------------------------------
+!
+      subroutine load_local_node_4_merge                                &
+     &         (mesh_file, nprocs_in, mesh)
+!
+      use mesh_MPI_IO_select
+      use load_mesh_data
+!
+      integer(kind = kint), intent(in) ::  nprocs_in
+      type(field_IO_params), intent(in) ::  mesh_file
+      type(mesh_geometry), intent(inout) :: mesh(nprocs_in)
+!
+      type(mesh_geometry) :: mesh_IO_m
+      integer(kind = kint) :: id_rank, iloop, ip
+!
+!
+      do iloop = 0, (nprocs_in-1) / nprocs
+        id_rank = my_rank + iloop * nprocs
+        ip = id_rank + 1
+        call sel_mpi_read_geometry_size                                 &
+     &     (nprocs_in, id_rank, mesh_file, mesh_IO_m)
+!
+        if(id_rank .lt. nprocs_in) then
+          call set_node_geometry_data                                   &
+     &       (mesh_IO_m, mesh(ip)%nod_comm, mesh(ip)%node)
+        end if
+      end do
+!
+      end subroutine load_local_node_4_merge
+!
+! -----------------------------------------------------------------------
+!
+      end module load_mesh_data_4_merge
