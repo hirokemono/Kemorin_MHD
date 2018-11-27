977e3b9d69d5b62a91010e38770de87a227cae44
Hiroaki Matsui <h_kemono@mac.com>
2018-11-27 11:56:25 -0800

    skip intermediate

--- MHD/Fortran_src/MHD_src/sph_SGS_MHD/sph_SGS_nl_gradient_pout.f90
+++ MHD/Fortran_src/MHD_src/sph_SGS_MHD/sph_SGS_nl_gradient_pout.f90
@@ -209,7 +209,7 @@
       real(kind = kreal) :: gamma_r, gamma_t, gamma_p
 !
 !
-      write(*,*) 'tako'
+      write(*,*) 'ahoaho'
       do lt = 1, nidx_rtp(2)
         do kr = 1, kr_in-1
           inod = kr + (lt-1)*nidx_rtp(1)                                &
@@ -227,49 +227,66 @@
           gamma_t = coef * theta_2nd_moment(lt) * (r(kr))**2
           gamma_p = coef * phi_2nd_moment * (r(kr) * sin_t(lt))**2
 !
-          du1_dx1 = grad_ux(inod,1)
-          du1_dx2 = grad_ux(inod,2) * r(kr) - u_rtp(inod,2)
-          du1_dx3 = grad_ux(inod,3) * sin_t(lt) * r(kr)                 &
-     &              - u_rtp(inod,3) * sin_t(lt)
-          du2_dx1 = grad_uy(inod,1)
-          du2_dx2 = grad_uy(inod,2) * r(kr) + u_rtp(inod,1)
-          du2_dx3 = grad_uy(inod,3) * sin_t(lt) * r(kr)                 &
-     &              - u_rtp(inod,3) * cos_t(lt)
-          du3_dx1 = grad_uz(inod,1)
-          du3_dx2 = grad_uz(inod,2) * r(kr)
-          du3_dx3 = grad_uz(inod,3) * sin_t(lt) * r(kr)                 &
-     &              + u_rtp(inod,2) * cos_t(lt)                         &
-     &              - u_rtp(inod,1) * sin_t(lt)
+          du1_dx1 = zero
+          du1_dx2 = zero
+          du1_dx3 = zero
+          du2_dx1 = zero
+          du2_dx2 = zero
+          du2_dx3 = zero
+          du3_dx1 = zero
+          du3_dx2 = zero
+          du3_dx3 = zero
+
+          db1_dx1 = zero
+          db1_dx2 = zero
+          db1_dx3 = zero
+          db2_dx1 = zero
+          db2_dx2 = zero
+          db2_dx3 = zero
+          db3_dx1 = zero
+          db3_dx2 = zero
+          db3_dx3 = zero
+
+!          du1_dx1 = grad_ux(inod,1)
+!          du1_dx2 = grad_ux(inod,2) * r(kr) - u_rtp(inod,2)
+!          du1_dx3 = grad_ux(inod,3) * sin_t(lt) * r(kr)                &
+!     &              - u_rtp(inod,3) * sin_t(lt)
+!          du2_dx1 = grad_uy(inod,1)
+!          du2_dx2 = grad_uy(inod,2) * r(kr) + u_rtp(inod,1)
+!          du2_dx3 = grad_uy(inod,3) * sin_t(lt) * r(kr)                &
+!     &              - u_rtp(inod,3) * cos_t(lt)
+!          du3_dx1 = grad_uz(inod,1)
+!          du3_dx2 = grad_uz(inod,2) * r(kr)
+!          du3_dx3 = grad_uz(inod,3) * sin_t(lt) * r(kr)                &
+!     &              + u_rtp(inod,2) * cos_t(lt)                        &
+!     &              - u_rtp(inod,1) * sin_t(lt)
 !
-          db1_dx1 = grad_bx(inod,1)
-          db1_dx2 = grad_bx(inod,2) * r(kr) - b_rtp(inod,2)
-          db1_dx3 = grad_bx(inod,3) * sin_t(lt) * r(kr)                 &
-     &              - b_rtp(inod,3) * sin_t(lt)
-          db2_dx1 = grad_by(inod,1)
-          db2_dx2 = grad_by(inod,2) * r(kr) + b_rtp(inod,1)
-          db2_dx3 = grad_by(inod,3) * sin_t(lt) * r(kr)                 &
-     &              - b_rtp(inod,3) * cos_t(lt)
-          db3_dx1 = grad_bz(inod,1)
-          db3_dx2 = grad_bz(inod,2) * r(kr)
-          db3_dx3 = grad_bz(inod,3) * sin_t(lt) * r(kr)                 &
-     &              + b_rtp(inod,2) * cos_t(lt)                         &
-     &              - b_rtp(inod,1) * sin_t(lt)
+!          db1_dx1 = grad_bx(inod,1)
+!          db1_dx2 = grad_bx(inod,2) * r(kr) - b_rtp(inod,2)
+!          db1_dx3 = grad_bx(inod,3) * sin_t(lt) * r(kr)                &
+!     &              - b_rtp(inod,3) * sin_t(lt)
+!          db2_dx1 = grad_by(inod,1)
+!          db2_dx2 = grad_by(inod,2) * r(kr) + b_rtp(inod,1)
+!          db2_dx3 = grad_by(inod,3) * sin_t(lt) * r(kr)                &
+!     &              - b_rtp(inod,3) * cos_t(lt)
+!          db3_dx1 = grad_bz(inod,1)
+!          db3_dx2 = grad_bz(inod,2) * r(kr)
+!          db3_dx3 = grad_bz(inod,3) * sin_t(lt) * r(kr)                &
+!     &              + b_rtp(inod,2) * cos_t(lt)                        &
+!     &              - b_rtp(inod,1) * sin_t(lt)
 !
-          d_SGS(inod,1)  = 0.0d0
-          d_SGS(inod,2)  = 0.0d0
-          d_SGS(inod,3)  = 0.0d0
-!          d_SGS(inod,1)                                                &
-!     &          =  gamma_r * (du2_dx1 * db3_dx1 - du3_dx1 * db2_dx1)   &
-!     &           + gamma_t * (du2_dx2 * db3_dx2 - du3_dx2 * db2_dx2)   &
-!     &           + gamma_p * (du2_dx3 * db3_dx3 - du3_dx3 * db2_dx3)
-!          d_SGS(inod,2)                                                &
-!     &          =  gamma_r * (du3_dx1 * db1_dx1 - du1_dx1 * db3_dx1)   &
-!     &           + gamma_t * (du3_dx2 * db1_dx2 - du1_dx2 * db3_dx2)   &
-!     &           + gamma_p * (du3_dx3 * db1_dx3 - du1_dx3 * db3_dx3)
-!          d_SGS(inod,3)                                                &
-!     &          =  gamma_r * (du1_dx1 * db2_dx1 - du2_dx1 * db1_dx1)   &
-!     &           + gamma_t * (du1_dx2 * db2_dx2 - du2_dx2 * db1_dx2)   &
- !    &           + gamma_p * (du1_dx3 * db2_dx3 - du2_dx3 * db1_dx3)
+          d_SGS(inod,1)                                                 &
+     &          =  gamma_r * (du2_dx1 * db3_dx1 - du3_dx1 * db2_dx1)    &
+     &           + gamma_t * (du2_dx2 * db3_dx2 - du3_dx2 * db2_dx2)    &
+     &           + gamma_p * (du2_dx3 * db3_dx3 - du3_dx3 * db2_dx3)
+          d_SGS(inod,2)                                                 &
+     &          =  gamma_r * (du3_dx1 * db1_dx1 - du1_dx1 * db3_dx1)    &
+     &           + gamma_t * (du3_dx2 * db1_dx2 - du1_dx2 * db3_dx2)    &
+     &           + gamma_p * (du3_dx3 * db1_dx3 - du1_dx3 * db3_dx3)
+          d_SGS(inod,3)                                                 &
+     &          =  gamma_r * (du1_dx1 * db2_dx1 - du2_dx1 * db1_dx1)    &
+     &           + gamma_t * (du1_dx2 * db2_dx2 - du2_dx2 * db1_dx2)    &
+     &           + gamma_p * (du1_dx3 * db2_dx3 - du2_dx3 * db1_dx3)
         end do
 !
         do kr = kr_out+1, nidx_rtp(1)
