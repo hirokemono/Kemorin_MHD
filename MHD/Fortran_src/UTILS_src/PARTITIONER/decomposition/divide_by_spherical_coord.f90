486686a772147f422b5ddb82b77792ac3404ee37
Hiroaki Matsui <h_kemono@mac.com>
2019-01-18 16:56:44 -0800

    Commit again

--- MHD/Fortran_src/UTILS_src/PARTITIONER/decomposition/divide_by_spherical_coord.f90
+++ MHD/Fortran_src/UTILS_src/PARTITIONER/decomposition/divide_by_spherical_coord.f90
@@ -30,8 +30,8 @@
      &          radius, theta, phi, num_bc_grp, ntot_bc_grp,            &
      &          istack_bc_grp, item_bc_grp, name_bc_grp, nod_d_grp)
 !
-      use m_shell_surface
       use m_ctl_param_partitioner
+      use t_shell_surface_4_part
       use t_domain_group_4_partition
 !
       use sort_sphere_4_rcb
@@ -54,6 +54,7 @@
 !
       type(domain_group_4_partition), intent(inout) :: nod_d_grp
 !
+      type(shell_surface_4_part) :: sphere_4_part
       integer(kind = kint)  :: irest1, num1
 !
 !C
@@ -62,10 +63,11 @@
 !C +------------------------------+
 !C===
 !
-      call allocate_local_nnod_shell(nproc)
+      call alloc_local_nnod_shell(nproc, sphere_4_part)
 !
       call cal_divide_and_rest(num1, irest1, nnod, nproc)
-      call set_number_of_segments(nproc, num1, irest1, numnod_local)
+      call set_number_of_segments                                       &
+     &   (nproc, num1, irest1, sphere_4_part%numnod_local)
 !
 !
         if (nnod_4_ele.ne.8 .and. iflag_sphere_data.eq.0) then
@@ -83,16 +85,16 @@
      &        istack_bc_grp, item_bc_grp, name_bc_grp, sphere_4_part)
         else
           if (nnod_4_ele .eq. 8) then
-            call set_sphere_data_4_linear                               &
-     &         (ndivide_eb(2), ndivide_eb(3), sphere_4_part)
+            call set_sphere_data_4_linear(sphere_data_file_name,        &
+     &          ndivide_eb(2), ndivide_eb(3), sphere_4_part)
           else if (nnod_4_ele .eq. 20) then
-            call set_sphere_data_4_quad                                 &
-     &         (ndivide_eb(2), ndivide_eb(3), sphere_4_part)
+            call set_sphere_data_4_quad(sphere_data_file_name,          &
+     &          ndivide_eb(2), ndivide_eb(3), sphere_4_part)
           end if
         end if
 !
-      allocate (VAL_sph(num_CMB))
-      allocate (IS_sph(num_CMB))
+      allocate (VAL_sph(sphere_4_part%num_CMB))
+      allocate (IS_sph(sphere_4_part%num_CMB))
 
       allocate (VAL_cube(nnod))
       allocate (IS_cube(nnod))
@@ -101,45 +103,59 @@
 
 !   grouping radial layer
 !
-        call part_sphere_with_radius(ndivide_eb(1), IGROUP_radius,      &
-     &          num_layer, nlayer_ICB, nlayer_CMB)
+        call part_sphere_with_radius(ndivide_eb(1),                     &
+     &      sphere_4_part%IGROUP_radius, sphere_4_part%num_layer,       &
+     &      sphere_4_part%nlayer_ICB, sphere_4_part%nlayer_CMB)
 !
 !   grouping nodes on sphere
 !
         write(*,*) 's_sort_sphere_4_rcb'
-        call s_sort_sphere_4_rcb(num_CMB, IGROUP_cmb, ndivide_eb(2),    &
-     &      ndivide_eb(3), rtp_cmb(1,2), rtp_cmb(1,3), VAL_sph, IS_sph)
+        call s_sort_sphere_4_rcb                                        &
+     &     (sphere_4_part%num_CMB, sphere_4_part%IGROUP_cmb,            &
+     &      ndivide_eb(2), ndivide_eb(3), sphere_4_part%rtp_cmb(1,2),   &
+     &      sphere_4_part%rtp_cmb(1,3), VAL_sph, IS_sph)
 !
-!       write(*,*) 'IGROUP_cmb', IGROUP_cmb
+!       write(*,*) 'IGROUP_cmb', sphere_4_part%IGROUP_cmb
 !   grouping nodes in shell
 !
         write(*,*) 'set_sphere_domain_list_l'
         call set_sphere_domain_list_l(nproc, nnod, ndivide_eb(1),       &
-     &      num_CMB, nnod_CMB, num_layer, istack_sph, item_sph,         &
-     &      IGROUP_cmb, IGROUP_radius, nod_d_grp%IGROUP, ncore_local)
+     &      sphere_4_part%num_CMB, sphere_4_part%nnod_CMB,              &
+     &      sphere_4_part%num_layer, sphere_4_part%istack_sph,          &
+     &      sphere_4_part%item_sph, sphere_4_part%IGROUP_cmb,           &
+     &      sphere_4_part%IGROUP_radius, nod_d_grp%IGROUP,              &
+     &      sphere_4_part%ncore_local)
 !
         if (nnod_4_ele .eq. 20) then
           write(*,*) 'set_sphere_domain_list_q'
           call set_sphere_domain_list_q(nproc, nnod, ndivide_eb(1),     &
-     &        num_CMB, num_layer, istack20_sph, item20_sph,             &
-     &        IGROUP_cmb, IGROUP_radius, nod_d_grp%IGROUP, ncore_local)
+     &        sphere_4_part%num_CMB, sphere_4_part%num_layer,           &
+     &        sphere_4_part%istack20_sph, sphere_4_part%item20_sph,     &
+     &        sphere_4_part%IGROUP_cmb, sphere_4_part%IGROUP_radius,    &
+     &        nod_d_grp%IGROUP, sphere_4_part%ncore_local)
         end if
 !
 !   count nuber of node for each subdomain
 !
         write(*,*) 'count_num_subdomain_4_cube'
-        call count_num_subdomain_4_cube(nproc,                          &
-     &          numnod_local, ncore_local, nrest_local)
+        call count_num_subdomain_4_cube                                 &
+     &     (nproc, sphere_4_part%numnod_local,                          &
+     &      sphere_4_part%ncore_local, sphere_4_part%nrest_local)
 !
 !  devide around center
 !
-        call s_sort_cube_4_rcb(nnod, num_cube, nproc, ndivide_eb(1),    &
-     &      ndivide_eb(2), ndivide_eb(3), xx, phi, inod_free,           &
-     &      nrest_local, nod_d_grp%IGROUP, IS_cube, VAL_cube)
+        call s_sort_cube_4_rcb(nnod, sphere_4_part%num_cube, nproc,     &
+     &      ndivide_eb(1), ndivide_eb(2), ndivide_eb(3), xx, phi,       &
+     &      sphere_4_part%inod_free, sphere_4_part%nrest_local,         &
+     &      nod_d_grp%IGROUP, IS_cube, VAL_cube)
 !
       deallocate (VAL_sph, IS_sph)
       deallocate (VAL_cube, IS_cube)
-
+!
+      if (nnod_4_ele .eq. 20) call dealloc_quad_shell(sphere_4_part)
+      call dealloc_linear_shell(sphere_4_part)
+      call dealloc_local_nnod_shell(sphere_4_part)
+!
       end subroutine divide_by_sphere_coord
 !
 !   --------------------------------------------------------------------
