da2a4e36408fe70f01e28e65eaee3ab4a334f047
Hiroaki Matsui <h_kemono@mac.com>
2019-03-15 09:50:51 -0700

    Split 32-bit integer and 64-bit integer versions

---
+++ MHD/Fortran_src/CORE_src/64bit_Integer/m_precision.f90
@@ -0,0 +1,27 @@
+!>@file   m_precision.f90
+!!@brief  module m_precision
+!!
+!!@author H. Matsui and H.Okuda
+!!@date Programmed in July 2000
+!!@n    Modified in Sep., 2005
+!
+!> @brief Define byte length for number and characters
+!
+      module m_precision
+!
+      implicit none
+!
+!>  kind parameter for integer type variable (  4 byte )
+      integer, parameter   ::  kint  =       8
+!>  kind parameter for real    type variable (  8 byte )
+      integer, parameter   ::  kreal =       8
+!>  character length for name               (  255 byte )
+      integer, parameter   ::  kchara =    255
+!>  character length for file name          ( 2047 byte )
+      integer, parameter   ::  len_fname = 2047
+!
+!>  kind parameter for long integer for global index (  8 byte )
+      integer, parameter   ::  kint_gl = 8
+!
+      end module m_precision
+
