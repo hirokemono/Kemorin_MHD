121e9e4dbe05c358ceef93871dbac256bc58f69e
Hiroaki Matsui <h_kemono@mac.com>
2019-06-26 04:45:00 -0700

    Add bcast check

--- MHD/Fortran_src/MHD_src/IO/bcast_ctl_data_SGS_model.f90
+++ MHD/Fortran_src/MHD_src/IO/bcast_ctl_data_SGS_model.f90
@@ -59,6 +59,24 @@
         call bcast_control_4_SGS_filter(sgs_ctl%sph_filter_ctl(i))
       end do
 !
+      write(*,*) my_rank, 'num_sph_filter_ctl', sgs_ctl%num_sph_filter_ctl
+      do i = 1, sgs_ctl%num_sph_filter_ctl
+        write(*,*) my_rank, 'sph_filter_type_ctl',  i, &
+     &     sgs_ctl%sph_filter_ctl(i)%sph_filter_type_ctl%charavalue
+        write(*,*) my_rank, 'radial_filter_type_ctl', i,   &
+     &     sgs_ctl%sph_filter_ctl(i)%radial_filter_type_ctl%charavalue
+        write(*,*) my_rank, 'maximum_moments_ctl', i,   &
+     &     sgs_ctl%sph_filter_ctl(i)%maximum_moments_ctl%intvalue
+        write(*,*) my_rank, 'sphere_filter_width_ctl', i,   &
+     &     sgs_ctl%sph_filter_ctl(i)%sphere_filter_width_ctl%realvalue
+        write(*,*) my_rank, 'radial_filter_width_ctl', i,   &
+     &     sgs_ctl%sph_filter_ctl(i)%radial_filter_width_ctl%realvalue
+        write(*,*) my_rank, 'first_reference_ctl', i,   &
+     &     sgs_ctl%sph_filter_ctl(i)%first_reference_ctl%intvalue
+        write(*,*) my_rank, 'second_reference_ctl', i,   &
+     &     sgs_ctl%sph_filter_ctl(i)%second_reference_ctl%intvalue
+      end do
+
       call bcast_ctl_array_c1(sgs_ctl%SGS_terms_ctl)
       call bcast_ctl_array_c1(sgs_ctl%commutate_fld_ctl)
 !
