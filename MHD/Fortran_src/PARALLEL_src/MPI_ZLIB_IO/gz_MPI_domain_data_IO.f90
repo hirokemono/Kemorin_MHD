88beffa625670be0c6c01d0b144e6b23120147af
Hiroaki Matsui <h_kemono@mac.com>
2019-07-03 16:24:04 -0700

    Delete skip zero number stack reading

--- MHD/Fortran_src/PARALLEL_src/MPI_ZLIB_IO/gz_MPI_domain_data_IO.f90
+++ MHD/Fortran_src/PARALLEL_src/MPI_ZLIB_IO/gz_MPI_domain_data_IO.f90
@@ -217,11 +217,12 @@
       integer(kind=kint), intent(inout) :: istack(0:num)
       integer(kind=kint), intent(inout) :: ntot
 !
+      integer(kind = kint) :: ist
+!
 !
       istack(0) = 0
-      if(num .gt. 0) then
-        call gz_mpi_read_int_vector(IO_param, num, istack(1))
-      end if
+      ist = min(1,num)
+      call gz_mpi_read_int_vector(IO_param, num, istack(ist))
       ntot = istack(num)
 !
       end subroutine gz_mpi_read_int_stack
