5ed31920541cc36cdb34bebcb79d20977aeb6b85
Hiroaki Matsui <h_kemono@mac.com>
2019-07-04 17:27:18 -0700

    again

--- MHD/Fortran_src/VIZ_src/volume_rendering/t_stencil_buffer_work.f90
+++ MHD/Fortran_src/VIZ_src/volume_rendering/t_stencil_buffer_work.f90
@@ -87,10 +87,6 @@
       call MPI_REDUCE(max_ray_start_lc, max_ray_start_gl, 1,            &
      &    CALYPSO_GLOBAL_INT, MPI_SUM, int(irank_image_file),           &
      &    CALYPSO_COMM, ierr_MPI)
-      if(num_ray_start_lc .le. 0) write(*,*)                  &
-     &     'num_ray_start_lc is 0 at', my_rank
-      if(max_ray_start_lc .le. 0) write(*,*)                  &
-     &     'max_ray_start_lc is 0 at', my_rank
 !
       call alloc_stencil_buffer_work(num_pixel_xy, stencil_wk)
       call set_global_stencil_buffer                                    &
