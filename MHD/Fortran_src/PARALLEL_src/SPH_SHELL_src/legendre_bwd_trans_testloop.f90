a881df7ae3facaa79194325eb354f01e565a8b51
Hiroaki Matsui <h_kemono@mac.com>
2019-07-10 23:14:34 -0700

    Check constants

--- MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/legendre_bwd_trans_testloop.f90
+++ MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/legendre_bwd_trans_testloop.f90
@@ -88,6 +88,8 @@
 !
       nkrs = ncomp * sph_rlm%nidx_rlm(1)
       nkrt = 2*nvector * sph_rlm%nidx_rlm(1)
+      write(*,*) 'nkrs', nkrs, WK_l_tst%nkrs
+      write(*,*) 'nkrt', nkrt, WK_l_tst%nkrt
 !
       do mp_rlm = 1, sph_rtm%nidx_rtm(3)
         jst = idx_trns%lstack_rlm(mp_rlm-1)
