84995ac32d3c00334ee7b5ba617b7d4f3d987285
Hiroaki Matsui <h_kemono@mac.com>
2019-07-12 10:03:42 -0700

    Swap loop order for spectr

--- MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/legendre_fwd_trans_testloop.f90
+++ MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/legendre_fwd_trans_testloop.f90
@@ -219,27 +219,27 @@
 !
       mn_rlm = nidx_rtm(3) - mp_rlm + 1
 !
-      do lt = 1, nlo_rtm
-        lp_rtm = lst_rtm + lt
-        ln_rtm = nidx_rtm(2) - lp_rtm + 1
-        wp_rtm =   weight_rtm(lp_rtm)
-        asin_rtm = asin_theta_1d_rtm(lp_rtm)
-        do k_rlm = 1, nidx_rlm(1)
+      do nd = 1, nvector
+        do lt = 1, nlo_rtm
+          lp_rtm = lst_rtm + lt
+          ln_rtm = nidx_rtm(2) - lp_rtm + 1
+          wp_rtm =   weight_rtm(lp_rtm)
+          asin_rtm = asin_theta_1d_rtm(lp_rtm)
+          do k_rlm = 1, nidx_rlm(1)
 !
-          ip_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                       &
+            ip_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                     &
      &                + (k_rlm-1) *  istep_rtm(1)                       &
      &                + (mp_rlm-1) * istep_rtm(3)
-          ip_rtnm = 1 + (ln_rtm-1) * istep_rtm(2)                       &
+            ip_rtnm = 1 + (ln_rtm-1) * istep_rtm(2)                     &
      &                + (k_rlm-1) *  istep_rtm(1)                       &
      &                + (mp_rlm-1) * istep_rtm(3)
-          in_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                       &
+            in_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                     &
      &                + (k_rlm-1) *  istep_rtm(1)                       &
      &                + (mn_rlm-1) * istep_rtm(3)
-          in_rtnm = 1 + (ln_rtm-1) * istep_rtm(2)                       &
+            in_rtnm = 1 + (ln_rtm-1) * istep_rtm(2)                     &
      &                + (k_rlm-1) *  istep_rtm(1)                       &
      &                + (mn_rlm-1) * istep_rtm(3)
 !
-          do nd = 1, nvector
             ipp_recv = 3*nd + (irev_sr_rtm(ip_rtpm) - 1) * ncomp_recv
             ipn_recv = 3*nd + (irev_sr_rtm(ip_rtnm) - 1) * ncomp_recv
             inp_recv = 3*nd + (irev_sr_rtm(in_rtpm) - 1) * ncomp_recv
@@ -273,20 +273,20 @@
       end do
 !
 !   Equator (if necessary)
-      do lt = nlo_rtm+1, nle_rtm
-        lp_rtm = lst_rtm + lt
-        wp_rtm = weight_rtm(lp_rtm)
-        asin_rtm = asin_theta_1d_rtm(lp_rtm)
-        do k_rlm = 1, nidx_rlm(1)
+      do nd = 1, nvector
+        do lt = nlo_rtm+1, nle_rtm
+          lp_rtm = lst_rtm + lt
+          wp_rtm = weight_rtm(lp_rtm)
+          asin_rtm = asin_theta_1d_rtm(lp_rtm)
+          do k_rlm = 1, nidx_rlm(1)
 !
-          ip_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                       &
+            ip_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                     &
      &                + (k_rlm-1) *  istep_rtm(1)                       &
      &                + (mp_rlm-1) * istep_rtm(3)
-          in_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                       &
+            in_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                     &
      &                + (k_rlm-1) *  istep_rtm(1)                       &
      &                + (mn_rlm-1) * istep_rtm(3)
 !
-          do nd = 1, nvector
             ipp_recv = 3*nd + (irev_sr_rtm(ip_rtpm) - 1) * ncomp_recv
             inp_recv = 3*nd + (irev_sr_rtm(in_rtpm) - 1) * ncomp_recv
 !
@@ -310,20 +310,20 @@
       end do
 !
 !
-      do lt = 1, nlo_rtm
-        lp_rtm = lst_rtm + lt
-        ln_rtm = nidx_rtm(2) - lp_rtm + 1
-        wp_rtm = weight_rtm(lp_rtm)
+      do nd = 1, nscalar
         do k_rlm = 1, nidx_rlm(1)
+          do lt = 1, nlo_rtm
+            lp_rtm = lst_rtm + lt
+            ln_rtm = nidx_rtm(2) - lp_rtm + 1
+            wp_rtm = weight_rtm(lp_rtm)
 !
-          ip_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                       &
+            ip_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                     &
      &                + (k_rlm-1) *  istep_rtm(1)                       &
      &                + (mp_rlm-1) * istep_rtm(3)
-          ip_rtnm = 1 + (ln_rtm-1) * istep_rtm(2)                       &
+            ip_rtnm = 1 + (ln_rtm-1) * istep_rtm(2)                     &
      &                + (k_rlm-1) *  istep_rtm(1)                       &
      &                + (mp_rlm-1) * istep_rtm(3)
 !
-          do nd = 1, nscalar
             ipp_recv = nd + 3*nvector                                   &
      &                  + (irev_sr_rtm(ip_rtpm) - 1) * ncomp_recv
             ipn_recv = nd + 3*nvector                                   &
@@ -338,15 +338,15 @@
       end do
 !
 !   Equator (if necessary)
-      do lt = nlo_rtm+1, nle_rtm
-        lp_rtm = lst_rtm + lt
-        wp_rtm = weight_rtm(lp_rtm)
+      do nd = 1, nscalar
         do k_rlm = 1, nidx_rlm(1)
-          ip_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                       &
+          do lt = nlo_rtm+1, nle_rtm
+            lp_rtm = lst_rtm + lt
+            wp_rtm = weight_rtm(lp_rtm)
+              ip_rtpm = 1 + (lp_rtm-1) * istep_rtm(2)                   &
      &                + (k_rlm-1) *  istep_rtm(1)                       &
      &                + (mp_rlm-1) * istep_rtm(3)
 !
-          do nd = 1, nscalar
             ipp_recv = nd + 3*nvector                                   &
      &                    + (irev_sr_rtm(ip_rtpm) - 1) * ncomp_recv
 !
