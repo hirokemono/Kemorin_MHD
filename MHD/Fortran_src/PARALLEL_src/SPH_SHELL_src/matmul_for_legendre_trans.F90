2e994ad70ce7a3baa503dabd9b8a5ad9dc4a0a3d
Hiroaki Matsui <h_kemono@mac.com>
2019-07-16 14:57:03 -0700

    Try again

--- MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/matmul_for_legendre_trans.F90
+++ MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/matmul_for_legendre_trans.F90
@@ -171,11 +171,10 @@
       if(nkr .eq. 0) return
       if(n_jk .eq. 0) then
         V_kl = 0.0d0
-      else
-!      else if(iflag_matmul .eq. iflag_INTRINSIC) then
+      else if(iflag_matmul .eq. iflag_INTRINSIC .or. &
+     &        iflag_matmul .eq. iflag_DGEMM) then
         V_kl = matmul(S_kj,P_jl)
         return
-      end if
 !
 #ifdef BLAS
       else if(iflag_matmul .eq. iflag_DGEMM) then
