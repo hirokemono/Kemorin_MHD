8766a8810913c42259121fcb0909f18ad83499d3
Hiroaki Matsui <h_kemono@mac.com>
2019-07-22 15:41:54 -0600

    Add functions to construct patch buffer for PSF

---
+++ MHD/C_src/KEMO_GL/psf_patch_buffer.c
@@ -0,0 +1,8 @@
+//
+//  psf_patch_buffer.c
+//  Kemoview_Cocoa
+//
+//  Created by Hiroaki Matsui on 2019/07/22.
+//
+
+#include "psf_patch_buffer.h"
---
+++ MHD/C_src/KEMO_GL/psf_patch_buffer.h
@@ -0,0 +1,13 @@
+//
+//  psf_patch_buffer.h
+//  Kemoview_Cocoa
+//
+//  Created by Hiroaki Matsui on 2019/07/22.
+//
+
+#ifndef psf_patch_buffer_h
+#define psf_patch_buffer_h
+
+#include <stdio.h>
+
+#endif /* psf_patch_buffer_h */
