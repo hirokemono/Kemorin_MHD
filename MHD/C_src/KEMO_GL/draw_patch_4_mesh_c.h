df57db1580cf9950f33652068709973feae91abb
Hiroaki Matsui <h_kemono@mac.com>
2019-08-17 12:48:25 -0700

    Delete old routine

--- MHD/C_src/KEMO_GL/draw_patch_4_mesh_c.h
+++ MHD/C_src/KEMO_GL/draw_patch_4_mesh_c.h
@@ -14,14 +14,6 @@
 #include "sort_by_patch_distance.h"
 
 /* prototypes */
-void mesh_patch_VBO(struct view_element *view_s, int shading_mode, int polygon_mode, int surface_color,
-			int color_mode, int color_loop, double opacity, GLfloat single_color[4], 
-			int num_grp, int *istack_grp, int *item_grp, 
-			double **normal_ele, double **normal_nod, int *isort_grp, 
-			int *ip_domain_far, int igrp, struct viewer_mesh *mesh_s, int *iflag_domain, 
-			struct VAO_ids *mesh_VAO, struct kemoview_shaders *kemo_shaders, 
-			struct gl_strided_buffer *mesh_buf);
-
 void draw_solid_mesh_VAO(struct viewer_mesh *mesh_s, struct mesh_menu_val *mesh_m,
 			struct view_element *view_s, struct VAO_ids *mesh_VAO, 
 			struct kemoview_shaders *kemo_shaders, struct gl_strided_buffer *mesh_buf);
