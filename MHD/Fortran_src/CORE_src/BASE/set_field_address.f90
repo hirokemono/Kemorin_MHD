1b97b51e180dd3e5173019cd398f2a82c89d0a36
Hiroaki Matsui <h_kemono@mac.com>
2020-03-09 15:37:52 -0700

    Use set_field_product_addresses

--- MHD/Fortran_src/CORE_src/BASE/set_field_address.f90
+++ MHD/Fortran_src/CORE_src/BASE/set_field_address.f90
@@ -46,6 +46,7 @@
       use m_phys_labels
       use t_base_force_labels
       use t_diff_vector_labels
+      use t_field_product_labels
       use t_SGS_term_labels
       use t_SGS_enegy_flux_labels
       use t_SGS_model_coef_labels
@@ -95,10 +96,6 @@
           iphys%i_scalar_p = i0
         else if ( field_name(i) .eq. fhd_current ) then
           iphys%i_current = i0
-        else if ( field_name(i) .eq. electric_field%name ) then
-          iphys%prod_fld%i_electric = i0
-        else if ( field_name(i) .eq. poynting_flux%name ) then
-          iphys%prod_fld%i_poynting = i0
         else if ( field_name(i) .eq. fhd_entropy ) then
           iphys%i_entropy = i0
         else if ( field_name(i) .eq. fhd_per_entropy ) then
@@ -137,24 +134,12 @@
         else if ( field_name(i) .eq. fhd_filter_comp ) then
           iphys%i_filter_comp =    i0
 !
-        else if ( field_name(i) .eq. truncated_magnetic_field%name) then
-          iphys%prod_fld%i_truncated_B =    i0
-!
         else if ( field_name(i) .eq. div_filtered_velo%name ) then
           iphys%i_div_filter_v =    i0
         else if ( field_name(i) .eq. div_filtered_magne%name ) then
           iphys%i_div_filter_b =    i0
         else if ( field_name(i) .eq. div_filtered_vector_potential%name ) then
           iphys%i_div_filter_a =    i0
-!
-        else if ( field_name(i) .eq. kinetic_helicity%name ) then
-          iphys%prod_fld%i_k_heli = i0
-        else if ( field_name(i) .eq. magnetic_helicity%name ) then
-          iphys%prod_fld%i_m_heli = i0
-        else if ( field_name(i) .eq. current_helicity%name ) then
-          iphys%prod_fld%i_c_heli = i0
-        else if ( field_name(i) .eq. cross_helicity%name ) then
-          iphys%prod_fld%i_x_heli = i0
 !
         else if (field_name(i) .eq. fhd_thermal_diffusion) then
           iphys%i_t_diffuse =  i0
@@ -176,6 +161,8 @@
      &     (i0, field_name(i), iphys%ene_flux, flag)
         call set_div_force_addresses                                    &
      &     (i0, field_name(i), iphys%div_forces, flag)
+        call set_field_product_addresses                                &
+     &     (i0, field_name(i), iphys%prod_fld, flag)
 !
         call set_SGS_term_addresses                                     &
      &     (i0, field_name(i), iphys%SGS_term, flag)
@@ -198,9 +185,7 @@
         call set_force_w_SGS_addresses                                  &
      &     (i0, field_name(i), iphys%frc_w_SGS, flag)
 !
-        if ( field_name(i) .eq. rest_of_geostrophic%name ) then
-          iphys%prod_fld%i_geostrophic =  i0
-        else if ( field_name(i) .eq. geostrophic_balance%name ) then
+        if ( field_name(i) .eq. geostrophic_balance%name ) then
           iphys%prod_fld%i_geostrophic =  i0
         end if
 !
@@ -223,22 +208,6 @@
      &     (i0, field_name(i), iphys%grad_fld, flag)
         call set_diff_vector_addresses                                  &
      &     (i0, field_name(i), iphys%diff_vector, flag)
-!
-        if      ( field_name(i) .eq. square_velocity%name ) then
-          iphys%prod_fld%i_square_v = i0
-        else if ( field_name(i) .eq. square_vorticity%name ) then
-          iphys%prod_fld%i_square_w = i0
-        else if ( field_name(i) .eq. square_magne%name ) then
-          iphys%prod_fld%i_square_b = i0
-        else if ( field_name(i) .eq. square_vector_potential%name) then
-          iphys%prod_fld%i_square_a = i0
-        else if ( field_name(i) .eq. square_current%name ) then
-          iphys%prod_fld%i_square_j = i0
-        else if ( field_name(i) .eq. square_temperature%name ) then
-          iphys%prod_fld%i_square_t = i0
-        else if ( field_name(i) .eq. square_composition%name ) then
-          iphys%prod_fld%i_square_c = i0
-        end if
 !
         call set_grad_filter_field_addresses                            &
      &     (i0, field_name(i), iphys%grad_fil_fld, flag)
@@ -321,16 +290,6 @@
           iphys%i_chk_press_2 =     i0
         else if ( field_name(i) .eq. fhd_chk_potential_2 ) then
           iphys%i_chk_potential_2 = i0
-        end if
-!
-        if ( field_name(i) .eq. velocity_scale%name) then
-          iphys%prod_fld%i_velo_scale =  i0
-        else if ( field_name(i) .eq. magnetic_scale%name) then
-          iphys%prod_fld%i_magne_scale = i0
-        else if ( field_name(i) .eq. temperature_scale%name) then
-          iphys%prod_fld%i_temp_scale =  i0
-        else if ( field_name(i) .eq. composition_scale%name) then
-          iphys%prod_fld%i_comp_scale =  i0
         end if
 !
 !   Old field label... Should be deleted later!!
