24e1deee06be7beec3f5840cfe311d45f83eadd8
Hiroaki Matsui <h_kemono@mac.com>
2020-03-19 12:59:11 -0700

    fix module reference

--- MHD/Fortran_src/MHD_src/evolution/cal_light_element.f90
+++ MHD/Fortran_src/MHD_src/evolution/cal_light_element.f90
@@ -67,7 +67,7 @@
       use t_filter_elength
       use t_filtering_data
       use t_bc_data_temp
-      use t_surface_bc_data
+      use t_surface_bc_scalar
       use t_material_property
       use t_SGS_model_coefs
       use t_solver_djds_MHD
