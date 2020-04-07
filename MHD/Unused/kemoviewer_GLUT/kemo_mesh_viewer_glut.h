986df7e1ddfe323a650209464f8285997eeee54e
Hiroaki Matsui <h_kemono@mac.com>
2020-04-07 09:55:04 -0700

    Delete old Fortran name mangling from Makefiles

--- MHD/Unused/kemoviewer_GLUT/kemo_mesh_viewer_glut.h
+++ MHD/Unused/kemoviewer_GLUT/kemo_mesh_viewer_glut.h
@@ -16,17 +16,6 @@
 #include "const_viewer_menu_glut.h"
 #include "kemoview_glut_console_input.h"
 
-
-#ifdef FC_NAME_LOWER_USCORE
-#define draw_mesh_kemo     draw_mesh_kemo_
-#elif  FC_NAME_UPPER
-#define draw_mesh_kemo     DRAW_MESH_KEMO
-#elif FC_NAME_UPPER_STDCALL
-#define draw_mesh_kemo     DRAW_MESH_KEMO
-#else
-#define draw_mesh_kemo     draw_mesh_kemo
-#endif 
-
 /* prototypes */
 
 void set_window_id_glut3(int *id3_window, int *id3_menu);
