1824302c413ef5890777c8dd8383b53da6ff78ae
Hiroaki Matsui <h_kemono@mac.com>
2020-05-20 09:39:11 -0700

    Add backward transform for trns_snap

--- MHD/Fortran_src/MHD_src/sph_MHD/lead_fields_4_sph_mhd.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/lead_fields_4_sph_mhd.f90
@@ -121,7 +121,8 @@
       end if
 !
       call lead_fields_by_sph_trans(SPH_MHD%sph, SPH_MHD%comms,         &
-     &    MHD_prop, trans_p, WK%trns_MHD, SPH_MHD%fld)
+     &    MHD_prop, trans_p, WK%trns_MHD, WK%trns_snap, WK%WK_sph,      &
+     &    SPH_MHD%fld)
 !
       call gradients_of_vectors_sph                                     &
      &   (SPH_MHD%sph, SPH_MHD%comms, r_2nd, sph_MHD_bc, trans_p,       &
@@ -137,10 +138,11 @@
 !
 ! ----------------------------------------------------------------------
 !
-      subroutine lead_fields_by_sph_trans                               &
-     &         (sph, comms_sph, MHD_prop, trans_p, trns_MHD, rj_fld)
+      subroutine lead_fields_by_sph_trans(sph, comms_sph, MHD_prop,     &
+     &          trans_p, trns_MHD, trns_snap, WK_sph, rj_fld)
 !
       use sph_transforms_4_MHD
+      use sph_transforms_snapshot
       use cal_energy_flux_rtp
 !
       type(sph_grids), intent(in) :: sph
@@ -149,24 +151,24 @@
       type(parameters_4_sph_trans), intent(in) :: trans_p
 !
       type(address_4_sph_trans), intent(inout) :: trns_MHD
+      type(address_4_sph_trans), intent(inout) :: trns_snap
+      type(spherical_trns_works), intent(inout) :: WK_sph
       type(phys_data), intent(inout) :: rj_fld
 !
 !
-!      if(iflag_debug.eq.1) write(*,*)                                  &
-!     &        'nonlinear_by_pseudo_sph for snapshot'
-!      call nonlinear_by_pseudo_sph                                     &
-!     &   (sph, comms_sph, omega_sph, r_2nd, MHD_prop,                  &
-!     &    sph_MHD_bc, trans_p, gt_cor, trns_snap, WK_sph,              &
-!     &    cor_rlm, ipol, rj_fld)
+      if(iflag_debug.gt.0) write(*,*) 'sph_back_trans_snapshot_MHD'
+      call sph_back_trans_snapshot_MHD(sph, comms_sph, trans_p,         &
+     &    rj_fld, trns_snap%backward, WK_sph)
+!
 !
       if    (sph%sph_params%iflag_shell_mode .eq. iflag_MESH_w_pole     &
      &  .or. sph%sph_params%iflag_shell_mode .eq. iflag_MESH_w_center)  &
      &      then
-        if (iflag_debug.eq.1) write(*,*) 'sph_pole_trans_4_MHD'
+        if (iflag_debug.gt.0) write(*,*) 'sph_pole_trans_4_MHD'
         call sph_pole_trans_4_MHD                                       &
      &     (sph, comms_sph, trans_p, rj_fld, trns_MHD%backward)
 !
-        if (iflag_debug.eq.1) write(*,*) 'cal_nonlinear_pole_MHD'
+        if (iflag_debug.gt.0) write(*,*) 'cal_nonlinear_pole_MHD'
         call cal_nonlinear_pole_MHD(sph%sph_rtp, MHD_prop,              &
      &      trns_MHD%f_trns, trns_MHD%b_trns,                           &
      &      trns_MHD%backward, trns_MHD%forward)
@@ -197,7 +199,7 @@
       type(phys_data), intent(inout) :: rj_fld
 !
 !
-      if (iflag_debug.eq.1) write(*,*) 'cal_div_of_forces_sph_2'
+      if (iflag_debug.gt.0) write(*,*) 'cal_div_of_forces_sph_2'
       call cal_div_of_forces_sph_2                                      &
      &   (sph%sph_rj, r_2nd, MHD_prop, sph_MHD_bc, leg%g_sph_rj,        &
      &    ipol%forces, ipol%div_forces, rj_fld)
@@ -212,12 +214,12 @@
       call sum_div_of_forces                                            &
      &    (MHD_prop%fl_prop, ipol%base, ipol%div_forces, rj_fld)
 !
-      if (iflag_debug.eq.1) write(*,*) 'cal_sol_pressure_by_div_v'
+      if (iflag_debug.gt.0) write(*,*) 'cal_sol_pressure_by_div_v'
       call cal_sol_pressure_by_div_v(sph%sph_rj, sph_MHD_bc%sph_bc_U,   &
      &    band_p_poisson, ipol, rj_fld)
 !
       if(ipol%forces%i_press_grad .gt. 0) then
-        if (iflag_debug.eq.1) write(*,*) 'const_pressure_gradient'
+        if (iflag_debug.gt.0) write(*,*) 'const_pressure_gradient'
         call const_pressure_gradient                                    &
      &     (sph%sph_rj, r_2nd, sph_MHD_bc%sph_bc_U,                     &
      &      leg%g_sph_rj, MHD_prop%fl_prop%coef_press,                  &
@@ -255,7 +257,7 @@
      &   (ltr_crust, sph, comms_sph, r_2nd, MHD_prop, sph_MHD_bc,       &
      &    trans_p, ipol, trns_MHD, trns_eflux,  WK_sph, rj_fld)
 !
-      if (iflag_debug.eq.1) write(*,*)                                  &
+      if (iflag_debug.gt.0) write(*,*)                                  &
      &                          'sph_forward_trans_snapshot_MHD'
       call sph_forward_trans_snapshot_MHD                               &
      &   (sph, comms_sph, trans_p, trns_eflux%forward, WK_sph, rj_fld)
@@ -288,16 +290,16 @@
       type(phys_data), intent(inout) :: rj_fld
 !
 !      Evaluate fields for output in spectrum space
-      if (iflag_debug.eq.1) write(*,*) 's_cal_energy_flux_rj'
+      if (iflag_debug.gt.0) write(*,*) 's_cal_energy_flux_rj'
       call s_cal_energy_flux_rj                                         &
      &   (ltr_crust, sph%sph_rj, r_2nd, sph_MHD_bc, ipol, rj_fld)
 !
-      if (iflag_debug.eq.1) write(*,*) 'sph_back_trans_snapshot_MHD'
+      if (iflag_debug.gt.0) write(*,*) 'sph_back_trans_snapshot_MHD'
       call sph_back_trans_snapshot_MHD(sph, comms_sph, trans_p,         &
      &    rj_fld, trns_eflux%backward, WK_sph)
 !
 !       Evaluate energy fluxes
-      if (iflag_debug.eq.1) write(*,*) 's_cal_energy_flux_rtp'
+      if (iflag_debug.gt.0) write(*,*) 's_cal_energy_flux_rtp'
       call s_cal_energy_flux_rtp                                        &
      &   (sph%sph_rtp, MHD_prop%fl_prop, MHD_prop%cd_prop,              &
      &    MHD_prop%ref_param_T, MHD_prop%ref_param_C, trans_p%leg,      &
@@ -328,16 +330,16 @@
       type(phys_data), intent(inout) :: rj_fld
 !
 !
-      if (iflag_debug.eq.1) write(*,*) 'copy_vectors_rtp_4_grad'
+      if (iflag_debug.gt.0) write(*,*) 'copy_vectors_rtp_4_grad'
       call copy_vectors_rtp_4_grad                                      &
      &   (sph, trns_MHD%b_trns, trns_tmp%f_trns,                        &
      &    trns_MHD%backward, trns_tmp%forward)
 !
-      if (iflag_debug.eq.1) write(*,*) 'sph_forward_trans_snapshot_MHD'
+      if (iflag_debug.gt.0) write(*,*) 'sph_forward_trans_snapshot_MHD'
       call sph_forward_trans_snapshot_MHD                               &
      &   (sph, comms_sph, trans_p, trns_tmp%forward, WK_sph, rj_fld)
 !
-      if (iflag_debug.eq.1) write(*,*) 'cal_grad_of_velocities_sph'
+      if (iflag_debug.gt.0) write(*,*) 'cal_grad_of_velocities_sph'
       call cal_grad_of_velocities_sph(sph%sph_rj, r_2nd,                &
      &    sph_MHD_bc%sph_bc_U, trans_p%leg%g_sph_rj, ipol, rj_fld)
 !
--- MHD/Fortran_src/MHD_src/sph_SGS_MHD/lead_fields_SPH_SGS_MHD.f90
+++ MHD/Fortran_src/MHD_src/sph_SGS_MHD/lead_fields_SPH_SGS_MHD.f90
@@ -94,7 +94,8 @@
 !
 !
       call lead_fields_by_sph_trans(SPH_MHD%sph, SPH_MHD%comms,         &
-     &    MHD_prop, trans_p, WK%trns_MHD, SPH_MHD%fld)
+     &    MHD_prop, trans_p, WK%trns_MHD, WK%trns_snap, WK%WK_sph,      &
+     &    SPH_MHD%fld)
       call gradients_of_vectors_sph                                     &
      &   (SPH_MHD%sph, SPH_MHD%comms, r_2nd, sph_MHD_bc, trans_p,       &
      &    SPH_MHD%ipol, WK%trns_MHD, WK%trns_tmp, WK%WK_sph,            &
