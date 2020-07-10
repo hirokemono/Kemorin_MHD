04a38200ff27446175c88b3ca4aba7b825d5afdd
Hiroaki Matsui <h_kemono@mac.com>
2020-07-10 13:05:10 -0700

    update elastic window for Calypsoview

--- MHD/C_src/Obj_C/ElasticMenuWindow.h
+++ MHD/C_src/Obj_C/ElasticMenuWindow.h
@@ -1,12 +1,22 @@
-//
+/*
 //  ElasticMenuWindow.h
 //  CalypsoView_Cocoa
 //
 //  Created by Hiroaki Matsui on 2020/07/10.
-//
+*/
 
 #ifndef ElasticMenuWindow_h
 #define ElasticMenuWindow_h
 
+@interface ElasticMenuWindowController : NSObject{
+    
+    IBOutlet NSView *_main_menu_view;
+    IBOutlet NSView *_PSF_menu_view;
+    IBOutlet NSWindow *_menu_window;
+}
+
+- (void) UpdateWindow:(NSInteger)DrawPsfFlag;
+
+@end
 
 #endif /* ElasticMenuWindow_h */
--- MHD/C_src/Obj_C/ElasticMenuWindow.m
+++ MHD/C_src/Obj_C/ElasticMenuWindow.m
@@ -5,4 +5,39 @@
 //  Created by Hiroaki Matsui on 2020/07/10.
 //
 
-#import <Foundation/Foundation.h>
+#import "ElasticMenuWindow.h"
+
+@implementation ElasticMenuWindowController
+    
+#define MARGIN_RIGHT 25
+
+- (void) UpdateWindow:(NSInteger)DrawPsfFlag
+{
+    NSRect window_frame = [_menu_window frame];
+    CGFloat mainMenuWidth = _main_menu_view.frame.size.width;
+    CGFloat PSFMenuWidth =  _PSF_menu_view.frame.size.width;
+
+    window_frame.size.width = mainMenuWidth + MARGIN_RIGHT
+                            + (PSFMenuWidth + MARGIN_RIGHT) * DrawPsfFlag;
+    [_menu_window setFrame:window_frame display:YES animate:YES];
+    printf("Size again: %d %d %d \n", (int) DrawPsfFlag, 
+           (int) window_frame.size.width, (int) window_frame.size.height);
+}
+
+- (void)awakeFromNib
+{
+    CGFloat mainMenuWidth = _main_menu_view.frame.size.width;
+    NSRect window_frame = [_menu_window frame];
+    window_frame.size.width = mainMenuWidth + MARGIN_RIGHT;
+    [_menu_window setFrame:window_frame display:YES animate:YES];
+    printf("Size: %e %e \n", window_frame.size.width, window_frame.size.height);
+//    [self update];
+}
+    
+    
+- (void)tabView:(NSTabView *)tabView didSelectTabViewItem:(NSView *)tabViewItem
+{
+//    [self update];    
+}
+    
+    @end
