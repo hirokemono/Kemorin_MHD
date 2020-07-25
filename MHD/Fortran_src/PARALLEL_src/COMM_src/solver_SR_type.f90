aae8e6e3f81887f5fe0c708ea0910839587fd53e
Hiroaki Matsui <h_kemono@mac.com>
2020-07-25 07:13:31 -0700

    Add wrapper for 3 vector send recv

--- MHD/Fortran_src/PARALLEL_src/COMM_src/solver_SR_type.f90
+++ MHD/Fortran_src/PARALLEL_src/COMM_src/solver_SR_type.f90
@@ -12,6 +12,11 @@
 !!      subroutine SOLVER_SEND_RECV_6_type(NP, comm_tbl, X)
 !!      subroutine SOLVER_SEND_RECV_N_type(NP, NB, comm_tbl, X)
 !!
+!!      subroutine SOLVER_SEND_RECV_1x3_type(NP, comm_tbl, X1, X2, X3)
+!!      subroutine SOLVER_SEND_RECV_3x3_type(NP, comm_tbl, X1, X2, X3)
+!!      subroutine SOLVER_SEND_RECV_Nx3_type                            &
+!!     &         (NP, NB, comm_tbl, X1, X2, X3)
+!!
 !!      subroutine SOLVER_SEND_RECV_int_type(NP, comm_tbl, iX)
 !!      subroutine SOLVER_SEND_RECV_int8_type(NP, comm_tbl, i8X)
 !!      subroutine SOLVER_SEND_RECV_num_type(comm_tbl, nSEND, nRECV)
@@ -140,6 +145,91 @@
       if(iflag_FSR_time) call end_elapsed_time(ist_elapsed_FSR+1)
 !
       end subroutine SOLVER_SEND_RECV_N_type
+!
+! ----------------------------------------------------------------------
+! ----------------------------------------------------------------------
+!
+      subroutine SOLVER_SEND_RECV_1x3_type(NP, comm_tbl, X1, X2, X3)
+!
+      use t_comm_table
+      use solver_SR
+      use m_solver_SR
+!
+      type(communication_table), intent(in) :: comm_tbl
+      integer(kind = kint), intent(in) :: NP
+!
+      real(kind = kreal), intent(inout) :: X1(NP)
+      real(kind = kreal), intent(inout) :: X2(NP)
+      real(kind = kreal), intent(inout) :: X3(NP)
+!
+!
+      if(iflag_FSR_time) call start_elapsed_time(ist_elapsed_FSR+1)
+      call SOLVER_SEND_RECVx3                                           &
+     &   (NP, comm_tbl%num_neib, comm_tbl%id_neib,                      &
+     &    comm_tbl%istack_import, comm_tbl%item_import,                 &
+     &    comm_tbl%istack_export, comm_tbl%item_export,                 &
+     &    SR_sig1, SR_r1, X1(1), X2(1), X3(1))
+      if(iflag_FSR_time) call end_elapsed_time(ist_elapsed_FSR+1)
+!
+      end subroutine SOLVER_SEND_RECV_1x3_type
+!
+! ----------------------------------------------------------------------
+!
+      subroutine SOLVER_SEND_RECV_3x3_type(NP, comm_tbl, X1, X2, X3)
+!
+      use t_comm_table
+      use solver_SR_3
+      use m_solver_SR
+!
+      type(communication_table), intent(in) :: comm_tbl
+      integer(kind = kint), intent(in) :: NP
+!
+      real(kind = kreal), intent(inout) :: X1(3*NP)
+      real(kind = kreal), intent(inout) :: X2(3*NP)
+      real(kind = kreal), intent(inout) :: X3(3*NP)
+!
+!
+      if(iflag_FSR_time) call start_elapsed_time(ist_elapsed_FSR+1)
+      call solver_send_recv_3x3                                         &
+     &   (NP, comm_tbl%num_neib, comm_tbl%id_neib,                      &
+     &    comm_tbl%istack_import, comm_tbl%item_import,                 &
+     &    comm_tbl%istack_export, comm_tbl%item_export,                 &
+     &    SR_sig1, SR_r1, X1(1), X2(1), X3(1))
+      if(iflag_FSR_time) call end_elapsed_time(ist_elapsed_FSR+1)
+!
+      end subroutine SOLVER_SEND_RECV_3x3_type
+!
+! ----------------------------------------------------------------------
+!
+      subroutine SOLVER_SEND_RECV_Nx3_type                              &
+     &         (NP, NB, comm_tbl, X1, X2, X3)
+!
+      use t_comm_table
+      use m_solver_SR
+      use solver_SR_N
+!
+      type(communication_table), intent(in) :: comm_tbl
+      integer(kind = kint), intent(in) :: NP, NB
+!
+      real(kind = kreal), intent(inout) :: X1(NB*NP)
+      real(kind = kreal), intent(inout) :: X2(NB*NP)
+      real(kind = kreal), intent(inout) :: X3(NB*NP)
+!
+!>      Structure of communication flags
+!      type(send_recv_status), intent(inout) :: SR_sig
+!>      Structure of communication buffer for 8-byte integer
+!      type(send_recv_real_buffer), intent(inout) :: SR_r
+!
+!
+      if(iflag_FSR_time) call start_elapsed_time(ist_elapsed_FSR+1)
+      call SOLVER_SEND_RECV_Nx3                                         &
+     &   (NP, NB, comm_tbl%num_neib, comm_tbl%id_neib,                  &
+     &   comm_tbl%istack_import, comm_tbl%item_import,                  &
+     &   comm_tbl%istack_export, comm_tbl%item_export,                  &
+     &   SR_sig1, SR_r1, X1(1), X2(1), X3(1))
+      if(iflag_FSR_time) call end_elapsed_time(ist_elapsed_FSR+1)
+!
+      end subroutine SOLVER_SEND_RECV_Nx3_type
 !
 ! ----------------------------------------------------------------------
 ! ----------------------------------------------------------------------
