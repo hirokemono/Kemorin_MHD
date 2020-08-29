29714d1d4e1ef5375feb6186e05dde423ddde8d0
Hiroaki Matsui <h_kemono@mac.com>
2020-08-29 13:31:01 -0700

    Fix temporal sph_rlm

--- MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/mpi_gen_sph_grids_modes.f90
+++ MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/mpi_gen_sph_grids_modes.f90
@@ -119,17 +119,16 @@
       if(my_rank .eq. 0) write(*,*) 'nidx_rj', sph%sph_rj%nidx_rj(:)
       call dealloc_sph_1d_index_rj(sph%sph_rj)
 !
-      call copy_gl_2_local_rlm_param                                    &
-     &   (my_rank, gen_sph%s3d_ranks, gen_sph%sph_lcp,                  &
-     &    gen_sph%stk_lc1d, sph%sph_rlm)
-      call alloc_sph_1d_index_rlm(sph%sph_rlm)
-      if(my_rank .eq. 0) write(*,*) 'nidx_rlm', sph%sph_rlm%nidx_rlm(:)
-      call dealloc_sph_1d_index_rlm(sph%sph_rlm)
-!
 !  ========= Generate each spherical harmonics table ===================
 !
       if(iflag_GSP_time) call start_elapsed_time(ist_elapsed_GSP+6)
       allocate(comm_rlm_mul(gen_sph%s3d_ranks%ndomain_sph))
+!
+      sph_rlm%nidx_rlm(1) = 17
+      sph_rlm%nidx_rlm(2) = 16384
+      call alloc_sph_1d_index_rlm(sph%sph_rlm)
+      if(my_rank .eq. 0) write(*,*) 'nidx_rlm', sph%sph_rlm%nidx_rlm(:)
+      call dealloc_sph_1d_index_rlm(sph%sph_rlm)
 !
       if(iflag_debug .gt. 0) write(*,*)                                 &
      &             'start rlm table generation for', my_rank
