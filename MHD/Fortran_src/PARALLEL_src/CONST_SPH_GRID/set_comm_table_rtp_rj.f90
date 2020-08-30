4a8d2508e15a273ac990c0becd61bad7333afff8
Hiroaki Matsui <h_kemono@mac.com>
2020-08-29 22:30:28 -0700

    fix debug write

--- MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/set_comm_table_rtp_rj.f90
+++ MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/set_comm_table_rtp_rj.f90
@@ -141,7 +141,8 @@
       call alloc_sph_1d_index_rj(sph_rj)
 !
       write(*,*) 's3d_radius%radius_1d_gl(j)', s3d_radius%radius_1d_gl(:)
-      write(*,*) 'sph_rj%idx_global_rj_r(j)', sph_rj%idx_global_rj_r(:)
+      write(*,*) 'sph_rj%radius_1d_rj_r(j)', sph_rj%radius_1d_rj_r(:)
+      write(*,*) 'sph_rj%idx_gl_1d_rj_r(j)', sph_rj%idx_gl_1d_rj_r(:)
       call copy_sph_1d_gl_idx_rj(s3d_radius, sph_gl1d, sph_rj)
 !
       if(iflag_debug .gt. 0) write(*,*)                                 &
