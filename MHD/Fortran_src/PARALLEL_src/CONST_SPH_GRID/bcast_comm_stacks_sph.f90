9f172212749438e6bcb05045e6a95600c8ffcf79
Hiroaki Matsui <h_kemono@mac.com>
2020-08-30 09:31:09 -0700

    skip beast

--- MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/bcast_comm_stacks_sph.f90
+++ MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/bcast_comm_stacks_sph.f90
@@ -177,10 +177,10 @@
      &       = comm_sph(ip)%istack_sr(0:comm_sph(ip)%nneib_domain)
         end if
 !
-        call calypso_mpi_bcast_int(comm_tmp%id_domain(1),               &
-     &      cast_long(comm_tmp%nneib_domain), iroot)
-        call calypso_mpi_bcast_int(comm_tmp%istack_sr(1),               &
-     &      cast_long(comm_tmp%nneib_domain), iroot)
+!        call calypso_mpi_bcast_int(comm_tmp%id_domain(1),              &
+!     &      cast_long(comm_tmp%nneib_domain), iroot)
+!        call calypso_mpi_bcast_int(comm_tmp%istack_sr(1),              &
+!     &      cast_long(comm_tmp%nneib_domain), iroot)
 !
         iflag = 0
         do i = 1, comm_tmp%nneib_domain
@@ -192,7 +192,6 @@
 !
         if(iflag .eq. 0) then
           comm_sph(ip)%nneib_domain = 0
-          call alloc_sph_comm_stack(comm_sph(ip))
         else if(iroot .ne. my_rank) then
 !          write(*,*) 'allocate rtm:', my_rank, ip
           comm_sph(ip)%nneib_domain = comm_tmp%nneib_domain
