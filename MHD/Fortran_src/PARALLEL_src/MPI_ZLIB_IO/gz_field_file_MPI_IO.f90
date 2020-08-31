67713e94b7b40995f239433de13dd53e75db8ba5
Hiroaki Matsui <h_kemono@mac.com>
2020-08-31 12:33:31 -0700

    Delete debug barrier

--- MHD/Fortran_src/PARALLEL_src/MPI_ZLIB_IO/gz_field_file_MPI_IO.f90
+++ MHD/Fortran_src/PARALLEL_src/MPI_ZLIB_IO/gz_field_file_MPI_IO.f90
@@ -175,8 +175,6 @@
      &   (id_fld, ioff_gl, fld_IO%num_field_IO, fld_IO%num_comp_IO)
 !
       call cal_istack_phys_comp_IO(fld_IO)
-      write(*,*) my_rank, num_pe, id_rank, 'fld_IO', fld_IO%nnod_IO, fld_IO%ntot_comp_IO
-      call calypso_mpi_barrier
       call alloc_phys_data_IO(fld_IO)
 !
       call read_field_data_gz_mpi                                       &
