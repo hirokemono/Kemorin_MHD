10314a6cb1c1315cb7b8e3f0a7245a4d057e1abc
Hiroaki Matsui <h_kemono@mac.com>
2020-08-31 11:36:22 -0700

    Add barrier

--- MHD/Fortran_src/PARALLEL_src/MPI_ZLIB_IO/gz_field_file_MPI_IO.f90
+++ MHD/Fortran_src/PARALLEL_src/MPI_ZLIB_IO/gz_field_file_MPI_IO.f90
@@ -175,6 +175,7 @@
      &   (id_fld, ioff_gl, fld_IO%num_field_IO, fld_IO%num_comp_IO)
 !
       call cal_istack_phys_comp_IO(fld_IO)
+      call calypso_mpi_barrier
       call alloc_phys_data_IO(fld_IO)
 !
       call read_field_data_gz_mpi                                       &
