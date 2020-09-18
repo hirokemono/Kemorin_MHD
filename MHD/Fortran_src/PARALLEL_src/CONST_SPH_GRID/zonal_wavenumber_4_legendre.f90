d4baeed26dd712f8b725077c2f6783b7ae99048c
Hiroaki Matsui <h_kemono@mac.com>
2020-09-18 12:48:59 -0700

    Add test mode routine for decomposition

--- MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/zonal_wavenumber_4_legendre.f90
+++ MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/zonal_wavenumber_4_legendre.f90
@@ -13,6 +13,9 @@
 !!     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
 !!      subroutine zonal_leg_wavenum_simple_dist(ndomain_m,             &
 !!     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
+!!
+!!      subroutine zonal_wavenum_list_test(ndomain_m,                   &
+!!     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
 !!@endverbatim
 !
       module zonal_wavenumber_4_legendre
@@ -24,6 +27,8 @@
       integer(kind = kint), parameter :: id_cyclic_eq_transform =   0
       integer(kind = kint), parameter :: id_cyclic_eq_mode =        1
       integer(kind = kint), parameter :: id_simple_rlm_distribute = 2
+!
+      integer(kind = kint), parameter :: id_test_distribute = 999
 !
 ! -----------------------------------------------------------------------
 !
@@ -220,6 +225,8 @@
       integer(kind = kint), allocatable :: ip_tmp(:)
 !
 !
+      write(*,*) 'test test'
+!
       allocate( ip_tmp(-ltr:ltr) )
       ip_tmp = 0
 !
@@ -288,6 +295,100 @@
 !
 !
       end subroutine zonal_leg_wavenum_simple_dist
+!
+! -----------------------------------------------------------------------
+!
+      subroutine zonal_wavenum_list_test(ndomain_m,                     &
+     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
+!
+      integer(kind = kint), intent(in) :: ltr, m_folding
+      integer(kind = kint), intent(in) :: nth, nph, ndomain_m
+      integer(kind = kint), intent(inout) :: jdx_fsph(-nth:nth)
+      integer(kind = kint), intent(inout) :: mdx_4_lgd(0:nph)
+!
+      integer(kind = kint) :: m, ip, mm, imark, ltr_half
+      integer(kind = kint), allocatable :: ip_tmp(:)
+!
+!
+      allocate( ip_tmp(-ltr:ltr) )
+      ip_tmp = 0
+!
+      ip = 1
+      imark = 1
+      ltr_half = ( ltr-mod(ltr,2) ) / (2*m_folding)
+!
+      do m = 1, ltr_half
+        ip_tmp(m) = ip
+        ip = ip + imark
+        if (ip .gt. ndomain_m) then
+          ip =    ndomain_m
+          imark = -1
+        else if (ip .lt. 1) then
+          ip = 1
+          imark =  1
+        end if
+      end do
+!
+      m = 0
+      ip_tmp(m) = ip
+      ip = ip + imark
+      if (ip .gt. ndomain_m) then
+        ip =    ndomain_m
+        imark = -1
+      else if (ip .lt. 1) then
+        ip = 1
+        imark =  1
+      end if
+!
+      do m = ltr_half+1, ltr/m_folding
+        ip_tmp(m) = ip
+        ip = ip + imark
+        if (ip .gt. ndomain_m) then
+          ip =    ndomain_m
+          imark = -1
+        else if (ip .lt. 1) then
+          ip = 1
+          imark =  1
+        end if
+      end do
+!
+      do m = 1, ltr
+        ip_tmp(-m) = ip_tmp(m)
+      end do
+!
+!        write(8,*) 'm, ip_tmp(m)'
+!      do m = -ltr, ltr
+!        write(8,*) m, ip_tmp(m)
+!      end do
+!
+      mm = -1
+      do ip = 1, ndomain_m
+        do m = -ltr/m_folding, ltr/m_folding
+          if (ip_tmp(m) .eq. ip) then
+            mm = mm + 1
+            jdx_fsph(m) = mm
+          end if
+        end do
+      end do
+!
+      do m = -ltr/m_folding, ltr/m_folding
+        mm = jdx_fsph(m)
+        mdx_4_lgd(mm) = m
+      end do
+!
+      deallocate( ip_tmp )
+!
+!        write(*,*) 'm, jdx_fsph(m)'
+!      do m = -ltr, ltr
+!        write(*,*) m, jdx_fsph(m)
+!      end do
+!        write(*,*) 'mm, mdx_4_lgd(mm)'
+!      do mm = 0,nph
+!        write(*,*) mm, mdx_4_lgd(mm)
+!      end do
+!
+!
+      end subroutine zonal_wavenum_list_test
 !
 ! -----------------------------------------------------------------------
 !
