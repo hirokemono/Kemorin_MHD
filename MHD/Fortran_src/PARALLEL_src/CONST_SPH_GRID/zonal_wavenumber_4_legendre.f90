a007ed59e953c1777440d67fed1fef8eb9cd041f
Hiroaki Matsui <h_kemono@mac.com>
2020-09-19 23:43:12 -0700

    Add new module for order ordering selector

--- MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/zonal_wavenumber_4_legendre.f90
+++ MHD/Fortran_src/PARALLEL_src/CONST_SPH_GRID/zonal_wavenumber_4_legendre.f90
@@ -7,15 +7,15 @@
 !>@brief  Set order of spheherical harmonics modes
 !!
 !!@verbatim
-!!      subroutine zonal_wavenum_eq_leg_trns(ndomain_m,                 &
-!!     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
-!!      subroutine zonal_wavenum_eq_leg_modes(ndomain_m,                &
-!!     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
-!!      subroutine zonal_leg_wavenum_simple_dist(ndomain_m,             &
-!!     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
+!!      subroutine set_domain_by_eq_leg_trns                            &
+!!     &         (ndomain_m, ltr, m_folding, ip_tmp)
+!!      subroutine set_domain_by_eq_leg_modes                           &
+!!     &         (ndomain_m, ltr, m_folding, ip_tmp)
 !!
-!!      subroutine zonal_wavenum_list_test(ndomain_m,                   &
-!!     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
+!!      subroutine set_local_sph_back_order(ndomain_m, ltr, m_folding,  &
+!!     &          nth, nph, ip_tmp, jdx_fsph, mdx_4_lgd)
+!!      subroutine set_local_sph_neib_order(ndomain_m, ltr, m_folding,  &
+!!     &          nth, nph, ip_tmp, jdx_fsph, mdx_4_lgd)
 !!@endverbatim
 !
       module zonal_wavenumber_4_legendre
@@ -24,32 +24,22 @@
 !
       implicit none
 !
-      integer(kind = kint), parameter :: id_cyclic_eq_transform =   0
-      integer(kind = kint), parameter :: id_cyclic_eq_mode =        1
-      integer(kind = kint), parameter :: id_simple_rlm_distribute = 2
-!
-      integer(kind = kint), parameter :: id_test_distribute = 999
-!
 ! -----------------------------------------------------------------------
 !
       contains
 !
 ! -----------------------------------------------------------------------
 !
-      subroutine zonal_wavenum_eq_leg_trns(ndomain_m,                   &
-     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
+      subroutine set_domain_by_eq_leg_trns                              &
+     &         (ndomain_m, ltr, m_folding, ip_tmp)
 !
       integer(kind = kint), intent(in) :: ltr, m_folding
-      integer(kind = kint), intent(in) :: nth, nph, ndomain_m
-      integer(kind = kint), intent(inout) :: jdx_fsph(-nth:nth)
-      integer(kind = kint), intent(inout) :: mdx_4_lgd(0:nph)
+      integer(kind = kint), intent(in) :: ndomain_m
 !
-      integer(kind = kint) :: m, ip, ma, mm, imark, ltr_half
-      integer(kind = kint), allocatable :: ip_tmp(:)
+      integer(kind = kint), intent(inout) :: ip_tmp(0:ltr)
 !
+      integer(kind = kint) :: m, ip, imark, ltr_half
 !
-      allocate( ip_tmp(0:ltr) )
-      ip_tmp = 0
 !
       ip = 1
       imark = 1
@@ -79,42 +69,20 @@
         end if
       end do
 !
-      mm = 0
-      do ip = 1, ndomain_m
-        do m = ltr/m_folding, -ltr/m_folding, -1
-          ma = abs(m)
-          if (ip_tmp(ma) .eq. ip) then
-            jdx_fsph(m) = mm
-            mm = mm + 1
-          end if
-        end do
-      end do
-!
-      do m = -ltr/m_folding, ltr/m_folding
-        mm = jdx_fsph(m)
-        mdx_4_lgd(mm) = m
-      end do
-!
-      deallocate( ip_tmp )
-!
-      end subroutine zonal_wavenum_eq_leg_trns
+      end subroutine set_domain_by_eq_leg_trns
 !
 ! -----------------------------------------------------------------------
 !
-      subroutine zonal_wavenum_eq_leg_modes(ndomain_m,                  &
-     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
+      subroutine set_domain_by_eq_leg_modes                             &
+     &         (ndomain_m, ltr, m_folding, ip_tmp)
 !
       integer(kind = kint), intent(in) :: ltr, m_folding
-      integer(kind = kint), intent(in) :: nth, nph, ndomain_m
-      integer(kind = kint), intent(inout) :: jdx_fsph(-nth:nth)
-      integer(kind = kint), intent(inout) :: mdx_4_lgd(0:nph)
+      integer(kind = kint), intent(in) :: ndomain_m
 !
-      integer(kind = kint) :: m, ip, ma, mm, imark, ltr_half
-      integer(kind = kint), allocatable :: ip_tmp(:)
+      integer(kind = kint), intent(inout) :: ip_tmp(0:ltr)
 !
+      integer(kind = kint) :: m, ip, imark, ltr_half
 !
-      allocate( ip_tmp(0:ltr) )
-      ip_tmp = 0
 !
       ip = 1
       imark = 1
@@ -155,6 +123,24 @@
         end if
       end do
 !
+      end subroutine set_domain_by_eq_leg_modes
+!
+! -----------------------------------------------------------------------
+! -----------------------------------------------------------------------
+!
+      subroutine set_local_sph_back_order(ndomain_m, ltr, m_folding,    &
+     &          nth, nph, ip_tmp, jdx_fsph, mdx_4_lgd)
+!
+      integer(kind = kint), intent(in) :: ltr, m_folding
+      integer(kind = kint), intent(in) :: nth, nph, ndomain_m
+      integer(kind = kint), intent(in) :: ip_tmp(0:ltr)
+!
+      integer(kind = kint), intent(inout) :: jdx_fsph(-nth:nth)
+      integer(kind = kint), intent(inout) :: mdx_4_lgd(0:nph)
+!
+      integer(kind = kint) :: m, ip, ma, mm
+!
+!
       mm = 0
       do ip = 1, ndomain_m
         do m = ltr/m_folding, -ltr/m_folding, -1
@@ -171,64 +157,37 @@
         mdx_4_lgd(mm) = m
       end do
 !
-      deallocate( ip_tmp )
-!
-      end subroutine zonal_wavenum_eq_leg_modes
+      end subroutine set_local_sph_back_order
 !
 ! -----------------------------------------------------------------------
 !
-      subroutine zonal_leg_wavenum_simple_dist(ndomain_m,               &
-     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
+      subroutine set_local_sph_neib_order(ndomain_m, ltr, m_folding,    &
+     &          nth, nph, ip_tmp, jdx_fsph, mdx_4_lgd)
 !
       integer(kind = kint), intent(in) :: ltr, m_folding
       integer(kind = kint), intent(in) :: nth, nph, ndomain_m
+      integer(kind = kint), intent(in) :: ip_tmp(0:ltr)
+!
       integer(kind = kint), intent(inout) :: jdx_fsph(-nth:nth)
       integer(kind = kint), intent(inout) :: mdx_4_lgd(0:nph)
 !
-      integer(kind = kint) :: m, ip, ma, mm, imark, ltr_half
-      integer(kind = kint), allocatable :: ip_tmp(:)
+      integer(kind = kint) :: m, ip, mm
 !
-!
-      allocate( ip_tmp(0:ltr) )
-      ip_tmp = 0
-!
-      ip = 1
-      imark = 1
-      ltr_half = ( ltr-mod(ltr,2) ) / (2*m_folding)
-!
-      do m = 0, ltr_half
-        ip_tmp(m) = ip
-        ip = ip + imark
-        if (ip .gt. ndomain_m) then
-          ip =    ndomain_m
-          imark = -1
-        else if (ip .lt. 1) then
-          ip = 1
-          imark =  1
-        end if
-      end do
-!
-      do m = ltr_half+1, ltr/m_folding
-        ip_tmp(m) = ip
-        ip = ip + imark
-        if (ip .gt. ndomain_m) then
-          ip =    ndomain_m
-          imark = -1
-        else if (ip .lt. 1) then
-          ip = 1
-          imark =  1
-        end if
-      end do
 !
       mm = 0
       do ip = 1, ndomain_m
-        do m = -ltr/m_folding, ltr/m_folding
-          ma = abs(m)
-          if (ip_tmp(ma) .eq. ip) then
-            jdx_fsph(m) = mm
-            mm = mm + 1
+        do m = ltr/m_folding, 1, -1
+          if(ip_tmp(m) .eq. ip) then
+            jdx_fsph( m) = mm
+            jdx_fsph(-m) = mm+1
+            mm = mm + 2
           end if
         end do
+!
+        if(ip_tmp(0) .eq. ip) then
+          jdx_fsph( m) = mm
+          mm = mm + 1
+        end if
       end do
 !
       do m = -ltr/m_folding, ltr/m_folding
@@ -236,116 +195,7 @@
         mdx_4_lgd(mm) = m
       end do
 !
-!        write(8,*) 'm, ip_tmp(m)'
-!      do m = -ltr, ltr
-!        write(8,*) m, ip_tmp(m)
-!      end do
-!
-        write(*,*) 'm, jdx_fsph(m)'
-      do m = -ltr, ltr
-        write(*,*) m, jdx_fsph(m)
-      end do
-        write(*,*) 'mm, mdx_4_lgd(mm)'
-      do mm = 0,nph
-        write(*,*) mm, mdx_4_lgd(mm)
-      end do
-!
-      deallocate( ip_tmp )
-!
-      end subroutine zonal_leg_wavenum_simple_dist
-!
-! -----------------------------------------------------------------------
-!
-      subroutine zonal_wavenum_list_test(ndomain_m,                     &
-     &          ltr, m_folding, nth, nph, jdx_fsph, mdx_4_lgd)
-!
-      integer(kind = kint), intent(in) :: ltr, m_folding
-      integer(kind = kint), intent(in) :: nth, nph, ndomain_m
-      integer(kind = kint), intent(inout) :: jdx_fsph(-nth:nth)
-      integer(kind = kint), intent(inout) :: mdx_4_lgd(0:nph)
-!
-      integer(kind = kint) :: m, ip, ma, mm, imark, ltr_half
-      integer(kind = kint), allocatable :: ip_tmp(:)
-!
-!
-      write(*,*) 'test test'
-!
-      allocate( ip_tmp(0:ltr) )
-      ip_tmp = 0
-!
-      ip = 1
-      imark = 1
-      ltr_half = ( ltr-mod(ltr,2) ) / (2*m_folding)
-!
-      do m = 1, ltr_half
-        ip_tmp(m) = ip
-        ip = ip + imark
-        if (ip .gt. ndomain_m) then
-          ip =    ndomain_m
-          imark = -1
-        else if (ip .lt. 1) then
-          ip = 1
-          imark =  1
-        end if
-      end do
-!
-      m = 0
-      ip_tmp(m) = ip
-      ip = ip + imark
-      if (ip .gt. ndomain_m) then
-        ip =    ndomain_m
-        imark = -1
-      else if (ip .lt. 1) then
-        ip = 1
-        imark =  1
-      end if
-!
-      do m = ltr_half+1, ltr/m_folding
-        ip_tmp(m) = ip
-        ip = ip + imark
-        if (ip .gt. ndomain_m) then
-          ip =    ndomain_m
-          imark = -1
-        else if (ip .lt. 1) then
-          ip = 1
-          imark =  1
-        end if
-      end do
-!
-!        write(8,*) 'm, ip_tmp(m)'
-!      do m = -ltr, ltr
-!        write(8,*) m, ip_tmp(m)
-!      end do
-!
-      mm = 0
-      do ip = 1, ndomain_m
-        do m = -ltr/m_folding, ltr/m_folding
-          ma = abs(m)
-          if (ip_tmp(ma) .eq. ip) then
-            jdx_fsph(m) = mm
-            mm = mm + 1
-          end if
-        end do
-      end do
-!
-      do m = -ltr/m_folding, ltr/m_folding
-        mm = jdx_fsph(m)
-        mdx_4_lgd(mm) = m
-      end do
-!
-      deallocate( ip_tmp )
-!
-!        write(*,*) 'm, jdx_fsph(m)'
-!      do m = -ltr, ltr
-!        write(*,*) m, jdx_fsph(m)
-!      end do
-!        write(*,*) 'mm, mdx_4_lgd(mm)'
-!      do mm = 0,nph
-!        write(*,*) mm, mdx_4_lgd(mm)
-!      end do
-!
-!
-      end subroutine zonal_wavenum_list_test
+      end subroutine set_local_sph_neib_order
 !
 ! -----------------------------------------------------------------------
 !
