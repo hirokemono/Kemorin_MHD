d1e564ad5e940e343f3e6da1801daa173256cff3
Hiroaki Matsui <h_kemono@mac.com>
2020-09-24 13:13:03 -0700

    add FFT test item

--- MHD/Fortran_src/MHD_src/sph_MHD/init_FFT_4_MHD.F90
+++ MHD/Fortran_src/MHD_src/sph_MHD/init_FFT_4_MHD.F90
@@ -38,20 +38,32 @@
       implicit none
 !
 #ifdef FFTW3
-      integer(kind = kint), parameter :: num_test =   6
+      integer(kind = kint), parameter :: num_test =   12
       integer(kind = kint), parameter :: list_test(num_test)            &
      &        = (/iflag_FFTPACK,                                        &
-     &            iflag_FFTW_SINGLE,                                    &
+     &            iflag_FFTPACK_COMPONENT,                              &
+     &            iflag_FFTPACK_DOMAIN,                                 &
+     &            iflag_FFTPACK_SINGLE,                                 &
      &            iflag_FFTW,                                           &
+     &            iflag_FFTW_SINGLE,                                    &
      &            iflag_FFTW_COMPONENT,                                 &
      &            iflag_ISPACK1,                                        &
-     &            iflag_ISPACK3/)
+     &            iflag_ISPACK3,                                        &
+     &            iflag_ISPACK3_COMPONENT,                              &
+     &            iflag_ISPACK3_DOMAIN,                                 &
+     &            iflag_ISPACK3_SINGLE/)
 #else
-      integer(kind = kint), parameter :: num_test =   3
+      integer(kind = kint), parameter :: num_test =   9
       integer(kind = kint), parameter :: list_test(num_test)            &
      &        = (/iflag_FFTPACK,                                        &
+     &            iflag_FFTPACK_COMPONENT,                              &
+     &            iflag_FFTPACK_DOMAIN,                                 &
+     &            iflag_FFTPACK_SINGLE,                                 &
      &            iflag_ISPACK1,                                        &
-     &            iflag_ISPACK3/)
+     &            iflag_ISPACK3,                                        &
+     &            iflag_ISPACK3_COMPONENT,                              &
+     &            iflag_ISPACK3_DOMAIN,                                 &
+     &            iflag_ISPACK3_SINGLE/)
 #endif
 !
       real(kind = kreal) :: etime_shortest = -1.0e10
