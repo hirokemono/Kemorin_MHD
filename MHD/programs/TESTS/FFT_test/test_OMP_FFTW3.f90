e96c4c87d7641933217551d730f5fd2644441175
Hiroaki Matsui <h_kemono@mac.com>
2020-09-26 23:22:18 -0700

    Add test program for FFTW with openMP

--- MHD/programs/TESTS/FFT_test/test_OMP_FFTW3.f90
+++ MHD/programs/TESTS/FFT_test/test_OMP_FFTW3.f90
@@ -3,42 +3,44 @@
 !
       use m_precision
       use m_constants
-      use m_machine_parameter
 !
-      use t_multi_FFTW_wrapper
+      use t_OMP_FFTW_wrapper
       use t_fft_test_data
 !
       implicit none
 !
-      character(len = kchara) :: mul_fftw_test = 'mul_fftw_test.dat'
+      character(len = kchara) :: mul_fftw_test = 'omp_fftw_test.dat'
 !
-      type(working_mul_FFTW) :: WK_MUL_FFTW_t
+      type(working_OMP_FFTW) :: WK_OMP_FFTW_t
       type(fft_test_data) :: ft3
 !
-      integer(kind = kint), parameter ::  ngrid = 128
+      integer(kind = kint), parameter ::  ngrid = 8192
 !
+      integer :: i
 !
       iflag_debug = 1
-      np_smp = 2
       call init_fft_test_data(ngrid, ft3)
 !
         write(*,*) 'Test FFTW'
-        call init_FFTW_mul_type                                         &
-     &     (np_smp, ft3%nstack, ft3%ngrd, WK_MUL_FFTW_t)
+        call init_OMP_FFTW_type                                         &
+     &     (ft3%nfld, ft3%ngrd, WK_OMP_FFTW_t)
+!
+      do i = 1, 10000
 !
 !$omp parallel workshare
       ft3%z(1:ft3%nfld,1:ft3%ngrd) = ft3%x(1:ft3%nfld,1:ft3%ngrd)
 !$omp end parallel workshare
 !
-        call FFTW_mul_forward_type(np_smp, ft3%nstack,                  &
-     &      ft3%nfld, ft3%ngrd, ft3%x, WK_MUL_FFTW_t)
+        call OMP_forward_FFTW_type                                      &
+     &     (ft3%nfld, ft3%ngrd, ft3%x, WK_OMP_FFTW_t)
 !
 !$omp parallel workshare
       ft3%y(1:ft3%nfld,1:ft3%ngrd) = ft3%x(1:ft3%nfld,1:ft3%ngrd)
 !$omp end parallel workshare
 !
-        call FFTW_mul_backward_type(np_smp, ft3%nstack,                 &
-     &      ft3%nfld, ft3%ngrd, ft3%x, WK_MUL_FFTW_t)
+        call OMP_backward_FFTW_type                                     &
+     &     (ft3%nfld, ft3%ngrd, ft3%x, WK_OMP_FFTW_t)
+      end do
 !
       call write_fft_test_data(mul_fftw_test, ft3)
       call dealloc_fft_test_data(ft3)
