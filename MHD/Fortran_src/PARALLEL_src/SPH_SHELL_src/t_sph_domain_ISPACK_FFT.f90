d883623f2e3635106b8d03c93dbf58eaf489aa21
Hiroaki Matsui <h_kemono@mac.com>
2020-10-06 16:36:25 -0700

    Delete unused integer

--- MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/t_sph_domain_ISPACK_FFT.f90
+++ MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/t_sph_domain_ISPACK_FFT.f90
@@ -199,8 +199,7 @@
 !
       type(work_for_domain_ispack), intent(inout) :: ispack_d
 !
-      integer(kind = kint) :: m, j, ip, ist, num, nd, ist_fft
-      integer(kind = kint) :: inum, inod_c
+      integer(kind = kint) :: ip, num, nd, ist_fft
 !
 !
       do nd = 1, ncomp_fwd
@@ -211,7 +210,7 @@
         if(iflag_FFT_time) call end_elapsed_time(ist_elapsed_FFT+4)
 !
         if(iflag_FFT_time) call start_elapsed_time(ist_elapsed_FFT+5)
-!$omp parallel do private(ip,m,j,num,inum,ist_fft)
+!$omp parallel do private(ip,num,ist_fft)
         do ip = 1, np_smp
           num = sph_rtp%istack_rtp_rt_smp(ip)                           &
      &         - sph_rtp%istack_rtp_rt_smp(ip-1)
