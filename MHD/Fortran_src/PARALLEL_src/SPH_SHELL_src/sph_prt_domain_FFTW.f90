e241a26e02b5f702f657b467d598479b7c211ef4
Hiroaki Matsui <h_kemono@mac.com>
2020-10-08 07:25:46 -0700

    Flip FFT data ordering in plan

--- MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/sph_prt_domain_FFTW.f90
+++ MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/sph_prt_domain_FFTW.f90
@@ -121,13 +121,13 @@
 !
         call dfftw_plan_many_dft_r2c                                    &
      &     (FFTW_f%plan_fwd(ip), IONE_4, int(FFTW_f%Nfft_r), howmany,   &
-     &      FFTW_f%X(ist_r+1), inembed, howmany, IONE_4,                &
-     &      FFTW_f%C(ist_c+1), inembed, howmany, IONE_4,                &
+     &      FFTW_f%X(ist_r+1), inembed, istride, idist_r,               &
+     &      FFTW_f%C(ist_c+1), inembed, istride, idist_c,               &
      &      FFTW_ESTIMATE)
         call dfftw_plan_many_dft_c2r                                    &
      &     (FFTW_f%plan_bwd(ip), IONE_4, int(FFTW_f%Nfft_r), howmany,   &
-     &      FFTW_f%C(ist_c+1), inembed, howmany, IONE_4,                &
-     &      FFTW_f%X(ist_r+1), inembed, howmany, IONE_4,                &
+     &      FFTW_f%C(ist_c+1), inembed, istride, idist_c,               &
+     &      FFTW_f%X(ist_r+1), inembed, istride, idist_r,               &
      &      FFTW_ESTIMATE)
       end do
       FFTW_f%aNfft = one / dble(sph_rtp%nidx_rtp(3))
