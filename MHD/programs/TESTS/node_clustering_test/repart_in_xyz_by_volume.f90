38d3d062db30613d96a3cd7b351bb7ea8df52e15
Hiroaki Matsui <h_kemono@mac.com>
2020-11-02 13:52:49 -0800

    Move subroutines into new module

---
+++ MHD/programs/TESTS/node_clustering_test/repart_in_xyz_by_volume.f90
@@ -0,0 +1,300 @@
+!>@file   repart_in_xyz_by_volume.f90
+!!@brief  module repart_in_xyz_by_volume
+!!
+!!@author H. Matsui
+!!@date Programmed on Oct., 2020
+!
+!>@brief  Construct block stack list for re-partitioning
+!!
+!!@verbatim
+!!      subroutine set_z_sorted_node_and_stack(node, id_block,          &
+!!     &          nblock_z, istack_block_z, inod_sort)
+!!      subroutine set_sorted_node_and_stack(nd, node, id_block,        &
+!!     &          num_nod_grp_yz, idomain_nod_grp_yz, nblock_x,         &
+!!     &          ndomain_yz, istack_yz_grp, istack_block_x, inod_sort)
+!!        type(node_data), intent(in) :: node
+!!
+!!      subroutine set_z_domain_grp_stack                               &
+!!     &         (nblock_z, ndomain_z, istack_block_z, istack_vol_z,    &
+!!     &          num_domain_grp, istack_domain_grp)
+!!      subroutine set_newdomain_grp_stack                              &
+!!     &         (nblock, ndomain_x, ndomain_yz,                        &
+!!     &          num_nod_grp, idomain_nod_grp, istack_block,           &
+!!     &          istack_volume, num_domain_grp, istack_domain_grp)
+!!
+!!      subroutine set_domain_grp_item(node, inod_sort,                 &
+!!     &          num_domain_grp, num_domain_item,                      &
+!!     &          istack_domain_grp, item_domain_grp)
+!!        type(node_data), intent(in) :: node
+!!@endverbatim
+!
+      module repart_in_xyz_by_volume
+!
+      use m_precision
+      use m_constants
+      use t_geometry_data
+!
+      implicit none
+!
+! ----------------------------------------------------------------------
+!
+      contains
+!
+! ----------------------------------------------------------------------
+!
+      subroutine set_z_sorted_node_and_stack(node, id_block,            &
+     &          nblock_z, istack_block_z, inod_sort)
+!
+      use quicksort
+!
+      type(node_data), intent(in) :: node
+      integer(kind = kint), intent(in) :: id_block(node%numnod)
+      integer(kind = kint), intent(in) :: nblock_z
+!
+      integer(kind = kint), intent(inout) :: istack_block_z(0:nblock_z)
+      integer(kind = kint), intent(inout) :: inod_sort(node%numnod)
+!
+      integer(kind = kint) :: inum, inod, jnod, iz, jz
+      real(kind = kreal), allocatable :: data_sort(:)
+!
+!
+      allocate(data_sort(node%numnod))
+!
+!$omp parallel do private(inod)
+      do inod = 1, node%internal_node
+        data_sort(inod) = node%xx(inod,3)
+        inod_sort(inod) = inod
+      end do
+!$omp end parallel do
+!$omp parallel do private(inod)
+      do inod = node%internal_node+1, node%numnod
+        data_sort(inod) = node%xx(inod,3)
+        inod_sort(inod) = inod
+      end do
+!$omp end parallel do
+!
+      call quicksort_real_w_index(node%numnod, data_sort,               &
+     &    ione, node%internal_node, inod_sort)
+!
+!$omp parallel workshare
+      istack_block_z(0:nblock_z) = 0
+!$omp end parallel workshare
+      do inum = 1, node%internal_node-1
+        inod = inod_sort(inum)
+        jnod = inod_sort(inum+1)
+        iz = id_block(inod)
+        jz = id_block(jnod)
+        if(iz .ne. jz) istack_block_z(iz:jz) = inum
+      end do
+      inum = node%internal_node
+      inod = inod_sort(inum)
+      iz = id_block(inod)
+      istack_block_z(iz:nblock_z) = inum
+!
+      deallocate(data_sort)
+!
+      end subroutine set_z_sorted_node_and_stack
+!
+! ----------------------------------------------------------------------
+!
+      subroutine set_sorted_node_and_stack(nd, node, id_block,          &
+     &          num_nod_grp_yz, idomain_nod_grp_yz, nblock_x,           &
+     &          ndomain_yz, istack_yz_grp, istack_block_x, inod_sort)
+!
+      use quicksort
+!
+      integer(kind = kint), intent(in) :: nd
+      type(node_data), intent(in) :: node
+      integer(kind = kint), intent(in) :: id_block(node%numnod)
+!
+      integer(kind = kint), intent(in) :: num_nod_grp_yz
+      integer(kind = kint), intent(in)                                  &
+     &     :: idomain_nod_grp_yz(num_nod_grp_yz)
+!
+      integer(kind = kint), intent(in) :: nblock_x
+      integer(kind = kint), intent(in) :: ndomain_yz
+      integer(kind = kint), intent(in) :: istack_yz_grp(0:ndomain_yz)
+!
+      integer(kind = kint), intent(inout)                               &
+     &                     :: istack_block_x(0:nblock_x,num_nod_grp_yz)
+      integer(kind = kint), intent(inout) :: inod_sort(node%numnod)
+!
+      integer(kind = kint) :: jk, ist, ied, icou, inum
+      integer(kind = kint) :: inod, jnod, ix, jx
+      real(kind = kreal), allocatable :: data_sort(:)
+!
+!
+      allocate(data_sort(node%numnod))
+!
+!$omp parallel do private(inod)
+      do inum = 1, node%internal_node
+        inod = inod_sort(inum)
+        data_sort(inum) = node%xx(inod,nd)
+      end do
+!$omp end parallel do
+!$omp parallel do private(inod)
+      do inod = node%internal_node+1, node%numnod
+        data_sort(inod) = node%xx(inod,nd)
+      end do
+!$omp end parallel do
+!
+      do icou = 1, num_nod_grp_yz
+        jk = idomain_nod_grp_yz(icou)
+        ist = istack_yz_grp(jk-1) + 1
+        ied = istack_yz_grp(jk)
+        if(ied .gt. ist) then
+          call quicksort_real_w_index                                   &
+     &       (node%numnod, data_sort, ist, ied, inod_sort)
+        end if
+!
+!$omp parallel workshare
+        istack_block_x(0:nblock_x,icou) = istack_yz_grp(jk-1)
+!$omp end parallel workshare
+        do inum = ist, ied-1
+          inod = inod_sort(inum)
+          jnod = inod_sort(inum+1)
+          ix = id_block(inod)
+          jx = id_block(jnod)
+          if(ix .ne. jx) istack_block_x(ix:jx,icou) = inum
+        end do
+        inod = inod_sort(ied)
+        ix = id_block(inod)
+        istack_block_x(ix:nblock_x,icou) = ied
+      end do
+!
+      deallocate(data_sort)
+!
+      end subroutine set_sorted_node_and_stack
+!
+! ----------------------------------------------------------------------
+! ----------------------------------------------------------------------
+!
+      subroutine set_z_domain_grp_stack                                 &
+     &         (nblock_z, ndomain_z, istack_block_z, istack_vol_z,      &
+     &          num_domain_grp, istack_domain_grp)
+!
+      integer(kind = kint), intent(in) :: nblock_z
+      integer(kind = kint), intent(in) :: ndomain_z
+      integer(kind = kint), intent(in) :: istack_block_z(0:nblock_z)
+      integer(kind = kint), intent(in) :: istack_vol_z(0:ndomain_z)
+!
+      integer(kind = kint), intent(in) :: num_domain_grp
+      integer(kind = kint), intent(inout)                               &
+     &       :: istack_domain_grp(0:num_domain_grp)
+!
+      integer(kind = kint) :: j, ist, ied, jst, jed
+!
+!
+!$omp parallel workshare
+      istack_domain_grp(0:num_domain_grp) = 0
+!$omp end parallel workshare
+!
+!$omp parallel do private(ist,ied,jst,jed,j)
+      do j = 1, ndomain_z
+        jst = istack_vol_z(j-1) + 1
+        jed = istack_vol_z(j)
+        ist = istack_block_z(jst-1) + 1
+        ied = istack_block_z(jed)
+        istack_domain_grp(j) = ied
+      end do
+!$omp end parallel do
+!
+      end subroutine set_z_domain_grp_stack
+!
+! ----------------------------------------------------------------------
+!
+      subroutine set_newdomain_grp_stack                                &
+     &         (nblock, ndomain_x, ndomain_yz,                          &
+     &          num_nod_grp, idomain_nod_grp, istack_block,             &
+     &          istack_volume, num_domain_grp, istack_domain_grp)
+!
+      integer(kind = kint), intent(in) :: nblock
+      integer(kind = kint), intent(in) :: ndomain_x, ndomain_yz
+      integer(kind = kint), intent(in) :: num_nod_grp
+      integer(kind = kint), intent(in)                                  &
+     &       :: idomain_nod_grp(num_nod_grp+1)
+      integer(kind = kint), intent(in)                                  &
+     &       :: istack_block(0:nblock,num_nod_grp)
+      integer(kind = kint), intent(in)                                  &
+     &       :: istack_volume(0:ndomain_x,ndomain_yz)
+!
+      integer(kind = kint), intent(in) :: num_domain_grp
+      integer(kind = kint), intent(inout)                               &
+     &       :: istack_domain_grp(0:num_domain_grp)
+!
+      integer(kind = kint) :: ist, ied, jst, jed
+      integer(kind = kint) :: icou, i, ix, jk, jk1, jk2
+!
+!
+!$omp parallel workshare
+      istack_domain_grp(0:num_domain_grp) = 0
+!$omp end parallel workshare
+!
+!$omp parallel do private(icou,ist,ied,jst,jed,i,ix,jk,jk1,jk2)
+      do icou = 1, num_nod_grp
+        jk1 = idomain_nod_grp(icou)
+        jk2 = idomain_nod_grp(icou+1)
+        do ix = 1, ndomain_x
+          i = ix + (jk1-1) * ndomain_x
+          jst = istack_volume(ix-1,jk1) + 1
+          jed = istack_volume(ix,  jk1)
+          ist = istack_block(jst-1,icou) + 1
+          ied = istack_block(jed,  icou)
+          istack_domain_grp(i) = ied
+        end do
+!
+        do jk = jk1+1, jk2-1
+          ist = (jk-1) * ndomain_x
+          istack_domain_grp(ist+1:ist+ndomain_x)                        &
+     &        = istack_domain_grp(jk1*ndomain_x)
+        end do
+      end do
+!$omp end parallel do
+!
+      end subroutine set_newdomain_grp_stack
+!
+! ----------------------------------------------------------------------
+! ----------------------------------------------------------------------
+!
+      subroutine set_domain_grp_item(node, inod_sort,                   &
+     &          num_domain_grp, num_domain_item,                        &
+     &          istack_domain_grp, item_domain_grp)
+!
+      use quicksort
+!
+      type(node_data), intent(in) :: node
+      integer(kind = kint), intent(in) :: inod_sort(node%numnod)
+!
+      integer(kind = kint), intent(in) :: num_domain_grp
+      integer(kind = kint), intent(in) :: num_domain_item
+      integer(kind = kint), intent(in)                                  &
+     &       :: istack_domain_grp(0:num_domain_grp)
+!
+      integer(kind = kint), intent(inout)                               &
+     &                     :: item_domain_grp(num_domain_item)
+!
+      integer(kind = kint) :: inum, inod, i, ist, num
+!
+!
+!$omp parallel do private(inum,inod)
+      do inum = 1, num_domain_item
+        inod = inod_sort(inum)
+        item_domain_grp(inum) = inod
+      end do
+!$omp end parallel do
+!
+!$omp parallel do private(i,ist,num)
+      do i = 1, num_domain_grp
+        ist = istack_domain_grp(i-1)
+        num = istack_domain_grp(i  ) - ist
+        if(num .gt. 1) then
+          call quicksort_int(num, item_domain_grp(ist+1), ione, num)
+        end if
+      end do
+!$omp end parallel do
+!
+      end subroutine set_domain_grp_item
+!
+! ----------------------------------------------------------------------
+!
+      end module repart_in_xyz_by_volume
---
+++ MHD/programs/TESTS/node_clustering_test/set_repartition_group_name.f90
@@ -0,0 +1,265 @@
+!>@file   set_repartition_group_name.f90
+!!@brief  module set_repartition_group_name
+!!
+!!@author H. Matsui
+!!@date Programmed on Oct., 2020
+!
+!>@brief  Set group names for re-partitiong groups
+!!
+!!@verbatim
+!!      subroutine set_z_domain_grp_name                                &
+!!     &         (part_param, num_domain_grp, domain_grp_name)
+!!      subroutine set_yz_domain_grp_name                               &
+!!     &         (part_param, num_domain_grp, domain_grp_name)
+!!      subroutine set_xyz_domain_grp_name                              &
+!!     &         (part_param, num_domain_grp, domain_grp_name)
+!!        type(mesh_test_files_param), intent(in) :: part_param
+!!
+!!      subroutine check_stacks_4_z_domain(my_rank, node, part_param,   &
+!!     &          inod_sort, num_nod_group_z, istack_nod_grp_z)
+!!      subroutine check_stacks_4_yz_domain(my_rank, node, part_param,  &
+!!     &          inod_sort, num_nod_grp_z, idomain_nod_grp_z,          &
+!!     &          num_nod_group_yz, istack_nod_grp_y)
+!!      subroutine check_stacks_4_new_domain(my_rank, node, part_param, &
+!!     &          inod_sort, num_nod_grp_yz, idomain_nod_grp_yz,        &
+!!     &          num_nod_grp_xyz, istack_nod_grp_xyz)
+!!        type(node_data), intent(in) :: node
+!!        type(mesh_test_files_param), intent(in) :: part_param
+!!@endverbatim
+!
+      module set_repartition_group_name
+!
+      use m_precision
+      use m_constants
+      use t_geometry_data
+      use t_control_param_vol_grping
+!
+      implicit none
+!
+! ----------------------------------------------------------------------
+!
+      contains
+!
+! ----------------------------------------------------------------------
+!
+      subroutine set_z_domain_grp_name                                  &
+     &         (part_param, num_domain_grp, domain_grp_name)
+!
+      use set_parallel_file_name
+!
+      type(mesh_test_files_param), intent(in) :: part_param
+!
+      integer(kind = kint), intent(in) :: num_domain_grp
+      character(len = kchara), intent(inout)                            &
+     &                        :: domain_grp_name(num_domain_grp)
+!
+      character(len = kchara), parameter :: base_name = 'new_domain'
+      character(len = kchara) :: chara_tmp
+!
+      integer(kind = kint) :: iz
+!
+!$omp parallel do private(iz)
+      do iz = 1, part_param%ndomain_eb(3)
+        write(chara_tmp,'(a,a2)') trim(base_name), '_z'
+        call add_index_after_name(iz, chara_tmp, domain_grp_name(iz))
+      end do
+!$omp end parallel do
+!
+      end subroutine set_z_domain_grp_name
+!
+! ----------------------------------------------------------------------
+!
+      subroutine set_yz_domain_grp_name                                 &
+     &         (part_param, num_domain_grp, domain_grp_name)
+!
+      use set_parallel_file_name
+!
+      type(mesh_test_files_param), intent(in) :: part_param
+!
+      integer(kind = kint), intent(in) :: num_domain_grp
+      character(len = kchara), intent(inout)                            &
+     &                        :: domain_grp_name(num_domain_grp)
+!
+      character(len = kchara), parameter :: base_name = 'new_domain'
+      character(len = kchara) :: chara_tmp
+!
+      integer(kind = kint) :: iz, iy, jk
+!
+!$omp parallel do private(iy,iz,jk)
+      do iz = 1, part_param%ndomain_eb(3)
+        do iy = 1, part_param%ndomain_eb(2)
+          jk = iy + (iz-1) * part_param%ndomain_eb(2)
+!
+          write(chara_tmp,'(a,a2)') trim(base_name),'_y'
+          call add_index_after_name(iy, chara_tmp, domain_grp_name(jk))
+          write(chara_tmp,'(a,a2)') trim(domain_grp_name(jk)),'_z'
+          call add_index_after_name(iz, chara_tmp, domain_grp_name(jk))
+        end do
+      end do
+!$omp end parallel do
+!
+      end subroutine set_yz_domain_grp_name
+!
+! ----------------------------------------------------------------------
+!
+      subroutine set_xyz_domain_grp_name                                &
+     &         (part_param, num_domain_grp, domain_grp_name)
+!
+      use set_parallel_file_name
+!
+      type(mesh_test_files_param), intent(in) :: part_param
+!
+      integer(kind = kint), intent(in) :: num_domain_grp
+      character(len = kchara), intent(inout)                            &
+     &                        :: domain_grp_name(num_domain_grp)
+!
+      character(len = kchara), parameter :: base_name = 'new_domain'
+      character(len = kchara) :: chara_tmp
+!
+      integer(kind = kint) :: iz, iy, ix, jk, i
+!
+!$omp parallel do private(i,ix,iy,iz,jk)
+      do iz = 1, part_param%ndomain_eb(3)
+        do iy = 1, part_param%ndomain_eb(2)
+          jk = iy + (iz-1) * part_param%ndomain_eb(2)
+          do ix = 1, part_param%ndomain_eb(1)
+            i = ix + (jk-1) * part_param%ndomain_eb(1)
+!
+            write(chara_tmp,'(a,a2)') trim(base_name), '_x'
+            call add_index_after_name                                   &
+     &         (ix, chara_tmp, domain_grp_name(i))
+            write(chara_tmp,'(a,a2)') trim(domain_grp_name(i)), '_y'
+            call add_index_after_name                                   &
+     &         (iy, chara_tmp, domain_grp_name(i))
+            write(chara_tmp,'(a,a2)') trim(domain_grp_name(i)), '_z'
+            call add_index_after_name                                   &
+     &         (iz, chara_tmp, domain_grp_name(i))
+          end do
+        end do
+      end do
+!$omp end parallel do
+!
+      end subroutine set_xyz_domain_grp_name
+!
+! ----------------------------------------------------------------------
+! ----------------------------------------------------------------------
+!
+      subroutine check_stacks_4_z_domain(my_rank, node, part_param,     &
+     &          inod_sort, num_nod_group_z, istack_nod_grp_z)
+!
+      integer, intent(in) :: my_rank
+      type(node_data), intent(in) :: node
+      type(mesh_test_files_param), intent(in) :: part_param
+      integer(kind = kint), intent(in) :: inod_sort(node%numnod)
+!
+      integer(kind = kint), intent(in) :: num_nod_group_z
+      integer(kind = kint), intent(in)                                  &
+     &                     :: istack_nod_grp_z(0:num_nod_group_z)
+!
+      integer(kind = kint) :: iz, ist, ied, inum, inod
+!
+      do iz = 1, part_param%ndomain_eb(3)
+        if(istack_nod_grp_z(iz) .eq. istack_nod_grp_z(iz-1)) cycle
+!
+        write(100+my_rank,*) 'istack_nod_grp_z', iz, istack_nod_grp_z(iz)
+        ist = istack_nod_grp_z(iz-1) + 1
+        ied = istack_nod_grp_z(iz  )
+        do inum = ist, ied
+          inod = inod_sort(inum)
+          write(100+my_rank,*) 'inod', inum, inod, iz, node%xx(inod,3)
+        end do
+      end do
+      write(100+my_rank,*) node%internal_node
+!
+      end subroutine check_stacks_4_z_domain
+!
+! ----------------------------------------------------------------------
+!
+      subroutine check_stacks_4_yz_domain(my_rank, node, part_param,    &
+     &          inod_sort, num_nod_grp_z, idomain_nod_grp_z,            &
+     &          num_nod_group_yz, istack_nod_grp_y)
+!
+      integer, intent(in) :: my_rank
+      type(node_data), intent(in) :: node
+      type(mesh_test_files_param), intent(in) :: part_param
+      integer(kind = kint), intent(in) :: inod_sort(node%numnod)
+!
+      integer(kind = kint), intent(in) :: num_nod_grp_z
+      integer(kind = kint), intent(in)                                  &
+     &     :: idomain_nod_grp_z(num_nod_grp_z)
+      integer(kind = kint), intent(in) :: num_nod_group_yz
+      integer(kind = kint), intent(in)                                  &
+     &                     :: istack_nod_grp_y(0:num_nod_group_yz)
+!
+      integer(kind = kint) :: iy, iz, jk, ist, ied, inum, inod, icou
+!
+      do icou = 1, num_nod_grp_z
+        iz = idomain_nod_grp_z(icou)
+        jk = (iz-1) * part_param%ndomain_eb(2)
+        write(100+my_rank,*) 'istack_nod_grp_y0', iz,                   &
+     &                      istack_nod_grp_y(jk)
+        do iy = 1, part_param%ndomain_eb(2)
+          jk = iy + (iz-1) * part_param%ndomain_eb(2)
+          write(100+my_rank,*) 'istack_nod_grp_y', iy, iz,              &
+     &                        istack_nod_grp_y(jk)
+          ist = istack_nod_grp_y(jk-1) + 1
+          ied = istack_nod_grp_y(jk  )
+          do inum = ist, ied
+            inod = inod_sort(inum)
+            write(100+my_rank,*) 'inod', inum, inod,                    &
+     &                           iy, iz, node%xx(inod,2:3)
+          end do
+        end do
+      end do
+      write(100+my_rank,*) node%internal_node
+!
+      end subroutine check_stacks_4_yz_domain
+!
+! ----------------------------------------------------------------------
+!
+      subroutine check_stacks_4_new_domain(my_rank, node, part_param,   &
+     &          inod_sort, num_nod_grp_yz, idomain_nod_grp_yz,          &
+     &          num_nod_grp_xyz, istack_nod_grp_xyz)
+!
+      integer, intent(in) :: my_rank
+      type(node_data), intent(in) :: node
+      type(mesh_test_files_param), intent(in) :: part_param
+      integer(kind = kint), intent(in) :: inod_sort(node%numnod)
+!
+      integer(kind = kint), intent(in) :: num_nod_grp_yz
+      integer(kind = kint), intent(in)                                  &
+     &     :: idomain_nod_grp_yz(num_nod_grp_yz)
+      integer(kind = kint), intent(in) :: num_nod_grp_xyz
+      integer(kind = kint), intent(in)                                  &
+     &                     :: istack_nod_grp_xyz(0:num_nod_grp_xyz)
+!
+      integer(kind = kint) :: ix, iy, iz, jk, ist, ied
+      integer(kind = kint) :: inum, inod, icou, i
+!
+      do icou = 1, num_nod_grp_yz
+        jk = idomain_nod_grp_yz(icou)
+        iy = 1 + mod(jk-1,part_param%ndomain_eb(2))
+        iz = 1 + (jk-1) / part_param%ndomain_eb(2)
+        i = (jk-1) * part_param%ndomain_eb(1)
+        write(100+my_rank,*) 'istack_nod_grp_x0',                       &
+     &                        iy, iz, istack_nod_grp_xyz(i)
+        do ix = 1, part_param%ndomain_eb(1)
+          i = ix + (jk-1) * part_param%ndomain_eb(1)
+          write(100+my_rank,*) 'istack_nod_grp_xyz', ix, iy, iz,        &
+     &                          istack_nod_grp_xyz(i)
+          ist = istack_nod_grp_xyz(i-1) + 1
+          ied = istack_nod_grp_xyz(i  )
+          do inum = ist, ied
+            inod = inod_sort(inum)
+            write(100+my_rank,*) 'inod', inum, inod,                    &
+     &                            ix, iy, iz, node%xx(inod,1:3)
+          end do
+        end do
+      end do
+      write(100+my_rank,*) node%internal_node
+!
+      end subroutine check_stacks_4_new_domain
+!
+! ----------------------------------------------------------------------
+!
+      end module set_repartition_group_name
