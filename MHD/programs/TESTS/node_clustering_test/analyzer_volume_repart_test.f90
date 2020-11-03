0542b5a20fc75c12b4fb7c8831cb1d9eef3ac209
Hiroaki Matsui <h_kemono@mac.com>
2020-11-03 15:13:25 -0800

    make new program for re-partitioning test

---
+++ MHD/programs/TESTS/node_clustering_test/analyzer_volume_repart_test.f90
@@ -0,0 +1,163 @@
+!>@file   analyzer_volume_repart_test.f90
+!!@brief  module analyzer_volume_repart_test
+!!
+!!@author H. Matsui
+!!@date Programmed on Oct., 2020
+!
+!>@brief  Make grouping with respect to volume
+!!
+!!@verbatim
+!!      subroutine initialize_volume_repartition
+!!      subroutine analyze_volume_grouping
+!!@endverbatim
+!
+      module analyzer_volume_repart_test
+!
+      use m_precision
+!
+      use m_constants
+      use m_machine_parameter
+      use calypso_mpi
+!
+      use t_mesh_data
+      use t_comm_table
+      use t_geometry_data
+      use t_group_data
+      use t_surface_data
+      use t_edge_data
+      use m_work_time
+!
+      implicit none
+!
+      type(mesh_data), save :: fem_T
+!
+! ----------------------------------------------------------------------
+!
+      contains
+!
+! ----------------------------------------------------------------------
+!
+      subroutine initialize_volume_repartition
+!
+      use m_array_for_send_recv
+      use m_default_file_prefix
+      use t_ctl_data_volume_grouping
+      use t_control_param_vol_grping
+      use t_1d_repartitioning_work
+      use t_repartition_by_volume
+      use set_istack_4_domain_block
+      use repart_in_xyz_by_volume
+      use set_repartition_group_name
+!
+      use t_file_IO_parameter
+      use t_mesh_data
+      use t_read_mesh_data
+      use t_shape_functions
+      use t_jacobians
+      use t_fem_gauss_int_coefs
+      use t_next_node_ele_4_node
+!
+      use mpi_load_mesh_data
+      use mesh_file_IO
+      use copy_mesh_structures
+      use append_group_data
+!
+      use calypso_mpi_int
+      use calypso_mpi_real
+      use const_jacobians_3d
+      use parallel_FEM_mesh_init
+      use output_test_mesh
+      use set_table_4_RHS_assemble
+      use nod_phys_send_recv
+      use solver_SR_type
+      use transfer_to_long_integers
+!
+      use set_parallel_file_name
+      use int_volume_of_single_domain
+!
+!>     Stracture for Jacobians
+!
+      type(new_patition_test_control) :: part_tctl1
+      type(mesh_test_files_param) ::  T_meshes
+!
+      type(next_nod_ele_table) :: next_tbl_T
+!
+      type(jacobians_type) :: jacobians_T
+      type(shape_finctions_at_points) :: spfs_T
+!
+      type(group_data) :: part_grp
+!
+!     --------------------- 
+!
+      call init_elapse_time_by_TOTAL
+!      call elapsed_label_4_ele_comm_tbl
+!
+!     --------------------- 
+!
+      if (my_rank.eq.0) then
+        write(*,*) 'Test mesh commnucations'
+        write(*,*) 'Input file: mesh data'
+      end if
+!
+!     ----- read control data
+!
+      call read_control_new_partition(part_tctl1)
+!
+      call s_set_ctl_params_4_test_mesh(part_tctl1, T_meshes)
+!
+!  --  read geometry
+      if (iflag_debug.gt.0) write(*,*) 'mpi_input_mesh'
+      call mpi_input_mesh(T_meshes%mesh_file_IO, nprocs, fem_T)
+!
+!  -------------------------------
+!
+      if(iflag_TOT_time) call start_elapsed_time(ied_total_elapsed)
+      if (iflag_debug.gt.0 ) write(*,*) 'FEM_mesh_initialization'
+      call FEM_mesh_initialization(fem_T%mesh, fem_T%group)
+      if(iflag_TOT_time) call end_elapsed_time(ied_total_elapsed)
+!
+!  -------------------------------
+!
+      if (iflag_debug.gt.0) write(*,*) 'const_jacobian_and_single_vol'
+      allocate(jacobians_T%g_FEM)
+      call sel_max_int_point_by_etype                                   &
+     &   (fem_T%mesh%ele%nnod_4_ele, jacobians_T%g_FEM)
+      call const_jacobian_and_single_vol                                &
+     &   (fem_T%mesh, fem_T%group, spfs_T, jacobians_T)
+!
+      call init_send_recv(fem_T%mesh%nod_comm)
+      if(iflag_debug .gt. 0) write(*,*) 'estimate node volume'
+!
+!  -------------------------------
+!
+      if(iflag_debug .gt. 0) write(*,*) 'set_belonged_ele_and_next_nod'
+      call set_belonged_ele_and_next_nod                                &
+     &   (fem_T%mesh, next_tbl_T%neib_ele, next_tbl_T%neib_nod)
+!
+!       Re-partitioning
+      call s_repartition_by_volume(fem_T%mesh, T_meshes, part_grp)
+!
+!       Append group data
+      call s_append_group_data(part_grp, fem_T%group%nod_grp)
+!
+!       Output appended mesh
+      call mpi_output_mesh(T_meshes%new_mesh_file_IO,                   &
+     &    fem_T%mesh, fem_T%group)
+!
+      end subroutine initialize_volume_repartition
+!
+! ----------------------------------------------------------------------
+!
+      subroutine analyze_volume_repartition
+!
+!
+!      call output_elapsed_times
+      call calypso_MPI_barrier
+!
+      if(iflag_debug.gt.0) write(*,*) 'exit analyze_volume_repartition'
+!
+      end subroutine analyze_volume_repartition
+!
+! ----------------------------------------------------------------------
+!
+      end module analyzer_volume_repart_test
