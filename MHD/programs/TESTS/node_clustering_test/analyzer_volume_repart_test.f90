095f8744573a4279c6e8c71025dc1ac67280bfb2
Hiroaki Matsui <h_kemono@mac.com>
2020-11-04 06:50:30 -0800

    Add routines to construct re-partitioned external node groups

--- MHD/programs/TESTS/node_clustering_test/analyzer_volume_repart_test.f90
+++ MHD/programs/TESTS/node_clustering_test/analyzer_volume_repart_test.f90
@@ -145,7 +145,7 @@
 !
       call const_external_grp_4_new_part                                &
      &  (fem_T%mesh%nod_comm, fem_T%mesh%node, next_tbl_T%neib_nod,     &
-     &   part_grp, ext_grp)
+     &   T_meshes, part_grp, ext_grp)
 !
       end subroutine initialize_volume_repartition
 !
@@ -229,18 +229,22 @@
 ! ----------------------------------------------------------------------
 !
       subroutine const_external_grp_4_new_part                          &
-     &         (nod_comm, node, neib_nod, part_grp, ext_grp)
+     &         (nod_comm, node, neib_nod, part_param,                   &
+     &          part_grp, ext_grp)
 !
       use t_comm_table
       use t_geometry_data
       use t_next_node_ele_4_node
       use t_calypso_comm_table
+      use t_control_param_vol_grping
 !
       use solver_SR_type
+      use set_repartition_group_name
 !
       type(communication_table), intent(in) :: nod_comm
       type(node_data), intent(in) :: node
       type(next_nod_id_4_nod), intent(in) :: neib_nod
+      type(mesh_test_files_param), intent(in) :: part_param
       type(group_data), intent(in) :: part_grp
 !
       type(group_data), intent(inout) :: ext_grp
@@ -277,51 +281,49 @@
       ext_grp%num_grp = part_grp%num_grp
       call alloc_group_num(ext_grp)
 !
-!$omp parallel workshare
-      ext_grp%grp_name(1:ext_grp%num_grp)                               &
-     &     = part_grp%grp_name(1:ext_grp%num_grp)
-!$omp end parallel workshare
+      call set_xyz_domain_grp_name                                      &
+     &   (part_param, ext_grp%num_grp, ext_grp%grp_name)
 !
 !
-      do igrp = 1, part_grp%num_grp
-!$omp parallel workshare
-        iflag_nod(1:node%internal_node) = 1
-!$omp end parallel workshare
-        if(node%numnod .gt. node%internal_node) then
-!$omp parallel workshare
-          iflag_nod(node%internal_node+1:node%numnod) = 0
-!$omp end parallel workshare
-        end if
-!
-        ist = part_grp%istack_grp(igrp-1) + 1
-        ied = part_grp%istack_grp(igrp)
-!$omp parallel do private(inum,inod)
-        do inum = ist, ied
-          inod = part_grp%item_grp(inum)
-          iflag_nod(inod) = 0
-        end do
-!$omp end parallel do
-!
-        do inum = ist, ied
-          inod = part_grp%item_grp(inum)
-          jst = neib_nod%istack_next(inod-1) + 1
-          jed = neib_nod%istack_next(inod)
-          do jnum = jst, jed
-            jnod = neib_nod%inod_next(jnum)
-            if(jnod .le. node%internal_node                             &
-     &          .and. iflag_nod(jnod) .gt. 0) then
-              ext_grp%nitem_grp(igrp) = ext_grp%nitem_grp(igrp) + 1
-              iflag_nod(jnod) = 0
-            end if
-          end do
-        end do
-        ext_grp%istack_grp(igrp) = ext_grp%istack_grp(igrp-1)           &
-     &                            + ext_grp%nitem_grp(igrp)
-      end do
-!
-      ext_grp%num_item = ext_grp%istack_grp(part_grp%num_grp)
+      call count_external_grp_4_new_part(node, neib_nod, part_grp,      &
+     &    ext_grp%num_grp, ext_grp%num_item, ext_grp%istack_grp,        &
+     &    iflag_nod)
       call alloc_group_item(ext_grp)
 !
+      call set_external_grp_4_new_part(node, neib_nod, part_grp,        &
+     &    ext_grp%num_grp, ext_grp%num_item, ext_grp%istack_grp,        &
+     &    ext_grp%item_grp, iflag_nod)
+!
+      deallocate(idomain_new, iflag_nod)
+!
+      end subroutine const_external_grp_4_new_part
+!
+! ----------------------------------------------------------------------
+!
+      subroutine count_external_grp_4_new_part                          &
+     &         (node, neib_nod, part_grp, num_ext_grp,                  &
+     &          ntot_ext_grp, istack_ext_grp, iflag_nod)
+!
+      use t_geometry_data
+      use t_group_data
+      use t_next_node_ele_4_node
+!
+      type(node_data), intent(in) :: node
+      type(next_nod_id_4_nod), intent(in) :: neib_nod
+      type(group_data), intent(in) :: part_grp
+      integer(kind = kint), intent(in) :: num_ext_grp
+!
+      integer(kind = kint), intent(inout) :: ntot_ext_grp
+      integer(kind = kint), intent(inout)                               &
+     &                     :: istack_ext_grp(0:num_ext_grp)
+      integer(kind = kint), intent(inout) :: iflag_nod(node%numnod)
+!
+      integer(kind = kint) :: inum, inod, ist, ied
+      integer(kind = kint) :: jnum, jnod, jst, jed
+      integer(kind = kint) :: igrp, icou
+!
+!
+      istack_ext_grp(0) = 0
       do igrp = 1, part_grp%num_grp
 !$omp parallel workshare
         iflag_nod(1:node%internal_node) = 1
@@ -332,6 +334,7 @@
 !$omp end parallel workshare
         end if
 !
+        icou = istack_ext_grp(igrp-1)
         ist = part_grp%istack_grp(igrp-1) + 1
         ied = part_grp%istack_grp(igrp)
 !$omp parallel do private(inum,inod)
@@ -341,9 +344,6 @@
         end do
 !$omp end parallel do
 !
-        icou = ext_grp%istack_grp(igrp-1)
-        ist = part_grp%istack_grp(igrp-1) + 1
-        ied = part_grp%istack_grp(igrp)
         do inum = ist, ied
           inod = part_grp%item_grp(inum)
           jst = neib_nod%istack_next(inod-1) + 1
@@ -353,16 +353,80 @@
             if(jnod .le. node%internal_node                             &
      &          .and. iflag_nod(jnod) .gt. 0) then
               icou = icou + 1
-              ext_grp%item_grp(icou) = jnod
               iflag_nod(jnod) = 0
+            end if
+          end do
+        end do
+        istack_ext_grp(igrp) = icou
+      end do
+!
+      ntot_ext_grp = istack_ext_grp(part_grp%num_grp)
+!
+      end subroutine count_external_grp_4_new_part
+!
+! ----------------------------------------------------------------------
+!
+      subroutine set_external_grp_4_new_part(node, neib_nod, part_grp,  &
+     &          num_ext_grp, ntot_ext_grp, istack_ext_grp,              &
+     &          item_ext_grp, iflag_nod)
+!
+      use t_geometry_data
+      use t_group_data
+      use t_next_node_ele_4_node
+!
+      type(node_data), intent(in) :: node
+      type(next_nod_id_4_nod), intent(in) :: neib_nod
+      type(group_data), intent(in) :: part_grp
+      integer(kind = kint), intent(in) :: num_ext_grp
+      integer(kind = kint), intent(in) :: ntot_ext_grp
+      integer(kind = kint), intent(in) :: istack_ext_grp(0:num_ext_grp)
+!
+      integer(kind = kint), intent(inout) :: item_ext_grp(ntot_ext_grp)
+      integer(kind = kint), intent(inout) :: iflag_nod(node%numnod)
+!
+!
+      integer(kind = kint) :: inum, inod, ist, ied
+      integer(kind = kint) :: jnum, jnod, jst, jed
+      integer(kind = kint) :: igrp, icou
+!
+!
+      do igrp = 1, part_grp%num_grp
+!$omp parallel workshare
+        iflag_nod(1:node%internal_node) = 1
+!$omp end parallel workshare
+        if(node%numnod .gt. node%internal_node) then
+!$omp parallel workshare
+          iflag_nod(node%internal_node+1:node%numnod) = 0
+!$omp end parallel workshare
+        end if
+!
+        icou = istack_ext_grp(igrp-1)
+        ist = part_grp%istack_grp(igrp-1) + 1
+        ied = part_grp%istack_grp(igrp)
+!$omp parallel do private(inum,inod)
+        do inum = ist, ied
+          inod = part_grp%item_grp(inum)
+          iflag_nod(inod) = 0
+        end do
+!$omp end parallel do
+!
+        do inum = ist, ied
+          inod = part_grp%item_grp(inum)
+          jst = neib_nod%istack_next(inod-1) + 1
+          jed = neib_nod%istack_next(inod)
+          do jnum = jst, jed
+            jnod = neib_nod%inod_next(jnum)
+            if(jnod .le. node%internal_node                             &
+     &          .and. iflag_nod(jnod) .gt. 0) then
+              icou = icou + 1
+              item_ext_grp(icou) = jnod
+              iflag_nod(jnod) =    0
             end if
           end do
         end do
       end do
 !
-      deallocate(idomain_new, iflag_nod)
-!
-      end subroutine const_external_grp_4_new_part
+      end subroutine set_external_grp_4_new_part
 !
 ! ----------------------------------------------------------------------
 ! ----------------------------------------------------------------------
