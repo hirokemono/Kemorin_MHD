958c62935d46d9c97d4ecc74fbd93d77c58ee510
Hiroaki Matsui <h_kemono@mac.com>
2020-11-04 08:10:00 -0800

    Fix subroutine call

--- MHD/programs/TESTS/node_clustering_test/analyzer_volume_grouping.f90
+++ MHD/programs/TESTS/node_clustering_test/analyzer_volume_grouping.f90
@@ -140,8 +140,8 @@
 !
 !       Re-partitioning for external node
       call const_external_grp_4_new_part                                &
-     &  (fem_T%mesh%nod_comm, fem_T%mesh%node, next_tbl_T%neib_nod,     &
-     &   T_meshes, part_grp, ext_grp)
+     &   (fem_T%mesh%node, next_tbl_T%neib_nod, T_meshes,               &
+     &    part_grp, ext_grp)
 !
 !       Append group data
       call s_append_group_data(part_grp, fem_T%group%nod_grp)
