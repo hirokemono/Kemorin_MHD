3108681f1479000f2acefc887a9357bcfbf21c4a
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-03-05 23:12:51 -0800

    Add routine to extend sleeve group data

--- MHD/programs/mesh_utilities/partitioner/analyzer_sleeve_extend2.f90
+++ MHD/programs/mesh_utilities/partitioner/analyzer_sleeve_extend2.f90
@@ -178,6 +178,7 @@
       type(communication_table), save :: new_ele_comm
       type(dist_from_wall_in_export) :: dist_4_comm
 !
+      integer :: i
 !
       if(iflag_SLEX_time) call start_elapsed_time(ist_elapsed_SLEX+1)
       if (iflag_debug.gt.0) write(*,*) 'set_belonged_ele_and_next_nod'
@@ -203,16 +204,19 @@
 !
 !      if(iflag_SLEX_time) call start_elapsed_time(ist_elapsed_SLEX+2)
       if (iflag_debug.gt.0) write(*,*) 'extend_node_comm_table2'
-      call extend_node_comm_table2                                     &
-     &   (mesh%nod_comm, ele_comm, mesh%node, inod_dbl_org,            &
-     &    mesh%ele, iele_dbl_org, next_tbl%neib_ele,                   &
-     &    newmesh%nod_comm, newmesh%node, newmesh%ele, new_ele_comm,   &
+      call extend_node_comm_table2                                      &
+     &   (mesh%nod_comm, ele_comm, mesh%node, inod_dbl_org,             &
+     &    mesh%ele, iele_dbl_org, next_tbl%neib_ele,                    &
+     &    newmesh%nod_comm, newmesh%node, newmesh%ele, new_ele_comm,    &
      &    dist_4_comm)
-!
-      write(*,*) my_rank, ' new_elements: ', newmesh%ele%numele - mesh%ele%numele
 !
       call check_extended_element                                       &
      &   (newmesh%nod_comm, newmesh%node, newmesh%ele, new_ele_comm)
+!
+!
+      call s_extend_groups                                              &
+     &   (mesh, group, newmesh, new_ele_comm, newgroup)
+!
 !      if(iflag_SLEX_time) call end_elapsed_time(ist_elapsed_SLEX+2)
 !
 !      if(iflag_SLEX_time) call start_elapsed_time(ist_elapsed_SLEX+3)
@@ -2054,5 +2058,193 @@
       end subroutine check_extended_element
 !
 !  ---------------------------------------------------------------------
+!  ---------------------------------------------------------------------
+!
+      subroutine s_extend_groups(org_mesh, org_groups,                  &
+     &          new_mesh, new_ele_comm, new_groups)
+!
+      type(mesh_geometry), intent(in) :: org_mesh
+      type(mesh_groups), intent(in) :: org_groups
+      type(mesh_geometry), intent(in) :: new_mesh
+      type(communication_table), intent(in) :: new_ele_comm
+!
+      type(mesh_groups), intent(inout) :: new_groups
+!
+!
+      call extended_node_group                                          &
+     &   (org_mesh%node, org_groups%nod_grp,                            &
+     &    new_mesh%node, new_mesh%nod_comm, new_groups%nod_grp)
+      call extended_element_group(org_groups%ele_grp,                   &
+     &    new_mesh%ele, new_ele_comm, new_groups%ele_grp)
+      call extended_surface_group(org_groups%surf_grp,                  &
+     &    new_mesh%ele, new_ele_comm, new_groups%surf_grp)
+!
+      end subroutine s_extend_groups
+!
+! ----------------------------------------------------------------------
+!
+      subroutine extended_node_group(node, nod_grp,                     &
+     &          new_node, new_comm, new_nod_grp)
+!
+      use solver_SR_type
+      use select_copy_from_recv
+      use redistribute_group_data
+!
+      type(node_data), intent(in) :: node
+      type(group_data), intent(in) :: nod_grp
+!
+      type(node_data), intent(in) :: new_node
+      type(communication_table), intent(in) :: new_comm
+!
+      type(group_data), intent(inout) :: new_nod_grp
+!
+      integer(kind = kint) :: igrp
+      integer(kind = kint), allocatable :: iflag_new(:)
+!
+!
+      allocate(iflag_new(new_node%numnod))
+!$omp parallel workshare
+      iflag_new(1:new_node%numnod) = 0
+!$omp end parallel workshare
+!
+      new_nod_grp%num_grp = nod_grp%num_grp
+      call alloc_group_num(new_nod_grp)
+!
+!$omp parallel do
+      do igrp = 1, nod_grp%num_grp
+        new_nod_grp%grp_name(igrp) = nod_grp%grp_name(igrp)
+      end do
+!$omp end parallel do
+!
+      new_nod_grp%istack_grp(0:new_nod_grp%num_grp) = 0
+      call alloc_group_item(new_nod_grp)
+      do igrp = 1, nod_grp%num_grp
+        call mark_org_group_repart                                      &
+     &     (igrp, node%numnod, nod_grp, iflag_new(1))
+!
+        call SOLVER_SEND_RECV_int_type                                  &
+     &     (new_node%numnod, new_comm, iflag_new)
+!
+        call count_group_item_repart                                    &
+     &     (igrp, new_node%numnod, iflag_new(1), new_nod_grp)
+        call append_group_item_repart                                   &
+     &     (igrp, new_node%numnod, iflag_new(1), new_nod_grp)
+      end do
+      deallocate(iflag_new)
+!
+      end subroutine extended_node_group
+!
+! ----------------------------------------------------------------------
+!
+      subroutine extended_element_group                                 &
+     &         (ele_grp, new_ele, new_ele_comm, new_ele_grp)
+!
+      use calypso_SR_type
+      use solver_SR_type
+      use select_copy_from_recv
+      use redistribute_group_data
+!
+      type(group_data), intent(in) :: ele_grp
+!
+      type(element_data), intent(in) :: new_ele
+      type(communication_table), intent(in) :: new_ele_comm
+!
+      type(group_data), intent(inout) :: new_ele_grp
+!
+      integer(kind = kint) :: igrp
+      integer(kind = kint), allocatable :: iflag_new(:)
+!
+!
+      allocate(iflag_new(new_ele%numele))
+!$omp parallel workshare
+      iflag_new(1:new_ele%numele) = 0
+!$omp end parallel workshare
+!
+      new_ele_grp%num_grp = ele_grp%num_grp
+      call alloc_group_num(new_ele_grp)
+!
+!$omp parallel do
+      do igrp = 1, ele_grp%num_grp
+        new_ele_grp%grp_name(igrp) = ele_grp%grp_name(igrp)
+      end do
+!$omp end parallel do
+!
+      new_ele_grp%istack_grp(0:new_ele_grp%num_grp) = 0
+      call alloc_group_item(new_ele_grp)
+      do igrp = 1, ele_grp%num_grp
+        call mark_org_group_repart                                      &
+     &     (igrp, new_ele%numele, ele_grp, iflag_new)
+!
+        call SOLVER_SEND_RECV_int_type                                  &
+     &     (new_ele%numele, new_ele_comm, iflag_new(1))
+!
+        call count_group_item_repart                                    &
+     &     (igrp, new_ele%numele, iflag_new(1), new_ele_grp)
+        call append_group_item_repart                                   &
+     &     (igrp, new_ele%numele, iflag_new(1), new_ele_grp)
+      end do
+      deallocate(iflag_new)
+!
+      end subroutine extended_element_group
+!
+! ----------------------------------------------------------------------
+!
+      subroutine extended_surface_group                                 &
+     &         (surf_grp, new_ele, new_ele_comm, new_surf_grp)
+!
+      use calypso_SR_type
+      use solver_SR_type
+      use select_copy_from_recv
+      use redistribute_group_data
+!
+      type(surface_group_data), intent(in) :: surf_grp
+!
+      type(element_data), intent(in) :: new_ele
+      type(communication_table), intent(in) :: new_ele_comm
+!
+      type(surface_group_data), intent(inout) :: new_surf_grp
+!
+      integer(kind = kint) :: igrp
+      integer(kind = kint), allocatable :: iflag_new(:)
+!
+!
+      allocate(iflag_new(new_ele%numele))
+!$omp parallel workshare
+      iflag_new(1:new_ele%numele) = 0
+!$omp end parallel workshare
+!
+!
+      new_surf_grp%num_grp = surf_grp%num_grp
+      call alloc_sf_group_num(new_surf_grp)
+!
+!$omp parallel do
+      do igrp = 1, surf_grp%num_grp
+        new_surf_grp%grp_name(igrp) = surf_grp%grp_name(igrp)
+      end do
+!$omp end parallel do
+!
+      new_surf_grp%istack_grp(0:new_surf_grp%num_grp) = 0
+      call alloc_sf_group_item(new_surf_grp)
+!
+      do igrp = 1, surf_grp%num_grp
+        call mark_org_surf_group_repart                                 &
+     &     (igrp, new_ele%numele, surf_grp, iflag_new)
+!
+        call SOLVER_SEND_RECV_int_type                                  &
+     &     (new_ele%numele, new_ele_comm, iflag_new)
+!
+!        write(100+my_rank,*) igrp, new_ele%numele, 'iflag',            &
+!     &        sum(iflag_new), sum(iflag_new)
+
+        call count_surf_group_item_repart                               &
+     &    (igrp, new_ele%numele, iflag_new(1), new_surf_grp)
+        call append_surf_group_item_repart                              &
+     &    (igrp, new_ele%numele, iflag_new(1), new_surf_grp)
+      end do
+      deallocate(iflag_new)
+!
+      end subroutine extended_surface_group
+!
+! ----------------------------------------------------------------------
 !
       end module analyzer_sleeve_extend2
