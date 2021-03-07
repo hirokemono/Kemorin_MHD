469cf75bcc538ba904bf03d1b24be4a1ce44e0cc
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-03-07 13:54:59 -0800

    Make new structure for sorting

--- MHD/Fortran_src/PARALLEL_src/modify_MESH/t_mesh_for_sleeve_extend.f90
+++ MHD/Fortran_src/PARALLEL_src/modify_MESH/t_mesh_for_sleeve_extend.f90
@@ -17,6 +17,11 @@
 !!      subroutine dealloc_ele_data_sleeve_ext(comm_connect)
 !!        integer(kind = kint), intent(in) :: ntot_comm, nnod_4_ele
 !!        type(ele_data_for_sleeve_ext), intent(inout) :: comm_connect
+!!
+!!      subroutine alloc_sort_data_sleeve_ext(ntot_comm, sorted_import)
+!!      subroutine dealloc_sort_data_sleeve_ext(sorted_import)
+!!        integer(kind = kint), intent(in) :: ntot_comm,
+!!        type(sort_data_for_sleeve_trim), intent(inout) :: sorted_import
 !!@endverbatim
 !
       module t_mesh_for_sleeve_extend
@@ -50,6 +55,16 @@
 !>        Home process
         integer(kind = kint), allocatable :: irank_comm(:)
       end type ele_data_for_sleeve_ext
+!
+!
+      type sort_data_for_sleeve_trim
+!>        Numper of node
+        integer(kind = kint) :: nitem_sort
+        integer(kind = kint), allocatable :: iref_lc_import(:)
+        integer(kind = kint), allocatable :: irank_import_sort(:)
+        integer(kind = kint), allocatable :: isorted_to_org(:)
+        integer(kind = kint), allocatable :: irank_orgin_pe(:)
+      end type sort_data_for_sleeve_trim
 !
 ! ----------------------------------------------------------------------
 !
@@ -101,11 +116,40 @@
 !
 !$omp parallel workshare
       comm_connect%iele_gl_comm(1:comm_connect%nele_comm) =       0
-      comm_connect%ie_comm(1:comm_connect%nele_comm,nnod_4_ele) = 0
       comm_connect%irank_comm(1:comm_connect%nele_comm) =        -1
 !$omp end parallel workshare
 !
+!$omp parallel workshare
+      comm_connect%ie_comm(1:comm_connect%nele_comm,1:nnod_4_ele) = 0
+!$omp end parallel workshare
+!
       end subroutine alloc_ele_data_sleeve_ext
+!
+! ----------------------------------------------------------------------
+!
+      subroutine alloc_sort_data_sleeve_ext(ntot_comm, sorted_import)
+!
+      integer(kind = kint), intent(in) :: ntot_comm
+      type(sort_data_for_sleeve_trim), intent(inout) :: sorted_import
+!
+!
+      sorted_import%nitem_sort =  ntot_comm
+!
+      allocate(sorted_import%iref_lc_import(sorted_import%nitem_sort))
+      allocate(sorted_import%irank_import_sort(sorted_import%nitem_sort))
+      allocate(sorted_import%isorted_to_org(sorted_import%nitem_sort))
+      allocate(sorted_import%irank_orgin_pe(sorted_import%nitem_sort))
+!
+      if(sorted_import%nitem_sort .le. 0) return
+!
+!$omp parallel workshare
+      sorted_import%iref_lc_import(1:sorted_import%nitem_sort) =     0
+      sorted_import%irank_import_sort(1:sorted_import%nitem_sort) = -1
+      sorted_import%isorted_to_org(1:sorted_import%nitem_sort) =     0
+      sorted_import%irank_orgin_pe(1:sorted_import%nitem_sort) =    -1
+!$omp end parallel workshare
+!
+      end subroutine alloc_sort_data_sleeve_ext
 !
 ! ----------------------------------------------------------------------
 ! ----------------------------------------------------------------------
@@ -138,6 +182,20 @@
       deallocate(comm_connect%irank_comm)
 !
       end subroutine dealloc_ele_data_sleeve_ext
+!
+! ----------------------------------------------------------------------
+!
+      subroutine dealloc_sort_data_sleeve_ext(sorted_import)
+!
+      type(sort_data_for_sleeve_trim), intent(inout) :: sorted_import
+!
+!
+      deallocate(sorted_import%iref_lc_import)
+      deallocate(sorted_import%irank_import_sort)
+      deallocate(sorted_import%isorted_to_org)
+      deallocate(sorted_import%irank_orgin_pe)
+!
+      end subroutine dealloc_sort_data_sleeve_ext
 !
 ! ----------------------------------------------------------------------
 !
