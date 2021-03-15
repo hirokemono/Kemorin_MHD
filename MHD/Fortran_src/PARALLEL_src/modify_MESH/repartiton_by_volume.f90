31033d4d8da8771062dfd60febe2e1c58e4af868
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-03-15 07:59:09 -0700

    Fix data output routines with sleeve extend

--- MHD/Fortran_src/PARALLEL_src/modify_MESH/repartiton_by_volume.f90
+++ MHD/Fortran_src/PARALLEL_src/modify_MESH/repartiton_by_volume.f90
@@ -82,13 +82,16 @@
       if(iflag_RPRT_time) call end_elapsed_time(ist_elapsed_RPRT+2)
 !
 ! Increase sleeve size
-      if(part_param%num_FEM_sleeve .le. 1) return
-      if(iflag_RPRT_time) call start_elapsed_time(ist_elapsed_RPRT+3)
-      call sleeve_extension_loop(part_param%num_FEM_sleeve,             &
-     &                           new_fem%mesh, new_fem%group)
-      if(iflag_RPRT_time) call end_elapsed_time(ist_elapsed_RPRT+3)
+      if(part_param%num_FEM_sleeve .gt. 1) then
+        if(iflag_RPRT_time) call start_elapsed_time(ist_elapsed_RPRT+3)
+        call sleeve_extension_loop(part_param%num_FEM_sleeve,           &
+     &                             new_fem%mesh, new_fem%group)
+        if(iflag_RPRT_time) call end_elapsed_time(ist_elapsed_RPRT+3)
+      end if
 !
 !       Output new mesh file
+      write(*,*) 'outout', part_param%viz_mesh_file%iflag_format , &
+     &         part_param%trans_tbl_file%iflag_format
       if(iflag_RPRT_time) call start_elapsed_time(ist_elapsed_RPRT+6)
       if(part_param%viz_mesh_file%iflag_format .eq. id_no_file) then
         if(my_rank .eq. 0) write(*,*)                                   &
