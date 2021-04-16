2fc5b50b30815228b21d8859ed78552a24c1a6f3
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-04-15 23:48:49 -0700

    Exit after count_trimmed_import_stack

--- MHD/Fortran_src/PARALLEL_src/modify_MESH/t_trim_overlapped_import.f90
+++ MHD/Fortran_src/PARALLEL_src/modify_MESH/t_trim_overlapped_import.f90
@@ -81,6 +81,7 @@
       call count_trimmed_import_stack(nprocs, sort_import,              &
      &    ext_trim%ntot_trimmed, ext_trim%istack_trimmed_pe,            &
      &    ext_trim%istack_trimmed_item)
+      return
 !
       call alloc_idx_trimed_to_sorted(ext_trim)
       call trim_original_import_items                                   &
