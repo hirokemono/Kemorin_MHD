e6577e7b13f8f1ab2271cb26e0c63b2514bc6b9c
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-05-06 13:04:38 -0700

    Fix subroutine calls

--- MHD/programs/mesh_utilities/LIC_noise/noise_on_cube.f90
+++ MHD/programs/mesh_utilities/LIC_noise/noise_on_cube.f90
@@ -21,7 +21,9 @@
       call  read_cube_noise_control_file(id_control, ctl_file_name,     &
      &    hd_cube_noise, noise_c1)
       call set_control_3d_cube_noise(noise_c1, noise_t1)
-      call sel_const_3d_cube_noise(0, noise_t1, ierr, 1)
+      call sel_const_3d_cube_noise(noise_t1, 1)
+      call sel_input_3d_cube_noise(0, noise_t1, ierr)
+      call sel_output_3d_cube_noise(noise_t1)
       call dealloc_3d_cube_noise(noise_t1)
       if(ierr .gt. 0) write(*,*) e_message
 !
