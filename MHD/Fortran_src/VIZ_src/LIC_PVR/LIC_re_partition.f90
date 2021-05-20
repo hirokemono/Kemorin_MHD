daacc8994a780a87b7edb5b3d56a7d64b5116145
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-05-19 18:30:20 -0700

    Add forgotten initialization

--- MHD/Fortran_src/VIZ_src/LIC_PVR/LIC_re_partition.f90
+++ MHD/Fortran_src/VIZ_src/LIC_PVR/LIC_re_partition.f90
@@ -49,6 +49,7 @@
       use parallel_FEM_mesh_init
       use set_normal_vectors
       use const_element_comm_tables
+      use const_jacobians_3d
 !
       type(volume_partioning_param), intent(in) :: repart_p
       type(next_nod_ele_table), intent(in) :: next_tbl
@@ -61,16 +62,18 @@
       type(jacobians_type) :: jac_viz
 !
 !
-        if(iflag_VIZ_time) call start_elapsed_time(ist_elapsed_VIZ+5)
-        call load_or_const_new_partition(repart_p, geofem, next_tbl,    &
-     &      viz_fem, mesh_to_viz_tbl)
+      if(iflag_VIZ_time) call start_elapsed_time(ist_elapsed_VIZ+5)
+      call load_or_const_new_partition(repart_p, geofem, next_tbl,      &
+     &    viz_fem, mesh_to_viz_tbl)
 !
-        if(iflag_debug.eq.1) write(*,*) 'FEM_mesh_initialization LIC'
-        call FEM_mesh_initialization(viz_fem%mesh, viz_fem%group)
-        if(iflag_debug.eq.1) write(*,*) 'surf_jacobian_sf_grp_normal'
-        call set_max_integration_points(ione, jac_viz%g_FEM)
-        call surf_jacobian_sf_grp_normal(my_rank, nprocs,               &
-     &      viz_fem%mesh, viz_fem%group, spfs_T, jac_viz)
+      if(iflag_debug.eq.1) write(*,*) 'FEM_mesh_initialization LIC'
+      call FEM_mesh_initialization(viz_fem%mesh, viz_fem%group)
+      call set_max_integration_points(ione, jac_viz%g_FEM)
+      call initialize_FEM_integration                                   &
+     &   (jac_viz%g_FEM, spfs_T%spf_3d, spfs_T%spf_2d, spfs_T%spf_1d)
+      if(iflag_debug.eq.1) write(*,*) 'surf_jacobian_sf_grp_normal'
+      call surf_jacobian_sf_grp_normal(my_rank, nprocs,                 &
+     &    viz_fem%mesh, viz_fem%group, spfs_T, jac_viz)
 !
       end subroutine s_LIC_re_partition
 !
