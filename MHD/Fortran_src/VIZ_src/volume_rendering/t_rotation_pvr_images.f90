a703e4086433deb93753b381bc2010bfc1e92749
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-05-21 12:11:47 -0700

    Add debug write

--- MHD/Fortran_src/VIZ_src/volume_rendering/t_rotation_pvr_images.f90
+++ MHD/Fortran_src/VIZ_src/volume_rendering/t_rotation_pvr_images.f90
@@ -63,6 +63,14 @@
      &     (pvr_rgb%num_pixels, rot_imgs%rot_pvr_rgb(i_rot))
       end do
 !
+      if(my_rank .gt. 0) return
+      do i_rot = view_param%istart_rot, view_param%iend_rot
+        write(*,*) i_rot, 'rot_pvr_rgb%irank_image_file', &
+     &                  rot_pvr_rgb%irank_image_file(i_rot), &
+     &                  rot_pvr_rgb%irank_end_composit(i_rot), &
+     &                  rot_pvr_rgb%npe_img_composit(i_rot)
+      end do
+!
       end subroutine init_rot_pvr_image_arrays
 !
 !  ---------------------------------------------------------------------
