c0525b86e1f9d099e608821c5d367d31a2100025
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-05-24 22:45:14 -0700

    Move sel_write_pvr_image_files into set_sequence_rgb_data

--- MHD/Fortran_src/VIZ_src/volume_rendering/rendering_streo_vr_image.f90
+++ MHD/Fortran_src/VIZ_src/volume_rendering/rendering_streo_vr_image.f90
@@ -58,7 +58,6 @@
       use m_elapsed_labels_4_VIZ
       use cal_pvr_modelview_mat
       use write_PVR_image
-      use set_parallel_file_name
       use output_image_sel_4_png
 !
       integer(kind = kint), intent(in) :: istep_pvr
@@ -72,13 +71,11 @@
       type(PVR_control_params), intent(inout) :: pvr_param
       type(PVR_projection_data), intent(inout) :: pvr_proj
 !
-      character(len=kchara) :: file_tmp, file_w_step
       integer(kind = kint) :: i_rot, iflag_img_fmt
       type(rotation_pvr_images) :: rot_imgs1
       type(MPI_quilt_bitmap_IO) :: quilt_d1
 !
 !
-      if(my_rank .eq. 0) write(*,*) 'init_rot_pvr_image_arrays'
       call init_rot_pvr_image_arrays                                    &
      &   (pvr_param%view, pvr_rgb, rot_imgs1)
 !
@@ -89,13 +86,9 @@
         call rendering_at_once(istep_pvr, time, mesh, group, field_pvr, &
      &      pvr_param, pvr_proj, rot_imgs1%rot_pvr_rgb(i_rot))
       end do
-      call calypso_mpi_barrier
-!
-      call calypso_mpi_barrier
-      call set_sequence_rgb_data                                        &
-     &   (pvr_param%view, rot_imgs1%rot_pvr_rgb, quilt_d1)
-!
       if(iflag_PVR_time) call end_elapsed_time(ist_elapsed_PVR+1)
+!
+!
       if(iflag_PVR_time) call start_elapsed_time(ist_elapsed_PVR+2)
       if(pvr_param%view%iflag_movie_fmt .eq. iflag_UNDEFINED) then
         iflag_img_fmt = pvr_rgb%id_pvr_file_type
@@ -103,25 +96,13 @@
         iflag_img_fmt = pvr_param%view%iflag_movie_fmt
       end if
 !
-      if(iflag_img_fmt .eq. iflag_QUILT_BMP) then
-        write(file_tmp,'(2a)') trim(pvr_rgb%pvr_prefix), '_quilt'
-      else
-        file_tmp = pvr_rgb%pvr_prefix
-      end if
-!
-      if(istep_pvr .ge. 0) then
-        file_w_step = add_int_suffix(istep_pvr, file_tmp)
-      else
-        file_w_step = file_tmp
-      end if
-!
-      call calypso_mpi_barrier
-      call sel_write_pvr_image_files                                    &
-     &   (iflag_img_fmt, file_w_step, quilt_d1)
-      if(iflag_PVR_time) call end_elapsed_time(ist_elapsed_PVR+2)
-      if(iflag_PVR_time) call start_elapsed_time(ist_elapsed_PVR+1)
-!
+      call set_sequence_rgb_data(istep_pvr, iflag_img_fmt,              &
+     &    pvr_rgb%pvr_prefix, pvr_param%view, rot_imgs1%rot_pvr_rgb,    &
+     &    quilt_d1)
       call dealloc_quilt_rgb_images(quilt_d1)
+      if(iflag_PVR_time) call end_elapsed_time(ist_elapsed_PVR+2)
+!
+      if(iflag_PVR_time) call start_elapsed_time(ist_elapsed_PVR+1)
       call dealloc_rot_pvr_image_arrays(pvr_param%view, rot_imgs1)
 !
       end subroutine rendering_with_rotation
@@ -170,9 +151,8 @@
      &      pvr_param, pvr_proj(2), rot_imgs1%rot_pvr_rgb(i_rot))
         call add_left_eye_image(rot_imgs1%rot_pvr_rgb(i_rot))
       end do
-      call calypso_mpi_barrier
-!
       if(iflag_PVR_time) call end_elapsed_time(ist_elapsed_PVR+1)
+!
       if(iflag_PVR_time) call start_elapsed_time(ist_elapsed_PVR+2)
       do i_rot = 1, pvr_param%view%num_frame
         call sel_write_pvr_image_file                                   &
@@ -187,17 +167,25 @@
 !  ---------------------------------------------------------------------
 !  ---------------------------------------------------------------------
 !
-      subroutine set_sequence_rgb_data(view_param, rot_rgb, quilt_d)
+      subroutine set_sequence_rgb_data(istep_pvr, iflag_img_fmt,        &
+     &          file_prefix, view_param, rot_rgb, quilt_d)
 !
       use t_pvr_image_array
       use t_MPI_quilt_bitmap_IO
       use convert_real_rgb_2_bite
+      use set_parallel_file_name
+      use output_image_sel_4_png
 !
-      type(pvr_view_parameter), intent(inout) :: view_param
+      integer(kind = kint), intent(in) :: istep_pvr
+      integer(kind = kint), intent(in) :: iflag_img_fmt
+      character(len=kchara), intent(in) :: file_prefix
+      type(pvr_view_parameter), intent(in) :: view_param
       type(pvr_image_type), intent(in) :: rot_rgb(view_param%num_frame)
       type(MPI_quilt_bitmap_IO), intent(inout) :: quilt_d
 !
       integer(kind = kint) :: i_rot, icou
+      character(len=kchara) :: file_tmp, file_w_step
+!
 !
       quilt_d%n_column(1) = view_param%n_row
       quilt_d%n_column(2) = view_param%n_column
@@ -209,7 +197,6 @@
       end do
       quilt_d%num_image_lc = icou
 !
-      call calypso_mpi_barrier
       call alloc_quilt_rgb_images                                       &
      &   (rot_rgb(1)%num_pixels(1), rot_rgb(1)%num_pixels(2), quilt_d)
 !
@@ -223,6 +210,21 @@
      &        quilt_d%images(icou)%rgb(1,1,1))
         end if
       end do
+!
+      if(iflag_img_fmt .eq. iflag_QUILT_BMP) then
+        write(file_tmp,'(2a)') trim(file_prefix), '_quilt'
+      else
+        file_tmp = file_prefix
+      end if
+!
+      if(istep_pvr .ge. 0) then
+        file_w_step = add_int_suffix(istep_pvr, file_tmp)
+      else
+        file_w_step = file_tmp
+      end if
+!
+      call sel_write_pvr_image_files                                    &
+     &   (iflag_img_fmt, file_w_step, quilt_d)
 !
       end subroutine set_sequence_rgb_data
 !
