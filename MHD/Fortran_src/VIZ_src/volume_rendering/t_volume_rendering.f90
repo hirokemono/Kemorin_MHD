b709effcf726578b9f25ea0183b1e900e7d35655
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-05-27 00:38:50 -0700

    Split each_PVR_rendering for anaglyph

--- MHD/Fortran_src/VIZ_src/volume_rendering/t_volume_rendering.f90
+++ MHD/Fortran_src/VIZ_src/volume_rendering/t_volume_rendering.f90
@@ -345,7 +345,7 @@
       type(volume_rendering_module), intent(inout) :: pvr
 !
       integer(kind = kint) :: i_pvr, ist_rdr
-      integer(kind = kint) :: i_img, ist_img, ied_img
+      integer(kind = kint) :: i_img, ist_img, ied_img, num_img
 !
 !
       if(pvr%num_pvr.le.0 .or. istep_pvr.le.0) return
@@ -356,11 +356,15 @@
      &                                 .ne. IFLAG_NO_MOVIE) cycle
 !
         ist_rdr = pvr%istack_pvr_render(i_pvr-1) + 1
-        ist_img = pvr%istack_pvr_images(i_pvr-1) + 1
-        call each_PVR_rendering                                         &
-     &     (istep_pvr, time, geofem, jacs, nod_fld,                     &
+        ist_img = pvr%istack_pvr_images(i_pvr-1)
+        num_img = pvr%istack_pvr_images(i_pvr  )
+        if(iflag_debug .gt. 0) write(*,*) 'cal_field_4_pvr'
+        call cal_field_4_each_pvr(geofem%mesh%node, geofem%mesh%ele,    &
+     &      jacs%g_FEM, jacs%jac_3d, nod_fld,                           &
+     &      pvr%pvr_param(i_pvr)%field_def, pvr%field_pvr(i_pvr))
+        call each_PVR_rendering(istep_pvr, time, num_img, geofem,       &
      &      pvr%field_pvr(i_pvr), pvr%pvr_param(i_pvr),                 &
-     &      pvr%pvr_proj(ist_rdr), pvr%pvr_rgb(ist_img))
+     &      pvr%pvr_proj(ist_rdr), pvr%pvr_rgb(ist_img+1))
       end do
       if(iflag_PVR_time) call end_elapsed_time(ist_elapsed_PVR+1)
 !
@@ -440,8 +444,10 @@
 !
         ist_rdr = pvr%istack_pvr_render(i_pvr-1) + 1
         ist_img = pvr%istack_pvr_images(i_pvr-1) + 1
-        call each_PVR_rendering                                         &
-     &     (istep_pvr, time, geofem, jacs, nod_fld,                     &
+        call cal_field_4_each_pvr(geofem%mesh%node, geofem%mesh%ele,    &
+     &      jacs%g_FEM, jacs%jac_3d, nod_fld,                           &
+     &      pvr%pvr_param(i_pvr)%field_def, pvr%field_pvr(i_pvr))
+        call each_PVR_anaglyph(istep_pvr, time, geofem,                 &
      &      pvr%field_pvr(i_pvr), pvr%pvr_param(i_pvr),                 &
      &      pvr%pvr_proj(ist_rdr), pvr%pvr_rgb(ist_img))
       end do
