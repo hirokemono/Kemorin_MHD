067023fb8c183a26b15f3ef6cbf696398121506d
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-06-02 12:49:29 -0700

    Fix wrong if statement

--- MHD/Fortran_src/PARALLEL_src/modify_MESH/checks_for_sleeve_extend.f90
+++ MHD/Fortran_src/PARALLEL_src/modify_MESH/checks_for_sleeve_extend.f90
@@ -108,7 +108,7 @@
       icou = check_idx_home_for_import(expand_nod_comm,                 &
      &    exp_import_xx%irank_comm, trim_nod_to_ext%idx_extend_to_trim)
       call calypso_mpi_reduce_one_int(icou, ntot_gl, MPI_SUM, 0)
-      if(iflag_debug .eq. 0) write(*,*) 'Number of Wrong address ',     &
+      if(iflag_debug .gt. 0) write(*,*) 'Number of Wrong address ',     &
      &         'in trim_nod_to_ext%idx_extend_to_trim ', ntot_gl
 !
       end subroutine check_appended_node_data
