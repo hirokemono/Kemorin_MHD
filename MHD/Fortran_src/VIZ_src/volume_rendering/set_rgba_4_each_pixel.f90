13e4e97172bebfa9efc639affef1a172228262e0
Hiroaki Matsui <matsui@Kemo-Mac-2018.local>
2021-07-11 12:15:03 -0700

    Reverse opacity evaluation

--- MHD/Fortran_src/VIZ_src/volume_rendering/set_rgba_4_each_pixel.f90
+++ MHD/Fortran_src/VIZ_src/volume_rendering/set_rgba_4_each_pixel.f90
@@ -103,8 +103,11 @@
      &    grad, color_param%pvr_lighting_real,                          &
      &    x4in_model(1), x4out_model(1), color, rgb(1))
 !
-      rgb(1:3) = rgb(1:3) * opa_current * ray_length
-      rgb(4) =   opa_current*ray_length
+!      rgb(4) = 1.0d0                                                   &
+!     &        - (1.0d0 - opa_current)**(ray_length)
+!      rgb(4) = 1.0d0 - ray_length * LOG(1.0d0 - opa_current)
+      rgb(4) = ray_length * opa_current
+      rgb(1:3) = rgb(1:3) * rgb(4)
       if(rgb(4) .gt. one) rgb(4) = one
       if(rgb(4) .lt. zero) rgb(4) = zero
 !
