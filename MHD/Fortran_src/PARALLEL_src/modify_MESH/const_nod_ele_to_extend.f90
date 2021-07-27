c1800db70819e5098838c06fa1d34ab7725f0e0a
Hiroaki Matsui <h_kemono@mac.com>
2021-07-27 11:35:39 -0700

    use structure for subroutine access

--- MHD/Fortran_src/PARALLEL_src/modify_MESH/const_nod_ele_to_extend.f90
+++ MHD/Fortran_src/PARALLEL_src/modify_MESH/const_nod_ele_to_extend.f90
@@ -292,17 +292,15 @@
      &                          pe_list_extend%irank_dist_recv, SR_sig)
 !
       call alloc_istack_set_import_recv(grp_list_export)
-      call count_istack_set_import_recv(nod_comm, maxpe_dist_send,      &
-     &    pe_list_extend%npe_dist_recv, pe_list_extend%irank_dist_recv, &
-     &    grp_list_export%nset_import_recv,                             &
-     &    grp_list_export%istack_set_import_recv)
+      call count_istack_set_import_recv(nod_comm, pe_list_extend,       &
+     &                          grp_list_export%nset_import_recv,       &
+     &                          grp_list_export%istack_set_import_recv)
 !
       call alloc_iset_import_recv(grp_list_export)
-      call set_iset_import_recv(nod_comm, maxpe_dist_send,              &
-     &    pe_list_extend%npe_dist_recv, pe_list_extend%irank_dist_recv, &
-     &    grp_list_export%istack_set_import_recv,                       &
-     &    grp_list_export%nset_import_recv,                             &
-     &    grp_list_export%iset_import_recv)
+      call set_iset_import_recv(nod_comm, pe_list_extend,               &
+     &                          grp_list_export%istack_set_import_recv, &
+     &                          grp_list_export%nset_import_recv,       &
+     &                          grp_list_export%iset_import_recv)
 !
       allocate(marked_export(maxpe_dist_send))
       do icou = 1, maxpe_dist_send
@@ -636,26 +634,22 @@
 !
 !  ---------------------------------------------------------------------
 !
-      subroutine count_istack_set_import_recv(nod_comm,                 &
-     &          maxpe_dist_send, npe_dist_recv, irank_dist_recv,        &
+      subroutine count_istack_set_import_recv                           &
+     &         (nod_comm, pe_list_extend,                               &
      &          nset_import_recv, istack_set_import_recv)
 !
       type(communication_table), intent(in) :: nod_comm
-      integer(kind = kint), intent(in) :: maxpe_dist_send
-      integer(kind = kint), intent(in)      &
-     &   :: npe_dist_recv(nod_comm%num_neib)
-      integer(kind = kint), intent(in)     &
-     &   :: irank_dist_recv(maxpe_dist_send,nod_comm%num_neib)
+      type(pe_list_for_marks_extend), intent(in) :: pe_list_extend
 !
       integer(kind = kint), intent(inout) :: nset_import_recv(nprocs)
-      integer(kind = kint), intent(inout)                   &
+      integer(kind = kint), intent(inout)                               &
      &      :: istack_set_import_recv(0:nprocs)
 !
       integer(kind = kint) :: ip, icou, jp
 !
       do ip = 1, nod_comm%num_neib
-        do icou = 1, npe_dist_recv(ip)
-          jp = irank_dist_recv(icou,ip) + 1
+        do icou = 1, pe_list_extend%npe_dist_recv(ip)
+          jp = pe_list_extend%irank_dist_recv(icou,ip) + 1
           nset_import_recv(jp) = nset_import_recv(jp) + 1
         end do
       end do
@@ -669,18 +663,13 @@
 !
 !  ---------------------------------------------------------------------
 !
-      subroutine set_iset_import_recv(nod_comm,                         &
-     &          maxpe_dist_send, npe_dist_recv, irank_dist_recv,        &
-     &          istack_set_import_recv, nset_import_recv,               &
-     &          iset_import_recv)
+      subroutine set_iset_import_recv                                   &
+     &         (nod_comm, pe_list_extend, istack_set_import_recv,       &
+     &          nset_import_recv, iset_import_recv)
 !
       type(communication_table), intent(in) :: nod_comm
-      integer(kind = kint), intent(in) :: maxpe_dist_send
-      integer(kind = kint), intent(in)      &
-     &   :: npe_dist_recv(nod_comm%num_neib)
-      integer(kind = kint), intent(in)     &
-     &   :: irank_dist_recv(maxpe_dist_send,nod_comm%num_neib)
-      integer(kind = kint), intent(in)                   &
+      type(pe_list_for_marks_extend), intent(in) :: pe_list_extend
+      integer(kind = kint), intent(in)                                  &
      &      :: istack_set_import_recv(0:nprocs)
 !
       integer(kind = kint), intent(inout) :: nset_import_recv(nprocs)
@@ -693,8 +682,8 @@
       nset_import_recv(1:nprocs) = 0
 !$omp end parallel workshare
       do ip = 1, nod_comm%num_neib
-        do icou = 1, npe_dist_recv(ip)
-          jp = irank_dist_recv(icou,ip) + 1
+        do icou = 1, pe_list_extend%npe_dist_recv(ip)
+          jp = pe_list_extend%irank_dist_recv(icou,ip) + 1
 !
           nset_import_recv(jp) = nset_import_recv(jp) + 1
           jcou = nset_import_recv(jp) + istack_set_import_recv(jp-1)
