eeef6433063e74a2a29f46c64bca64ad3f414165
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2022-11-18 18:43:28 -0800

    use header structure

--- MHD/programs/data_utilities/sph_spectr/write_sph_monitor_data.f90
+++ MHD/programs/data_utilities/sph_spectr/write_sph_monitor_data.f90
@@ -9,15 +9,15 @@
 !!
 !!@verbatim
 !!      subroutine select_output_sph_pwr_head                           &
-!!     &         (id_file, flag_vol_ave, sph_IN)
+!!     &         (id_file, flag_vol_ave, sph_OUT)
 !!        integer(kind = kint), intent(in) :: id_file
 !!        logical, intent(in) :: flag_vol_ave
-!!        type(read_sph_spectr_data), intent(inout) :: sph_IN
+!!        type(read_sph_spectr_data), intent(inout) :: sph_OUT
 !!      subroutine select_output_sph_series_data                        &
-!!     &         (id_file, flag_spectr, flag_vol_ave, sph_IN)
+!!     &         (id_file, flag_spectr, flag_vol_ave, sph_OUT)
 !!        integer(kind = kint), intent(in) :: id_file
 !!        logical, intent(in) :: flag_spectr, flag_vol_ave
-!!        type(read_sph_spectr_data), intent(in) :: sph_IN
+!!        type(read_sph_spectr_data), intent(in) :: sph_OUT
 !!@endverbatim
 !
       module write_sph_monitor_data
@@ -40,17 +40,19 @@
 !   --------------------------------------------------------------------
 !
       subroutine select_output_sph_pwr_head                             &
-     &         (id_file, flag_vol_ave, sph_IN)
+     &         (id_file, flag_vol_ave, sph_OUT)
 !
       integer(kind = kint), intent(in) :: id_file
       logical, intent(in) :: flag_vol_ave
-      type(read_sph_spectr_data), intent(inout) :: sph_IN
+      type(read_sph_spectr_data), intent(inout) :: sph_OUT
 !
+!
+      call sph_mean_squre_header_labels(sph_OUT)
 !
       if(flag_vol_ave) then
-        call write_sph_pwr_vol_head(id_file, sph_IN)
+        call write_sph_pwr_vol_head(id_file, sph_OUT)
       else
-        call write_sph_pwr_layer_head(id_file, sph_IN)
+        call write_sph_pwr_layer_head(id_file, sph_OUT)
       end if
 !
       end subroutine select_output_sph_pwr_head
@@ -58,24 +60,24 @@
 !   --------------------------------------------------------------------
 !
       subroutine select_output_sph_series_data                          &
-     &         (id_file, flag_spectr, flag_vol_ave, sph_IN)
+     &         (id_file, flag_spectr, flag_vol_ave, sph_OUT)
 !
       integer(kind = kint), intent(in) :: id_file
       logical, intent(in) :: flag_spectr, flag_vol_ave
-      type(read_sph_spectr_data), intent(in) :: sph_IN
+      type(read_sph_spectr_data), intent(in) :: sph_OUT
 !
 !
       if(flag_spectr) then
         if(flag_vol_ave) then
-          call write_vol_spectr_data(id_file, sph_IN)
+          call write_vol_spectr_data(id_file, sph_OUT)
         else
-          call write_layer_spectr_data(id_file, sph_IN)
+          call write_layer_spectr_data(id_file, sph_OUT)
         end if
       else
         if(flag_vol_ave) then
-          call write_vol_sph_data(id_file, sph_IN)
+          call write_vol_sph_data(id_file, sph_OUT)
         else
-          call write_layer_sph_data(id_file, sph_IN)
+          call write_layer_sph_data(id_file, sph_OUT)
         end if
       end if
 !
@@ -105,21 +107,21 @@
 !   --------------------------------------------------------------------
 !   --------------------------------------------------------------------
 !
-      subroutine write_vol_sph_data(id_file, sph_IN)
+      subroutine write_vol_sph_data(id_file, sph_OUT)
 !
       use write_field_labels
 !
       integer(kind = kint), intent(in) :: id_file
-      type(read_sph_spectr_data), intent(in) :: sph_IN
+      type(read_sph_spectr_data), intent(in) :: sph_OUT
 !
       integer(kind = kint) :: i
 !
 !
       write(id_file,'(i16,1pE25.15e3)', ADVANCE='NO')                   &
-     &                          sph_IN%i_step, sph_IN%time
-      do i = 1, sph_IN%ntot_sph_spec
+     &                          sph_OUT%i_step, sph_OUT%time
+      do i = 1, sph_OUT%ntot_sph_spec
         write(id_file,'(1pE25.15e3)', ADVANCE='NO')                     &
-     &                           sph_IN%spectr_IO(i,0,1)
+     &                           sph_OUT%spectr_IO(i,0,1)
       end do
       write(id_file,'(a)')
 !
@@ -127,21 +129,21 @@
 !
 !   --------------------------------------------------------------------
 !
-      subroutine write_vol_spectr_data(id_file, sph_IN)
+      subroutine write_vol_spectr_data(id_file, sph_OUT)
 !
       integer(kind = kint), intent(in) :: id_file
-      type(read_sph_spectr_data), intent(in) :: sph_IN
+      type(read_sph_spectr_data), intent(in) :: sph_OUT
 !
       integer(kind = kint) :: lth
       character(len=kchara) :: fmt_txt
 !
 !
       write(fmt_txt,'(a20,i5,a16)')  '(i16,1pE25.15e3,i16,',            &
-     &                     sph_IN%ntot_sph_spec, '(1p255E25.15e3))'
-      do lth = 0, sph_IN%ltr_sph
+     &                     sph_OUT%ntot_sph_spec, '(1p255E25.15e3))'
+      do lth = 0, sph_OUT%ltr_sph
         write(id_file,fmt_txt)                                          &
-     &               sph_IN%i_step, sph_IN%time, sph_IN%i_mode(lth),    &
-     &               sph_IN%spectr_IO(1:sph_IN%ntot_sph_spec,lth,1)
+     &               sph_OUT%i_step, sph_OUT%time, sph_OUT%i_mode(lth), &
+     &               sph_OUT%spectr_IO(1:sph_OUT%ntot_sph_spec,lth,1)
       end do
 !
       end subroutine write_vol_spectr_data
@@ -149,31 +151,31 @@
 !   --------------------------------------------------------------------
 !   --------------------------------------------------------------------
 !
-      subroutine write_layer_sph_data(id_file, sph_IN)
+      subroutine write_layer_sph_data(id_file, sph_OUT)
 !
       integer(kind = kint), intent(in) :: id_file
-      type(read_sph_spectr_data), intent(in) :: sph_IN
+      type(read_sph_spectr_data), intent(in) :: sph_OUT
 !
       integer(kind = kint) :: kr
       character(len=kchara) :: fmt_txt
 !
 !
       write(fmt_txt,'(a31,i5,a16)')  '(i16,1pE25.15e3,i16,1pE25.15e3,', &
-     &                     sph_IN%ntot_sph_spec, '(1p255E25.15e3))'
-      do kr = 1, sph_IN%nri_sph
-        write(id_file,fmt_txt) sph_IN%i_step, sph_IN%time,              &
-     &         sph_IN%kr_sph(kr), sph_IN%r_sph(kr),                     &
-     &         sph_IN%spectr_IO(1:sph_IN%ntot_sph_spec,0,kr)
+     &                     sph_OUT%ntot_sph_spec, '(1p255E25.15e3))'
+      do kr = 1, sph_OUT%nri_sph
+        write(id_file,fmt_txt) sph_OUT%i_step, sph_OUT%time,            &
+     &         sph_OUT%kr_sph(kr), sph_OUT%r_sph(kr),                   &
+     &         sph_OUT%spectr_IO(1:sph_OUT%ntot_sph_spec,0,kr)
       end do
 !
       end subroutine write_layer_sph_data
 !
 !   --------------------------------------------------------------------
 !
-      subroutine write_layer_spectr_data(id_file, sph_IN)
+      subroutine write_layer_spectr_data(id_file, sph_OUT)
 !
       integer(kind = kint), intent(in) :: id_file
-      type(read_sph_spectr_data), intent(in) :: sph_IN
+      type(read_sph_spectr_data), intent(in) :: sph_OUT
 !
       integer(kind = kint) :: kr, lth
       character(len=kchara) :: fmt_txt
@@ -181,12 +183,12 @@
 !
       write(fmt_txt,'(a35,i5,a16)')                                     &
      &            '(i16,1pE25.15e3,i16,1pE25.15e3,i16,',                &
-     &              sph_IN%ntot_sph_spec, '(1p255E25.15e3))'
-      do kr = 1, sph_IN%nri_sph
-        do lth = 0, sph_IN%ltr_sph
-          write(id_file,fmt_txt) sph_IN%i_step, sph_IN%time,            &
-     &         sph_IN%kr_sph(kr), sph_IN%r_sph(kr), sph_IN%i_mode(lth), &
-     &         sph_IN%spectr_IO(1:sph_IN%ntot_sph_spec,lth,kr)
+     &              sph_OUT%ntot_sph_spec, '(1p255E25.15e3))'
+      do kr = 1, sph_OUT%nri_sph
+        do lth = 0, sph_OUT%ltr_sph
+          write(id_file,fmt_txt) sph_OUT%i_step, sph_OUT%time,          &
+     &      sph_OUT%kr_sph(kr), sph_OUT%r_sph(kr), sph_OUT%i_mode(lth), &
+     &      sph_OUT%spectr_IO(1:sph_OUT%ntot_sph_spec,lth,kr)
         end do
       end do
 !
@@ -195,63 +197,39 @@
 !   --------------------------------------------------------------------
 !   --------------------------------------------------------------------
 !
-      subroutine write_sph_pwr_vol_head(id_file, sph_IN)
+      subroutine write_sph_pwr_vol_head(id_file, sph_OUT)
+!
+      use sph_power_spectr_data_text
 !
       integer(kind = kint), intent(in) :: id_file
-      type(read_sph_spectr_data), intent(in) :: sph_IN
+      type(read_sph_spectr_data), intent(in) :: sph_OUT
 !
-      integer(kind = kint) :: i
+      integer(kind = kint) :: len_each(6)
+      integer(kind = kint) :: len_tot
 !
 !
-      write(id_file,'(a)')    'radial_layers, truncation'
-      write(id_file,'(2i16)') sph_IN%nri_sph, sph_IN%ltr_sph
-      write(id_file,'(a)')    'ICB_id, CMB_id'
-      write(id_file,'(2i16)') sph_IN%kr_ICB, sph_IN%kr_CMB
-      write(id_file,'(a)')    'Lower boundary'
-      write(id_file,'(i16,1pe23.15e3)') sph_IN%kr_inner, sph_IN%r_inner
-      write(id_file,'(a)')    'Upper boundary'
-      write(id_file,'(i16,1pe23.15e3)') sph_IN%kr_outer, sph_IN%r_outer
-      write(id_file,'(a)')    'number of components'
-      write(id_file,'(5i16)')                                           &
-     &      sph_IN%nfield_sph_spec, sph_IN%ntot_sph_spec
-      write(id_file,'(16i5)')                                           &
-     &      sph_IN%ncomp_sph_spec(1:sph_IN%nfield_sph_spec)
-!
-      do i = 1, sph_IN%num_labels
-        write(id_file,'(2a)',advance='no')                              &
-     &            trim(sph_IN%ene_sph_spec_name(i)), '    '
-      end  do
-      write(id_file,*)
+      call len_sph_vol_spectr_header(sph_OUT, len_each, len_tot)
+      write(id_file,'(a)',ADVANCE='NO')                                 &
+     &       sph_vol_spectr_header_text(len_tot, len_each, sph_OUT)
 !
       end subroutine write_sph_pwr_vol_head
 !
 !   --------------------------------------------------------------------
 !
-      subroutine write_sph_pwr_layer_head(id_file, sph_IN)
+      subroutine write_sph_pwr_layer_head(id_file, sph_OUT)
 !
-      use skip_comment_f
+      use sph_power_spectr_data_text
 !
       integer(kind = kint), intent(in) :: id_file
-      type(read_sph_spectr_data), intent(in) :: sph_IN
+      type(read_sph_spectr_data), intent(in) :: sph_OUT
 !
-      integer(kind = kint) :: i
+      integer(kind = kint) :: len_each(6)
+      integer(kind = kint) :: len_tot
 !
 !
-      write(id_file,'(a)')    'radial_layers, truncation'
-      write(id_file,'(3i16)') sph_IN%nri_sph, sph_IN%ltr_sph
-      write(id_file,'(a)')    'ICB_id, CMB_id'
-      write(id_file,'(2i16)') sph_IN%kr_ICB, sph_IN%kr_CMB
-      write(id_file,'(a)')  'Number_of_field, Number_of_components'
-      write(id_file,'(2i16)')                                           &
-     &      sph_IN%nfield_sph_spec, sph_IN%ntot_sph_spec
-      write(id_file,'(16i5)')                                           &
-     &      sph_IN%ncomp_sph_spec(1:sph_IN%nfield_sph_spec)
-!
-      do i = 1, sph_IN%num_labels
-        write(id_file,'(2a)',advance='no')                              &
-     &            trim(sph_IN%ene_sph_spec_name(i)), '    '
-      end  do
-      write(id_file,*)
+      call len_sph_layer_spectr_header(sph_OUT, len_each, len_tot)
+      write(id_file,'(a)',ADVANCE='NO')                                 &
+     &      sph_layer_spectr_header_text(len_tot, len_each, sph_OUT)
 !
       end subroutine write_sph_pwr_layer_head
 !
