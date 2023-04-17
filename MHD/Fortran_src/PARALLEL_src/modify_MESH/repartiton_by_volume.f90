5d601f7eb8211146595fe6981a6d2c706f324099
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2023-04-17 00:37:04 -0700

    Temporal commit

--- MHD/Fortran_src/PARALLEL_src/modify_MESH/repartiton_by_volume.f90
+++ MHD/Fortran_src/PARALLEL_src/modify_MESH/repartiton_by_volume.f90
@@ -70,6 +70,13 @@
 !
       use t_next_node_ele_4_node
       use t_interpolate_table
+
+      use t_para_double_numbering
+      use calypso_SR_type
+      use solver_SR_type
+      use reverse_SR_int
+      use quicksort
+      use cal_minmax_and_stacks
 !
       use m_file_format_switch
       use m_elapsed_labels_4_REPART
@@ -111,10 +118,24 @@
       type(mesh_SR), intent(inout) :: m_SR
 !
       type(communication_table) :: new_ele_comm
-      type(calypso_comm_table) :: repart_ele_tbl
+      type(calypso_comm_table) :: repart_ele_tbl, repart_ele_tbl_2
       integer(kind = kint) :: ierr
       integer :: nnod_tot_org, nele_tot_org, nnod_tot_new, nele_tot_new
       integer :: nele_ele_tbl
+!
+      type(node_ele_double_number) :: inod_dbl_org, iele_dbl_org
+      type(node_ele_double_number) :: inod_dbl_new, iele_dbl_new
+      type(node_ele_double_number) :: inod_dbl_org_on_new
+      type(node_ele_double_number) :: iele_dbl_org_on_new
+      integer(kind = kint), allocatable :: ip_recv_tmp(:)
+      integer(kind = kint), allocatable :: ip_send_tmp(:)
+      integer(kind = kint), allocatable :: num_recv_tmp(:)
+      integer(kind = kint), allocatable :: num_send_tmp(:)
+      integer(kind = kint), allocatable :: iele_sort(:)
+      integer(kind = kint_gl), allocatable :: iele_gl_org(:)
+      integer(kind = kint) :: iele, ist, ied, num, ip, irank, inum
+      integer(kind = kint) :: iele_org, irank_org
+      integer(kind = kint) ::icou_recv, icou_send
 !
 !  -------------------------------
 !
@@ -157,7 +178,177 @@
      &      new_mesh, new_group, new_ele_comm, sleeve_exp_WK, m_SR)
         if(iflag_RPRT_time) call end_elapsed_time(ist_elapsed_RPRT+3)
       end if
+      call calypso_mpi_barrier
+!
+!  ----------------
+!
+      call alloc_double_numbering(mesh%node%numnod, inod_dbl_org)
+      call alloc_double_numbering(mesh%ele%numele, iele_dbl_org)
+      call set_node_ele_double_address                                  &
+     &   (mesh%node, mesh%ele, mesh%nod_comm, ele_comm,                 &
+     &    inod_dbl_org, iele_dbl_org, m_SR%SR_sig, m_SR%SR_i)
+!
+      call alloc_double_numbering(new_mesh%node%numnod, inod_dbl_new)
+      call alloc_double_numbering(new_mesh%ele%numele, iele_dbl_new)
+      call set_node_ele_double_address                                  &
+     &   (new_mesh%node, new_mesh%ele, new_mesh%nod_comm, new_ele_comm, &
+     &    inod_dbl_new, iele_dbl_new, m_SR%SR_sig, m_SR%SR_i)
+!
+      call alloc_double_numbering(new_mesh%node%numnod,                 &
+     &                            inod_dbl_org_on_new)
+      call calypso_SR_type_int(iflag_import_item, repart_nod_tbl,       &
+     &    mesh%node%numnod, new_mesh%node%numnod,                       &
+     &    inod_dbl_org%index(1), inod_dbl_org_on_new%index(1),          &
+     &    m_SR%SR_sig, m_SR%SR_i)
+      call calypso_SR_type_int(iflag_import_item, repart_nod_tbl,       &
+     &    mesh%node%numnod, new_mesh%node%numnod,                       &
+     &    inod_dbl_org%irank(1), inod_dbl_org_on_new%irank(1),          &
+     &    m_SR%SR_sig, m_SR%SR_i)
+      call SOLVER_SEND_RECV_int_type                                    &
+     &   (new_mesh%node%numnod, new_mesh%nod_comm,                      &
+     &    m_SR%SR_sig, m_SR%SR_i, inod_dbl_org_on_new%index(1))
+      call SOLVER_SEND_RECV_int_type                                    &
+     &   (new_mesh%node%numnod, new_mesh%nod_comm,                      &
+     &    m_SR%SR_sig, m_SR%SR_i, inod_dbl_org_on_new%irank(1))
+!
+      call alloc_double_numbering(new_mesh%ele%numele,                  &
+     &                            iele_dbl_org_on_new)
+      call calypso_SR_type_int(iflag_import_item, repart_ele_tbl,       &
+     &    mesh%ele%numele, new_mesh%ele%numele,                         &
+     &    iele_dbl_org%index(1), iele_dbl_org_on_new%index(1),          &
+     &    m_SR%SR_sig, m_SR%SR_i)
+      call calypso_SR_type_int(iflag_import_item, repart_ele_tbl,       &
+     &    mesh%ele%numele, new_mesh%ele%numele,                         &
+     &    iele_dbl_org%irank(1), iele_dbl_org_on_new%irank(1),          &
+     &    m_SR%SR_sig, m_SR%SR_i)
+!
+      call SOLVER_SEND_RECV_int_type                                    &
+     &   (new_mesh%ele%numele, new_ele_comm,                            &
+     &    m_SR%SR_sig, m_SR%SR_i, iele_dbl_org_on_new%index(1))
+      call SOLVER_SEND_RECV_int_type                                    &
+     &   (new_mesh%ele%numele, new_ele_comm,                            &
+     &    m_SR%SR_sig, m_SR%SR_i, iele_dbl_org_on_new%irank(1))
       call dealloc_comm_table(new_ele_comm)
+      call calypso_mpi_barrier
+!
+      allocate(num_recv_tmp(0:nprocs-1))
+      num_recv_tmp(0:nprocs-1) = 0
+      do iele = 1, new_mesh%ele%numele
+        irank = iele_dbl_org_on_new%irank(iele)
+        num_recv_tmp(irank) = num_recv_tmp(irank) + 1
+      end do
+!
+      allocate(num_send_tmp(0:nprocs-1))
+      num_send_tmp(0:nprocs-1) = 0
+      call calypso_mpi_alltoall_one_int                                 &
+     &   (num_recv_tmp(0), num_send_tmp(0))
+!
+      icou_recv = 0
+      icou_send = 0
+      do irank = 0, nprocs-1
+        if(num_recv_tmp(irank) .gt. 0) icou_recv = icou_recv + 1
+        if(num_send_tmp(irank) .gt. 0) icou_send = icou_send + 1
+      end do
+!
+      repart_ele_tbl_2%nrank_import = icou_recv
+      call alloc_calypso_import_num(repart_ele_tbl_2)
+!
+      repart_ele_tbl_2%nrank_export = icou_send
+      call alloc_calypso_export_num(repart_ele_tbl_2)
+!
+      allocate(ip_send_tmp(0:nprocs-1))
+      allocate(ip_recv_tmp(0:nprocs-1))
+      ip_send_tmp(0:nprocs-1) = -1
+      ip_recv_tmp(0:nprocs-1) = -1
+      icou_recv = 0
+      icou_send = 0
+      do ip = 1, nprocs
+        irank = mod(my_rank+ip,nprocs)
+        if(num_recv_tmp(irank) .gt. 0) then
+          icou_recv = icou_recv + 1
+          repart_ele_tbl_2%irank_import(icou_recv) = irank
+          repart_ele_tbl_2%num_import(icou_recv) = num_recv_tmp(irank)
+          ip_recv_tmp(irank) = icou_recv
+        end if
+        if(num_send_tmp(irank) .gt. 0) then
+          icou_send = icou_send + 1
+          repart_ele_tbl_2%irank_export(icou_send) = irank
+          repart_ele_tbl_2%num_export(icou_recv) = num_send_tmp(irank)
+          ip_send_tmp(irank) = icou_send
+        end if
+      end do
+      repart_ele_tbl_2%iflag_self_copy = 0
+      if(num_recv_tmp(my_rank) .gt. 0) then
+        repart_ele_tbl_2%iflag_self_copy = 1
+      end if
+!
+      call s_cal_total_and_stacks(repart_ele_tbl_2%nrank_import,        &
+     &                            repart_ele_tbl_2%num_import, izero,   &
+     &                            repart_ele_tbl_2%istack_import,       &
+     &                            repart_ele_tbl_2%ntot_import)
+      call s_cal_total_and_stacks(repart_ele_tbl_2%nrank_export,        &
+     &                            repart_ele_tbl_2%num_export, izero,   &
+     &                            repart_ele_tbl_2%istack_export,       &
+     &                            repart_ele_tbl_2%ntot_export)
+!
+      go to 100
+      call alloc_calypso_import_item(repart_ele_tbl_2)
+      call alloc_calypso_import_rev(new_mesh%ele%numele,                &
+     &                              repart_ele_tbl_2)
+      call alloc_calypso_export_item(repart_ele_tbl_2)
+!
+!      write(*,*) my_rank, 'repart_ele_tbl_2%ntot_import', &
+!     &  repart_ele_tbl_2%ntot_import, new_mesh%ele%numele
+!
+      allocate(iele_sort(repart_ele_tbl_2%ntot_import))
+      iele_sort(1:repart_ele_tbl_2%ntot_import) = 0
+      num_recv_tmp(0:nprocs-1) = 0
+      do iele = 1, new_mesh%ele%numele
+        iele_org =  iele_dbl_org_on_new%irank(iele)
+        irank_org = iele_dbl_org_on_new%irank(iele)
+        num_recv_tmp(irank_org) = num_recv_tmp(irank_org) + 1
+        ip = ip_recv_tmp(irank_org)
+        inum = repart_ele_tbl_2%istack_import(ip-1)                     &
+     &        + num_recv_tmp(irank_org)
+        repart_ele_tbl_2%item_import(inum) = iele
+        iele_sort(inum) = iele_org
+      end do
+!
+      do ip = 1, repart_ele_tbl_2%nrank_import
+        ist = repart_ele_tbl_2%istack_import(ip-1) + 1
+        ied = repart_ele_tbl_2%istack_import(ip  )
+        if((ied-ist) .gt. 0) then
+          call quicksort_w_index(repart_ele_tbl_2%ntot_import,          &
+     &        iele_sort, ist, ied, repart_ele_tbl_2%item_import)
+        end if
+      end do
+!
+      call calypso_mpi_barrier
+      call comm_items_send_recv                                         &
+     &   (repart_ele_tbl_2%nrank_import, repart_ele_tbl_2%irank_import, &
+     &    repart_ele_tbl_2%istack_import, iele_sort,                    &
+     &    repart_ele_tbl_2%nrank_export, repart_ele_tbl_2%irank_export, &
+     &    repart_ele_tbl_2%istack_export,                               &
+     &    repart_ele_tbl_2%iflag_self_copy,                             &
+     &    repart_ele_tbl_2%item_export, m_SR%SR_sig)
+      call calypso_mpi_barrier
+!
+      allocate(iele_gl_org(new_mesh%ele%numele))
+      iele_gl_org(1:new_mesh%ele%numele) = 0
+      call calypso_SR_type_int8(iflag_import_item, repart_ele_tbl_2,    &
+     &                          mesh%ele%numele, new_mesh%ele%numele,   &
+     &                          mesh%ele%iele_global, iele_gl_org,      &
+     &                          m_SR%SR_sig, m_SR%SR_il)
+      call calypso_mpi_barrier
+!
+
+      inum = 0
+      do iele = 1, new_mesh%ele%numele
+        if(iele_gl_org(iele) .ne. new_mesh%ele%iele_global(iele)) inum = inum+1
+      end do
+      write(*,*) my_rank, 'errror table: ', inum
+ 100  continue
+!  ----------------
 !
       if(iflag_RPRT_time) call start_elapsed_time(ist_elapsed_RPRT+7)
       call set_nod_and_ele_infos(new_mesh%node, new_mesh%ele)
