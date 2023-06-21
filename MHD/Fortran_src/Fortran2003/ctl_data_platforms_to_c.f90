5d8c1760b43c596247eb5727f6ae4403fb217f0c
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2023-06-21 10:49:30 -0700

    Update GKT menu test

---
+++ MHD/Fortran_src/Fortran2003/ctl_data_platforms_to_c.f90
@@ -0,0 +1,337 @@
+!>@file   ctl_data_platforms_to_c.f90
+!!        module ctl_data_platforms_to_c
+!!
+!!@author H. Matsui
+!!@date Programmed in June, 2023
+!!
+!>@brief  Send pointers for control items to C
+      module ctl_data_platforms_to_c
+!
+      use m_precision
+      use iso_c_binding
+      use t_ctl_data_4_platforms
+!
+      implicit none
+!
+!  ---------------------------------------------------------------------
+!
+      contains
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_chara_item_block_name(c_ctl)               &
+     &          bind(C, NAME = 'c_chara_item_block_name')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(read_character_item), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_chara_item_block_name = C_loc(f_ctl%item_name)
+      end function c_chara_item_block_name
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_chara_item_iflag(c_ctl)                    &
+     &          bind(C, NAME = 'c_chara_item_iflag')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(read_character_item), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_chara_item_iflag = C_loc(f_ctl%iflag)
+      end function c_chara_item_iflag
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_chara_item_charavalue(c_ctl)               &
+     &          bind(C, NAME = 'c_chara_item_charavalue')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(read_character_item), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_chara_item_charavalue = C_loc(f_ctl%charavalue)
+      end function c_chara_item_charavalue
+!
+!  ---------------------------------------------------------------------
+!
+!  ---------------------------------------------------------------------
+!
+!  ---------------------------------------------------------------------
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_block_name(c_ctl)                      &
+     &          bind(C, NAME = 'c_plt_block_name')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_block_name = C_loc(f_ctl%block_name)
+      end function c_plt_block_name
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_iflag(c_ctl)                           &
+     &          bind(C, NAME = 'c_plt_iflag')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_iflag = C_loc(f_ctl%ndomain_ctl)
+      end function c_plt_iflag
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_ndomain_ctl(c_ctl)                     &
+     &          bind(C, NAME = 'c_plt_ndomain_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_ndomain_ctl = C_loc(f_ctl%num_smp_ctl)
+      end function c_plt_ndomain_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_num_smp_ctl(c_ctl)                     &
+     &          bind(C, NAME = 'c_plt_num_smp_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_num_smp_ctl = C_loc(f_ctl%debug_flag_ctl)
+      end function c_plt_num_smp_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_debug_flag_ctl(c_ctl)                  &
+     &          bind(C, NAME = 'c_plt_debug_flag_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_debug_flag_ctl = C_loc(f_ctl%sph_file_prefix)
+      end function c_plt_debug_flag_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_sph_file_prefix(c_ctl)                 &
+     &          bind(C, NAME = 'c_plt_sph_file_prefix')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_sph_file_prefix = C_loc(f_ctl%mesh_file_prefix)
+      end function c_plt_sph_file_prefix
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_mesh_file_prefix(c_ctl)                &
+     &          bind(C, NAME = 'c_plt_mesh_file_prefix')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_mesh_file_prefix = C_loc(f_ctl%restart_file_prefix)
+      end function c_plt_mesh_file_prefix
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_restart_file_prefix(c_ctl)             &
+     &          bind(C, NAME = 'c_plt_restart_file_prefix')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_restart_file_prefix = C_loc(f_ctl%restart_file_prefix)
+      end function c_plt_restart_file_prefix
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_field_file_prefix(c_ctl)               &
+     &          bind(C, NAME = 'c_plt_field_file_prefix')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_field_file_prefix = C_loc(f_ctl%field_file_prefix)
+      end function c_plt_field_file_prefix
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_spectr_field_file_prefix(c_ctl)        &
+     &          bind(C, NAME = 'c_plt_spectr_field_file_prefix')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_spectr_field_file_prefix                                    &
+     &      = C_loc(f_ctl%spectr_field_file_prefix)
+      end function c_plt_spectr_field_file_prefix
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_coriolis_int_file_name(c_ctl)          &
+     &          bind(C, NAME = 'c_plt_coriolis_int_file_name')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_coriolis_int_file_name= C_loc(f_ctl%coriolis_int_file_name)
+      end function c_plt_coriolis_int_file_name
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_bc_data_file_name_ctl(c_ctl)           &
+     &          bind(C, NAME = 'c_plt_bc_data_file_name_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_bc_data_file_name_ctl = C_loc(f_ctl%bc_data_file_name_ctl)
+      end function c_plt_bc_data_file_name_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_radial_data_file_name_ctl(c_ctl)       &
+     &          bind(C, NAME = 'c_plt_radial_data_file_name_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_radial_data_file_name_ctl                                   &
+     &      = C_loc(f_ctl%radial_data_file_name_ctl)
+      end function c_plt_radial_data_file_name_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_interpolate_sph_to_fem(c_ctl)          &
+     &          bind(C, NAME = 'c_plt_interpolate_sph_to_fem')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_interpolate_sph_to_fem= C_loc(f_ctl%interpolate_sph_to_fem)
+      end function c_plt_interpolate_sph_to_fem
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_interpolate_fem_to_sph(c_ctl)          &
+     &          bind(C, NAME = 'c_plt_interpolate_fem_to_sph')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_interpolate_fem_to_sph= C_loc(f_ctl%interpolate_fem_to_sph)
+      end function c_plt_interpolate_fem_to_sph
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_rayleigh_spectr_dir(c_ctl)             &
+     &          bind(C, NAME = 'c_plt_rayleigh_spectr_dir')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_rayleigh_spectr_dir = C_loc(f_ctl%rayleigh_spectr_dir)
+      end function c_plt_rayleigh_spectr_dir
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_rayleigh_field_dir(c_ctl)              &
+     &          bind(C, NAME = 'c_plt_rayleigh_field_dir')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_rayleigh_field_dir = C_loc(f_ctl%rayleigh_field_dir)
+      end function c_plt_rayleigh_field_dir
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_sph_file_fmt_ctl(c_ctl)                &
+     &          bind(C, NAME = 'c_plt_sph_file_fmt_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_sph_file_fmt_ctl = C_loc(f_ctl%sph_file_fmt_ctl)
+      end function c_plt_sph_file_fmt_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_mesh_file_fmt_ctl(c_ctl)               &
+     &          bind(C, NAME = 'c_plt_mesh_file_fmt_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_mesh_file_fmt_ctl = C_loc(f_ctl%mesh_file_fmt_ctl)
+      end function c_plt_mesh_file_fmt_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_restart_file_fmt_ctl(c_ctl)            &
+     &          bind(C, NAME = 'c_plt_restart_file_fmt_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_restart_file_fmt_ctl = C_loc(f_ctl%restart_file_fmt_ctl)
+      end function c_plt_restart_file_fmt_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_field_file_fmt_ctl(c_ctl)              &
+     &          bind(C, NAME = 'c_plt_field_file_fmt_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_field_file_fmt_ctl = C_loc(f_ctl%field_file_fmt_ctl)
+      end function c_plt_field_file_fmt_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_spectr_field_fmt_ctl(c_ctl)            &
+     &          bind(C, NAME = 'c_plt_spectr_field_fmt_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_spectr_field_fmt_ctl = C_loc(f_ctl%spectr_field_fmt_ctl)
+      end function c_plt_spectr_field_fmt_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_itp_file_fmt_ctl(c_ctl)                &
+     &          bind(C, NAME = 'c_plt_itp_file_fmt_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_itp_file_fmt_ctl = C_loc(f_ctl%itp_file_fmt_ctl)
+      end function c_plt_itp_file_fmt_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_coriolis_file_fmt_ctl(c_ctl)           &
+     &          bind(C, NAME = 'c_plt_coriolis_file_fmt_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_coriolis_file_fmt_ctl = C_loc(f_ctl%coriolis_file_fmt_ctl)
+      end function c_plt_coriolis_file_fmt_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      type(c_ptr) function c_plt_del_org_data_ctl(c_ctl)                &
+     &          bind(C, NAME = 'c_plt_del_org_data_ctl')
+      type(c_ptr), value, intent(in) :: c_ctl
+      type(platform_data_control), pointer :: f_ctl
+!
+      call c_f_pointer(c_ctl, f_ctl)
+      c_plt_del_org_data_ctl = C_loc(f_ctl%del_org_data_ctl)
+      end function c_plt_del_org_data_ctl
+!
+!  ---------------------------------------------------------------------
+!
+      end module ctl_data_platforms_to_c
