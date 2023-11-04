8bd5aa9a0c7736c2dbb141f2d4391e1b0fc90500
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2023-11-03 18:54:11 -0700

    Make new directory for Metal rendering

--- MHD/C_src/Metal/AAPLShaderTypes.h
+++ MHD/C_src/Metal/AAPLShaderTypes.h
@@ -19,6 +19,13 @@ typedef enum AAPLVertexInputIndex
     AAPLVertexInputIndexScale =        2,
 } AAPLVertexInputIndex;
 
+// Texture index values shared between shader and C code to ensure Metal shader buffer inputs match
+//   Metal API texture set calls
+typedef enum AAPLTextureIndex
+{
+    AAPLTextureIndexBaseColor = 0,
+} AAPLTextureIndex;
+
 //  This structure defines the layout of vertices sent to the vertex
 //  shader. This header is shared between the .metal shader and C code, to guarantee that
 //  the layout of the vertex array in the C code matches the layout that the .metal
@@ -28,5 +35,14 @@ typedef struct
     vector_float2 position;
     vector_float4 color;
 } AAPLVertex;
+
+typedef struct
+{
+    // Positions in pixel space. A value of 100 indicates 100 pixels from the origin/center.
+    vector_float2 position;
+
+    // 2D texture coordinate
+    vector_float2 textureCoordinate;
+} AAPLVertexWithTexture;
 
 #endif /* AAPLShaderTypes_h */
