15f6fd448185c50cba5559952f7be82196d7626a
Hiroaki Matsui <matsui@Kemo-mac-2009.local>
2024-02-06 13:55:59 -0800

    Fix dependency checks for symmetries

--- MHD/Fortran_src/CORE_src/Fields/check_field_w_symmetry.f90
+++ MHD/Fortran_src/CORE_src/Fields/check_field_w_symmetry.f90
@@ -40,63 +40,75 @@
       if(      check_field_list_ctl(sym_velocity, field_ctl)            &
      &    .or. check_field_list_ctl(asym_velocity, field_ctl)) then
         call add_phys_name_ctl(velocity, field_ctl)
-      else if( check_field_list_ctl(sym_vorticity, field_ctl)           &
+      end if
+      if( check_field_list_ctl(sym_vorticity, field_ctl)                &
      &    .or. check_field_list_ctl(asym_vorticity, field_ctl)) then
         call add_phys_name_ctl(vorticity, field_ctl)
-      else if( check_field_list_ctl(sym_magnetic_field, field_ctl)      &
+      end if
+      if( check_field_list_ctl(sym_magnetic_field, field_ctl)           &
      &    .or. check_field_list_ctl(asym_magnetic_field,                &
      &                              field_ctl)) then
         call add_phys_name_ctl(magnetic_field, field_ctl)
-      else if( check_field_list_ctl(sym_vector_potential, field_ctl)    &
+      end if
+      if( check_field_list_ctl(sym_vector_potential, field_ctl)         &
      &    .or. check_field_list_ctl(asym_vector_potential,              &
      &                              field_ctl)) then
         call add_phys_name_ctl(vector_potential, field_ctl)
-      else if( check_field_list_ctl(sym_current_density, field_ctl)     &
+      end if
+      if( check_field_list_ctl(sym_current_density, field_ctl)          &
      &    .or. check_field_list_ctl(asym_current_density,               &
      &                              field_ctl)) then
         call add_phys_name_ctl(current_density, field_ctl)
+      end if
 !
-      else if( check_field_list_ctl(sym_pressure, field_ctl)            &
+      if( check_field_list_ctl(sym_pressure, field_ctl)                 &
      &    .or. check_field_list_ctl(asym_pressure, field_ctl)) then
         call add_phys_name_ctl(pressure, field_ctl)
-      else if( check_field_list_ctl(sym_magnetic_potential, field_ctl)  &
-     &    .or. check_field_list_ctl(asym_magnetic_potential,            &
-     &                              field_ctl)) then
+      end if
+      if( check_field_list_ctl(sym_magnetic_potential, field_ctl)       &
+     &    .or. check_field_list_ctl(asym_magnetic_potential, field_ctl) &
+     &   ) then
         call add_phys_name_ctl(magnetic_potential, field_ctl)
-      else if( check_field_list_ctl(sym_scalar_potential, field_ctl)    &
-     &    .or. check_field_list_ctl(asym_scalar_potential,              &
-     &                              field_ctl)) then
+      end if
+      if( check_field_list_ctl(sym_scalar_potential, field_ctl)         &
+     &    .or. check_field_list_ctl(asym_scalar_potential, field_ctl)   &
+     &   ) then
         call add_phys_name_ctl(scalar_potential, field_ctl)
+      end if
 !
-      else if( check_field_list_ctl(sym_temperature, field_ctl)         &
+      if( check_field_list_ctl(sym_temperature, field_ctl)              &
      &    .or. check_field_list_ctl(asym_temperature, field_ctl)) then
         call add_phys_name_ctl(temperature, field_ctl)
-      else if( check_field_list_ctl(sym_composition, field_ctl)         &
+      end if
+      if( check_field_list_ctl(sym_composition, field_ctl)              &
      &    .or. check_field_list_ctl(asym_composition, field_ctl)) then
         call add_phys_name_ctl(composition, field_ctl)
-      else if( check_field_list_ctl(sym_density, field_ctl)             &
+      end if
+      if( check_field_list_ctl(sym_density, field_ctl)                  &
      &    .or. check_field_list_ctl(asym_density, field_ctl)) then
         call add_phys_name_ctl(density, field_ctl)
-      else if( check_field_list_ctl(sym_entropy, field_ctl)             &
+      end if
+      if( check_field_list_ctl(sym_entropy, field_ctl)                  &
      &    .or. check_field_list_ctl(asym_entropy, field_ctl)) then
         call add_phys_name_ctl(entropy, field_ctl)
+      end if
 !
-      else if( check_field_list_ctl(sym_perturbation_temp, field_ctl)   &
-     &    .or. check_field_list_ctl(asym_perturbation_temp,             &
-     &                              field_ctl)) then
+      if( check_field_list_ctl(sym_perturbation_temp, field_ctl)        &
+     &    .or. check_field_list_ctl(asym_perturbation_temp, field_ctl)  &
+     &   ) then
         call add_phys_name_ctl(perturbation_temp, field_ctl)
-      else if( check_field_list_ctl(sym_perturbation_composition,       &
-     &                              field_ctl)                          &
+      end if
+      if( check_field_list_ctl(sym_perturbation_composition, field_ctl) &
      &  .or. check_field_list_ctl(asym_perturbation_composition,        &
      &                              field_ctl)) then
         call add_phys_name_ctl(perturbation_composition, field_ctl)
-      else if( check_field_list_ctl(sym_perturbation_density,           &
-     &                              field_ctl)                          &
+      end if
+      if( check_field_list_ctl(sym_perturbation_density, field_ctl)     &
      &    .or. check_field_list_ctl(asym_perturbation_density,          &
      &                              field_ctl)) then
         call add_phys_name_ctl(perturbation_density, field_ctl)
-      else if( check_field_list_ctl(sym_perturbation_entropy,           &
-     &                              field_ctl)                          &
+      end if
+      if( check_field_list_ctl(sym_perturbation_entropy, field_ctl)     &
      &    .or. check_field_list_ctl(asym_perturbation_entropy,          &
      &                              field_ctl)) then
         call add_phys_name_ctl(perturbation_entropy, field_ctl)
