f07f7051b8c449f4a725cb137a566d12c7a5abed
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2024-02-15 12:44:20 -0800

    Reduce number of valuables in modify_Csim_by_SGS_buo_ele

--- MHD/Fortran_src/MHD_src/sgs_dynamic/modify_Csim_by_SGS_buo_ele.f90
+++ MHD/Fortran_src/MHD_src/sgs_dynamic/modify_Csim_by_SGS_buo_ele.f90
@@ -67,57 +67,55 @@
      &     .and. fl_prop%iflag_4_composit_buo) then
         if(SGS_param%SGS_momentum%itype_Csym_flux                       &
      &      .eq. id_CSIM_COMPONENT) then
-          call modify_cmpCsim_by_SGS_dbuo_ele                           &
-     &       (iak_sgs_term%i_SGS_comp_buo, iak_sgs_term%i_SGS_buoyancy, &
-     &        icomp_sgs_term%i_SGS_m_flux,                              &
+          call modify_cmpCsim_by_SGS_dbuo_ele(n_sym_tensor, ele%numele, &
      &        layer_egrp%num_grp, layer_egrp%num_item,                  &
      &        layer_egrp%istack_grp_smp, layer_egrp%item_grp,           &
-     &        ele%numele, sgs_coefs%num_field, sgs_coefs%ntot_comp,     &
-     &        wk_sgs%fld_coef, wk_sgs%comp_clip, sgs_coefs%ak)
+     &        wk_sgs%fld_coef(1,iak_sgs_term%i_SGS_buoyancy),           &
+     &        wk_sgs%fld_coef(1,iak_sgs_term%i_SGS_comp_buo),           &
+     &        wk_sgs%comp_clip(1,icomp_sgs_term%i_SGS_m_flux),          &
+     &        sgs_coefs%ak(1,icomp_sgs_term%i_SGS_m_flux))
         else
-          call modify_fldCsim_by_SGS_dbuo_ele                           &
-     &       (iak_sgs_term%i_SGS_comp_buo, iak_sgs_term%i_SGS_buoyancy, &
-     &        iak_sgs_term%i_SGS_m_flux, icomp_sgs_term%i_SGS_m_flux,   &
+          call modify_fldCsim_by_SGS_dbuo_ele(n_sym_tensor, ele%numele, &
      &        layer_egrp%num_grp, layer_egrp%num_item,                  &
      &        layer_egrp%istack_grp_smp, layer_egrp%item_grp,           &
-     &        ele%numele, sgs_coefs%num_field, sgs_coefs%ntot_comp,     &
-     &        wk_sgs%fld_coef, wk_sgs%fld_clip, sgs_coefs%ak)
+     &        wk_sgs%fld_coef(1,iak_sgs_term%i_SGS_buoyancy),           &
+     &        wk_sgs%fld_coef(1,iak_sgs_term%i_SGS_comp_buo),           &
+     &        wk_sgs%fld_clip(1,iak_sgs_term%i_SGS_m_flux),             &
+     &        sgs_coefs%ak(1,icomp_sgs_term%i_SGS_m_flux))
         end if
       else if(fl_prop%iflag_4_gravity) then
         if(SGS_param%SGS_momentum%itype_Csym_flux                       &
      &      .eq. id_CSIM_COMPONENT) then
-          call modify_cmpCsim_by_SGS_buo_ele                            &
-     &       (iak_sgs_term%i_SGS_buoyancy, icomp_sgs_term%i_SGS_m_flux, &
+          call modify_cmpCsim_by_SGS_buo_ele(n_sym_tensor, ele%numele,  &
      &        layer_egrp%num_grp, layer_egrp%num_item,                  &
      &        layer_egrp%istack_grp_smp, layer_egrp%item_grp,           &
-     &        ele%numele, sgs_coefs%num_field, sgs_coefs%ntot_comp,     &
-     &        wk_sgs%fld_coef, wk_sgs%comp_clip, sgs_coefs%ak)
+     &        wk_sgs%fld_coef(1,iak_sgs_term%i_SGS_buoyancy),           &
+     &        wk_sgs%comp_clip(1,icomp_sgs_term%i_SGS_m_flux),          &
+     &        sgs_coefs%ak(1,icomp_sgs_term%i_SGS_m_flux))
         else
-          call modify_fldCsim_by_SGS_buo_ele                            &
-     &       (iak_sgs_term%i_SGS_buoyancy,                              &
-     &        iak_sgs_term%i_SGS_m_flux, icomp_sgs_term%i_SGS_m_flux,   &
+          call modify_fldCsim_by_SGS_buo_ele(n_sym_tensor, ele%numele,  &
      &        layer_egrp%num_grp, layer_egrp%num_item,                  &
      &        layer_egrp%istack_grp_smp, layer_egrp%item_grp,           &
-     &        ele%numele, sgs_coefs%num_field, sgs_coefs%ntot_comp,     &
-     &        wk_sgs%fld_coef, wk_sgs%fld_clip, sgs_coefs%ak)
+     &        wk_sgs%fld_coef(1,iak_sgs_term%i_SGS_buoyancy),           &
+     &        wk_sgs%fld_clip(1,iak_sgs_term%i_SGS_m_flux),             &
+     &        sgs_coefs%ak(1,icomp_sgs_term%i_SGS_m_flux))
         end if
       else if(fl_prop%iflag_4_composit_buo) then
         if(SGS_param%SGS_momentum%itype_Csym_flux                       &
      &      .eq. id_CSIM_COMPONENT) then
-          call modify_cmpCsim_by_SGS_buo_ele                            &
-     &       (iak_sgs_term%i_SGS_comp_buo, icomp_sgs_term%i_SGS_m_flux, &
+          call modify_cmpCsim_by_SGS_buo_ele(n_sym_tensor, ele%numele,  &
      &        layer_egrp%num_grp, layer_egrp%num_item,                  &
      &        layer_egrp%istack_grp_smp, layer_egrp%item_grp,           &
-     &        ele%numele, sgs_coefs%num_field, sgs_coefs%ntot_comp,     &
-     &        wk_sgs%fld_coef, wk_sgs%comp_clip, sgs_coefs%ak)
+     &        wk_sgs%fld_coef(1,iak_sgs_term%i_SGS_comp_buo),           &
+     &        wk_sgs%comp_clip(1,icomp_sgs_term%i_SGS_m_flux),          &
+     &        sgs_coefs%ak(1,icomp_sgs_term%i_SGS_m_flux))
         else
-          call modify_fldCsim_by_SGS_buo_ele                            &
-     &       (iak_sgs_term%i_SGS_comp_buo,                              &
-     &        iak_sgs_term%i_SGS_m_flux, icomp_sgs_term%i_SGS_m_flux,   &
+          call modify_fldCsim_by_SGS_buo_ele(n_sym_tensor, ele%numele,  &
      &        layer_egrp%num_grp, layer_egrp%num_item,                  &
      &        layer_egrp%istack_grp_smp, layer_egrp%item_grp,           &
-     &        ele%numele, sgs_coefs%num_field, sgs_coefs%ntot_comp,     &
-     &        wk_sgs%fld_coef, wk_sgs%fld_clip, sgs_coefs%ak)
+     &        wk_sgs%fld_coef(1,iak_sgs_term%i_SGS_comp_buo),           &
+     &        wk_sgs%fld_clip(1,iak_sgs_term%i_SGS_m_flux),             &
+     &        sgs_coefs%ak(1,icomp_sgs_term%i_SGS_m_flux))
         end if
       end if
 !
@@ -126,39 +124,29 @@
 !  ---------------------------------------------------------------------
 !  ---------------------------------------------------------------------
 !
-      subroutine modify_fldCsim_by_SGS_buo_ele                          &
-     &         (iak_sgs_buo, iak_sgs_mf, icomp_sgs_mf,                  &
+      subroutine modify_fldCsim_by_SGS_buo_ele(numdir, numele,          &
      &          n_layer_d, n_item_layer_d, layer_stack_smp, item_layer, &
-     &          numele, num_kinds_ele, ntot_comp_ele,                   &
      &          sgs_f_coef, sgs_f_clip, ak_sgs)
 !
       use m_machine_parameter
 !
-      integer (kind = kint), intent(in) :: iak_sgs_buo
-      integer (kind = kint), intent(in) :: icomp_sgs_mf, iak_sgs_mf
       integer (kind = kint), intent(in) :: n_layer_d, n_item_layer_d
       integer (kind = kint), intent(in)                                 &
      &                      :: layer_stack_smp(0:n_layer_d*np_smp)
       integer (kind = kint), intent(in) :: item_layer(n_item_layer_d)
-      integer (kind = kint), intent(in) :: numele
-      integer (kind = kint), intent(in) :: num_kinds_ele, ntot_comp_ele
-      real(kind = kreal), intent(in)                                    &
-     &          :: sgs_f_coef(n_layer_d,num_kinds_ele)
-      real(kind = kreal), intent(in)                                    &
-     &          :: sgs_f_clip(n_layer_d,num_kinds_ele)
+      integer (kind = kint), intent(in) :: numele, numdir
+      real(kind = kreal), intent(in) :: sgs_f_coef(n_layer_d)
+      real(kind = kreal), intent(in) :: sgs_f_clip(n_layer_d)
 !
-      real(kind = kreal), intent(inout) :: ak_sgs(numele,ntot_comp_ele)
+      real(kind = kreal), intent(inout) :: ak_sgs(numele,numdir)
 !
-      integer (kind = kint) :: ip, is, ist, ied, nst, ned
+      integer (kind = kint) :: ip, is, ist, ied
       integer (kind = kint) :: igrp, iele0, iele, nd
 !
 !
-      nst = icomp_sgs_mf
-      ned = icomp_sgs_mf + n_sym_tensor - 1
-!
 !$omp parallel do private(is,ist,ied,igrp,iele0,iele)
         do ip = 1, np_smp
-          do nd = nst, ned
+          do nd = 1, numdir
             do igrp = 1, n_layer_d
               is = (igrp-1)*np_smp + ip
               ist = layer_stack_smp(is-1) + 1
@@ -167,8 +155,8 @@
 !cdir nodep
               do iele0 = ist, ied
                 iele = item_layer(iele0)
-                ak_sgs(iele,nd) =  sgs_f_clip(igrp,iak_sgs_mf)          &
-                                 * (one + sgs_f_coef(igrp,iak_sgs_buo))
+                ak_sgs(iele,nd) =  sgs_f_clip(igrp)                     &
+                                 * (one + sgs_f_coef(igrp))
               end do
             end do
           end do
@@ -179,11 +167,9 @@
 !
 !  ---------------------------------------------------------------------
 !
-      subroutine modify_fldCsim_by_SGS_dbuo_ele                         &
-     &         (iak_sgs_cbuo, iak_sgs_tbuo, iak_sgs_mf, icomp_sgs_mf,   &
+      subroutine modify_fldCsim_by_SGS_dbuo_ele(numdir, numele,         &
      &          n_layer_d, n_item_layer_d, layer_stack_smp, item_layer, &
-     &          numele, num_kinds_ele, ntot_comp_ele,                   &
-     &          sgs_f_coef, sgs_f_clip, ak_sgs)
+     &          sgs_f_tbuo, sgs_f_cbuo, sgs_f_clip, ak_sgs)
 !
       use m_machine_parameter
 !
@@ -191,27 +177,20 @@
       integer (kind = kint), intent(in)                                 &
      &                      :: layer_stack_smp(0:n_layer_d*np_smp)
       integer (kind = kint), intent(in) :: item_layer(n_item_layer_d)
-      integer (kind = kint), intent(in) :: numele
-      integer (kind = kint), intent(in) :: num_kinds_ele, ntot_comp_ele
-      integer (kind = kint), intent(in) :: iak_sgs_cbuo, iak_sgs_tbuo
-      integer (kind = kint), intent(in) :: iak_sgs_mf, icomp_sgs_mf
-      real(kind = kreal), intent(in)                                    &
-     &          :: sgs_f_coef(n_layer_d,num_kinds_ele)
-      real(kind = kreal), intent(in)                                    &
-     &          :: sgs_f_clip(n_layer_d,num_kinds_ele)
+      integer (kind = kint), intent(in) :: numele, numdir
+      real(kind = kreal), intent(in) :: sgs_f_tbuo(n_layer_d)
+      real(kind = kreal), intent(in) :: sgs_f_cbuo(n_layer_d)
+      real(kind = kreal), intent(in) :: sgs_f_clip(n_layer_d)
 !
-      real(kind = kreal), intent(inout) :: ak_sgs(numele,ntot_comp_ele)
+      real(kind = kreal), intent(inout) :: ak_sgs(numele,numdir)
 !
-      integer (kind = kint) :: ip, is, ist, ied, nst, ned
+      integer (kind = kint) :: ip, is, ist, ied
       integer (kind = kint) :: igrp, iele0, iele, nd
 !
 !
-      nst = icomp_sgs_mf
-      ned = icomp_sgs_mf + n_sym_tensor - 1
-!
 !$omp parallel do private(is,ist,ied,igrp,iele0,iele)
         do ip = 1, np_smp
-          do nd = nst, ned
+          do nd = 1, numdir
             do igrp = 1, n_layer_d
               is = (igrp-1)*np_smp + ip
               ist = layer_stack_smp(is-1) + 1
@@ -220,9 +199,9 @@
 !cdir nodep
               do iele0 = ist, ied
                 iele = item_layer(iele0)
-                ak_sgs(iele,nd) =  sgs_f_clip(igrp,iak_sgs_mf)          &
-                                 * (one + sgs_f_coef(igrp,iak_sgs_tbuo) &
-     &                            + sgs_f_coef(igrp,iak_sgs_cbuo) )
+                ak_sgs(iele,nd) =  sgs_f_clip(igrp)                     &
+                                 * (one + sgs_f_tbuo(igrp)              &
+     &                                  + sgs_f_cbuo(igrp) )
               end do
             end do
           end do
@@ -233,10 +212,8 @@
 !
 !  ---------------------------------------------------------------------
 !
-      subroutine modify_cmpCsim_by_SGS_buo_ele                          &
-     &         (iak_sgs_buo, icomp_sgs_mf,                              &
+      subroutine modify_cmpCsim_by_SGS_buo_ele(numdir, numele,          &
      &          n_layer_d, n_item_layer_d, layer_stack_smp, item_layer, &
-     &          numele, num_kinds_ele, ntot_comp_ele,                   &
      &          sgs_f_coef, sgs_c_clip, ak_sgs)
 !
       use m_machine_parameter
@@ -245,27 +222,19 @@
       integer (kind = kint), intent(in)                                 &
      &                      :: layer_stack_smp(0:n_layer_d*np_smp)
       integer (kind = kint), intent(in) :: item_layer(n_item_layer_d)
-      integer (kind = kint), intent(in) :: iak_sgs_buo
-      integer (kind = kint), intent(in) :: numele
-      integer (kind = kint), intent(in) :: num_kinds_ele, ntot_comp_ele
-      integer (kind = kint), intent(in) :: icomp_sgs_mf
-      real(kind = kreal), intent(in)                                    &
-     &          :: sgs_f_coef(n_layer_d,num_kinds_ele)
-      real(kind = kreal), intent(in)                                    &
-     &          :: sgs_c_clip(n_layer_d,ntot_comp_ele)
+      integer (kind = kint), intent(in) :: numele, numdir
+      real(kind = kreal), intent(in) :: sgs_f_coef(n_layer_d)
+      real(kind = kreal), intent(in) :: sgs_c_clip(n_layer_d,numdir)
 !
-      real(kind = kreal), intent(inout) :: ak_sgs(numele,ntot_comp_ele)
+      real(kind = kreal), intent(inout) :: ak_sgs(numele,numdir)
 !
-      integer (kind = kint) :: ip, is, ist, ied, nst, ned
+      integer (kind = kint) :: ip, is, ist, ied
       integer (kind = kint) :: igrp, iele0, iele, nd
 !
 !
-      nst = icomp_sgs_mf
-      ned = icomp_sgs_mf + n_sym_tensor - 1
-!
 !$omp parallel do private(is,ist,ied,igrp,iele0,iele)
         do ip = 1, np_smp
-          do nd = nst, ned
+          do nd = 1, numdir
             do igrp = 1, n_layer_d
               is = (igrp-1)*np_smp + ip
               ist = layer_stack_smp(is-1) + 1
@@ -275,7 +244,7 @@
               do iele0 = ist, ied
                 iele = item_layer(iele0)
                 ak_sgs(iele,nd) =  sgs_c_clip(igrp,nd)                  &
-                                 * (one + sgs_f_coef(igrp,iak_sgs_buo))
+                                 * (one + sgs_f_coef(igrp))
               end do
             end do
           end do
@@ -286,11 +255,9 @@
 !
 !  ---------------------------------------------------------------------
 !
-      subroutine modify_cmpCsim_by_SGS_dbuo_ele                         &
-     &         (iak_sgs_cbuo, iak_sgs_tbuo, icomp_sgs_mf,               &
+      subroutine modify_cmpCsim_by_SGS_dbuo_ele(numdir, numele,         &
      &          n_layer_d, n_item_layer_d, layer_stack_smp, item_layer, &
-     &          numele, num_kinds_ele, ntot_comp_ele,                   &
-     &          sgs_f_coef, sgs_c_clip, ak_sgs)
+     &          sgs_tbuo_coef, sgs_cbuo_coef, sgs_c_clip, ak_sgs)
 !
       use m_machine_parameter
 !
@@ -298,27 +265,20 @@
       integer (kind = kint), intent(in)                                 &
      &                      :: layer_stack_smp(0:n_layer_d*np_smp)
       integer (kind = kint), intent(in) :: item_layer(n_item_layer_d)
-      integer (kind = kint), intent(in) :: numele
-      integer (kind = kint), intent(in) :: num_kinds_ele, ntot_comp_ele
-      integer (kind = kint), intent(in) :: iak_sgs_cbuo, iak_sgs_tbuo
-      integer (kind = kint), intent(in) :: icomp_sgs_mf
-      real(kind = kreal), intent(in)                                    &
-     &          :: sgs_f_coef(n_layer_d,num_kinds_ele)
-      real(kind = kreal), intent(in)                                    &
-     &          :: sgs_c_clip(n_layer_d,ntot_comp_ele)
+      integer (kind = kint), intent(in) :: numele, numdir
+      real(kind = kreal), intent(in) :: sgs_tbuo_coef(n_layer_d)
+      real(kind = kreal), intent(in) :: sgs_cbuo_coef(n_layer_d)
+      real(kind = kreal), intent(in) :: sgs_c_clip(n_layer_d,numdir)
 !
-      real(kind = kreal), intent(inout) :: ak_sgs(numele,ntot_comp_ele)
+      real(kind = kreal), intent(inout) :: ak_sgs(numele,numdir)
 !
-      integer (kind = kint) :: ip, is, ist, ied, nst, ned
+      integer (kind = kint) :: ip, is, ist, ied
       integer (kind = kint) :: igrp, iele0, iele, nd
 !
 !
-      nst = icomp_sgs_mf
-      ned = icomp_sgs_mf + n_sym_tensor - 1
-!
 !$omp parallel do private(is,ist,ied,igrp,iele0,iele)
         do ip = 1, np_smp
-          do nd = nst, ned
+          do nd = 1, n_sym_tensor
             do igrp = 1, n_layer_d
               is = (igrp-1)*np_smp + ip
               ist = layer_stack_smp(is-1) + 1
@@ -328,8 +288,8 @@
               do iele0 = ist, ied
                 iele = item_layer(iele0)
                 ak_sgs(iele,nd) =  sgs_c_clip(igrp,nd)                  &
-                                 * (one + sgs_f_coef(igrp,iak_sgs_tbuo) &
-     &                            + sgs_f_coef(igrp,iak_sgs_cbuo) )
+                                 * (one + sgs_tbuo_coef(igrp)           &
+     &                                  + sgs_cbuo_coef(igrp) )
               end do
             end do
           end do
