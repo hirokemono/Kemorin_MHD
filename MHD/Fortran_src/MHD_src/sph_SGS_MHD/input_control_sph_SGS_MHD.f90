58c19054b437da8af37492de6cfb5b1370804cfc
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2024-02-19 07:08:41 -0800

    Add check routine in input_control_SPH_SGS_dynamo

--- MHD/Fortran_src/MHD_src/sph_SGS_MHD/input_control_sph_SGS_MHD.f90
+++ MHD/Fortran_src/MHD_src/sph_SGS_MHD/input_control_sph_SGS_MHD.f90
@@ -126,6 +126,7 @@
       type(SPH_mesh_field_data), intent(inout) :: SPH_MHD
       type(FEM_mesh_field_data), intent(inout) :: FEM_dat
 !
+      integer :: j
 !  Read control file
       if(iflag_debug.eq.1) write(*,*) 'load_control_sph_SGS_MHD'
       call load_control_sph_SGS_MHD(ctl_file_name, MHD_ctl,             &
@@ -148,6 +149,12 @@
      &    SPH_MHD%sph, SPH_MHD%fld, FEM_dat%field, SPH_WK%monitor,      &
      &    FEM_dat%nod_mntr)
       call dealloc_sph_sgs_mhd_ctl_data(MHD_ctl, add_SSMHD_ctl)
+!
+      write(*,*) 'list of numbers: load_para_SPH_and_FEM_w_LIC', my_rank
+      do j = 1, SSMHDs%SPH_model%MHD_prop%MHD_coef_list%dimless_list%num
+        write(*,*) j, trim(SSMHDs%SPH_model%MHD_prop%MHD_coef_list%dimless_list%name(j)), ': ', &
+     &                    SSMHDs%SPH_model%MHD_prop%MHD_coef_list%dimless_list%value(j)
+      end do
 !
 !  Load spherical shell table
       if(iflag_debug.eq.1) write(*,*) 'load_para_SPH_and_FEM_w_LIC'
