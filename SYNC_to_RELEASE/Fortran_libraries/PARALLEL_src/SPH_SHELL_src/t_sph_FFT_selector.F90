1b3cf9c581801647931f83cdf3498af9b6387eb6
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2024-04-07 23:44:21 -0500

    fix links for release version

--- SYNC_to_RELEASE/Fortran_libraries/PARALLEL_src/SPH_SHELL_src/t_sph_FFT_selector.F90
+++ SYNC_to_RELEASE/Fortran_libraries/PARALLEL_src/SPH_SHELL_src/t_sph_FFT_selector.F90
@@ -12,7 +12,8 @@
 !!        type(sph_rtp_grid), intent(in) :: sph_rtp
 !!        type(sph_comm_tbl), intent(in) :: comm_rtp
 !!        type(work_for_FFTs), intent(inout) :: WK_FFTs
-!!      subroutine finalize_sph_FFT_select(WK_FFTs)
+!!      subroutine finalize_sph_FFT_select(sph_rtp, WK_FFTs)
+!!        type(sph_rtp_grid), intent(in) :: sph_rtp
 !!        type(work_for_FFTs), intent(inout) :: WK_FFTs
 !!      subroutine verify_sph_FFT_select                                &
 !!     &         (sph_rtp, comm_rtp, ncomp_bwd, ncomp_fwd, WK_FFTs)
@@ -208,8 +209,9 @@
 !
 ! ------------------------------------------------------------------
 !
-      subroutine finalize_sph_FFT_select(WK_FFTs)
+      subroutine finalize_sph_FFT_select(sph_rtp, WK_FFTs)
 !
+      type(sph_rtp_grid), intent(in) :: sph_rtp
       type(work_for_FFTs), intent(inout) :: WK_FFTs
 !
 !
