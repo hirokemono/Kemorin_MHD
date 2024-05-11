50078aa2b44efebcfd2fea5f54503cc268ccda66
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2024-05-11 08:42:52 -0700

    Add opengl include file in glsl.h

--- MHD/C_src/GLSL/glsl.h
+++ MHD/C_src/GLSL/glsl.h
@@ -5,6 +5,12 @@
 #ifndef GLSL__
 #define GLSL__
 
+#ifdef __APPLE__
+#include<OpenGL/gl3.h>
+#else
+#include<GL/gl.h>
+#endif
+
 #include "kemoviewer.h"
 #include "m_gl_transfer_matrix.h"
 #include "m_phong_light_table_c.h"
