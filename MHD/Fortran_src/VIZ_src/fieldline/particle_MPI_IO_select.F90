8ae0b0e3527e92267946fa938a090ccc28581486
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2024-05-31 22:28:07 -0700

    Fix Makefile to recognize fortran source using preprocessor

--- MHD/Fortran_src/VIZ_src/fieldline/particle_MPI_IO_select.F90
+++ MHD/Fortran_src/VIZ_src/fieldline/particle_MPI_IO_select.F90
@@ -7,14 +7,16 @@
 !>@brief  Choose mesh file to read
 !!
 !!@verbatim
-!!      subroutine sel_mpi_read_particle_file(mesh_file,                &
+!!      subroutine sel_mpi_read_particle_file(mesh_file, istep_file,    &
 !!     &                                      t_IO, particle_IO)
+!!        integer(kind = kint), intent(in) :: istep_file
 !!        type(field_IO_params), intent(in) ::  mesh_file
 !!        type(time_data), intent(inout) :: t_IO
 !!        type(surf_edge_IO_file), intent(inout) :: particle_IO
 !!
-!!      subroutine sel_mpi_write_particle_file(mesh_file,               &
+!!      subroutine sel_mpi_write_particle_file(mesh_file, istep_file,   &
 !!     &                                       t_IO, particle_IO)
+!!        integer(kind = kint), intent(in) :: istep_file
 !!        type(field_IO_params), intent(in) ::  mesh_file
 !!        type(time_data), intent(in) :: t_IO
 !!        type(surf_edge_IO_file), intent(in) :: particle_IO
@@ -28,6 +30,7 @@
       use m_file_format_switch
       use t_file_IO_parameter
       use t_mesh_data
+      use t_time_data
 !
       use particle_file_IO_select
 !
@@ -43,28 +46,102 @@
 !
       implicit none
 !
-      character(len=kchara), private :: file_name
+      character(len=3), parameter, private :: pcl_ext = "pcl"
+      character(len=3), parameter, private :: pcb_ext = "pcb"
+!
+      private :: add_pcl_extension, add_pcb_extension
+      private :: set_tracer_file_name
 !
 !  ---------------------------------------------------------------------
 !
       contains
 !
+!-----------------------------------------------------------------------
+!
+      character(len=kchara) function add_pcl_extension(file_head)
+      use set_parallel_file_name
+!
+      character(len=kchara), intent(in) :: file_head
+!
+      add_pcl_extension = add_3chara_extension(file_head, pcl_ext)
+!
+      end function add_pcl_extension
+!
+!-----------------------------------------------------------------------
+!
+      character(len=kchara) function add_pcb_extension(file_head)
+      use set_parallel_file_name
+!
+      character(len=kchara), intent(in) :: file_head
+!
+      add_pcb_extension = add_3chara_extension(file_head, pcb_ext)
+!
+      end function add_pcb_extension
+!
+!-----------------------------------------------------------------------
+!-----------------------------------------------------------------------
+!
+      character(len=kchara) function set_tracer_file_name               &
+     &                    (file_prefix, itype_file, id_rank, istep_fld)
+!
+      use set_parallel_file_name
+      use set_sph_extensions
+      use m_file_format_switch
+!
+      integer, intent(in) :: id_rank
+      integer(kind=kint), intent(in) :: itype_file, istep_fld
+      character(len=kchara), intent(in) ::    file_prefix
+      character(len=kchara) :: fname_tmp, file_name
+!
+!
+      if(istep_fld .eq. iminus) then
+        fname_tmp = add_elaps_postfix(file_prefix)
+      else
+        fname_tmp = add_int_suffix(istep_fld, file_prefix)
+      end if
+!
+      if((itype_file/iflag_single) .eq. 0) then
+        file_name = add_process_id(id_rank, fname_tmp)
+      else
+        file_name = fname_tmp
+      end if
+!
+      if     (mod(itype_file,iten) .eq. id_gzip_bin_file_fmt) then
+        fname_tmp = add_pcb_extension(file_name)
+        file_name = add_gzip_extension(fname_tmp)
+      else if(mod(itype_file,iten) .eq. id_gzip_txt_file_fmt) then
+        fname_tmp = add_pcl_extension(file_name)
+        file_name = add_gzip_extension(fname_tmp)
+      else if(mod(itype_file,iten) .eq. id_binary_file_fmt) then
+        fname_tmp = add_pcb_extension(file_name)
+        file_name = fname_tmp
+      else
+        fname_tmp =  add_pcl_extension(file_name)
+        file_name = fname_tmp
+      end if
+      set_tracer_file_name = file_name
+!
+      end function set_tracer_file_name
+!
 !  ---------------------------------------------------------------------
 !
-      subroutine sel_mpi_read_particle_file(mesh_file,                  &
+      subroutine sel_mpi_read_particle_file(mesh_file, istep_file,      &
      &                                      t_IO, particle_IO)
 !
       use set_element_mesh_file_names
 !
+      integer(kind = kint), intent(in) :: istep_file
       type(field_IO_params), intent(in) ::  mesh_file
       type(time_data), intent(inout) :: t_IO
       type(surf_edge_IO_file), intent(inout) :: particle_IO
 !
+      character(len=kchara) :: file_name
       integer(kind = kint) :: ierr = 0
 !
 !
-      file_name = set_edge_mesh_file_name                               &
-     &        (mesh_file%file_prefix, mesh_file%iflag_format, my_rank)
+      file_name = set_tracer_file_name(mesh_file%file_prefix,           &
+     &                                 mesh_file%iflag_format,          &
+     &                                 my_rank, istep_file)
 !
       if(mesh_file%iflag_format                                         &
      &     .eq. iflag_single+id_binary_file_fmt) then
@@ -91,25 +168,28 @@
       end if 
 !
       if(ierr .gt. 0) then
-        call calypso_mpi_abort(ierr, 'Mesh data is wrong!!')
+        call calypso_mpi_abort(ierr, 'Tracer data is wrong!!')
       end if
 !
       end subroutine sel_mpi_read_particle_file
 !
 !------------------------------------------------------------------
 !
-      subroutine sel_mpi_write_particle_file(mesh_file,                 &
+      subroutine sel_mpi_write_particle_file(mesh_file, istep_file,     &
      &                                       t_IO, particle_IO)
 !
       use set_element_mesh_file_names
 !
+      integer(kind = kint), intent(in) :: istep_file
       type(field_IO_params), intent(in) ::  mesh_file
       type(time_data), intent(in) :: t_IO
       type(surf_edge_IO_file), intent(in) :: particle_IO
 !
+      character(len=kchara) :: file_name
 !
-      file_name = set_edge_mesh_file_name                               &
-     &        (mesh_file%file_prefix, mesh_file%iflag_format, my_rank)
+      file_name = set_tracer_file_name(mesh_file%file_prefix,           &
+     &                                 mesh_file%iflag_format,          &
+     &                                 my_rank, istep_file)
 !
       if(mesh_file%iflag_format                                         &
      &     .eq. iflag_single+id_binary_file_fmt) then
--- MHD/Fortran_src/VIZ_src/fieldline/tracer_restart_file_IO.f90
+++ MHD/Fortran_src/VIZ_src/fieldline/tracer_restart_file_IO.f90
@@ -68,20 +68,19 @@
       integer(kind = kint) :: istep_rst
 !
 !
+      istep_rst = set_IO_step(i_step, rst_step)
       call copy_time_step_size_data(time_d, time_IO)
       call set_particles_to_trace_list(viz_fields, fln_tce, fline_lc)
       call count_global_num_of_tracer(fline_lc)
       call copy_local_tracer_to_IO(fline_lc, particle_IO)
 
-      call sel_mpi_write_particle_file(tracer_file_prm,                 &
+      call sel_mpi_write_particle_file(tracer_file_prm, istep_rst,      &
      &                                 time_IO, particle_IO)
       call dealloc_neib_id(particle_IO%comm)
       call dealloc_surf_geometry_data(particle_IO)
       call dealloc_ele_connect(particle_IO%ele)
 !
       if(viz_fields%num_color_fields .le. 1) return
-!
-      istep_rst = set_IO_step(i_step, rst_step)
 !
       call field_on_local_tracer_to_IO(viz_fields, fline_lc, fld_IO)
 
@@ -117,7 +116,8 @@
       type(field_IO) :: fld_IO
 !
 !
-      call sel_mpi_read_particle_file(tracer_file_prm,                  &
+      istep_rst = set_IO_step(init_d%i_time_step, rst_step)
+      call sel_mpi_read_particle_file(tracer_file_prm, istep_rst,       &
      &                                time_IO, particle_IO)
       call copy_local_tracer_from_IO(particle_IO, fline_lc)
       call dealloc_neib_id(particle_IO%comm)
@@ -127,8 +127,6 @@
       call return_particles_to_trace_list(fline_lc, fln_tce)
 !
       if(viz_fields%num_color_fields .le. 1) return
-!
-     istep_rst = set_IO_step(init_d%i_time_step, rst_step)
 !
       call sel_read_alloc_step_SPH_file(nprocs, my_rank,                &
      &    istep_rst, tracer_file_prm, time_IO, fld_IO)
