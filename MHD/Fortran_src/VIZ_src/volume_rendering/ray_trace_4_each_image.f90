720c65c721b2116d0cce8a4bc2621def12f32014
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2024-06-09 19:47:31 -0700

    Add loop increment

--- MHD/Fortran_src/VIZ_src/volume_rendering/ray_trace_4_each_image.f90
+++ MHD/Fortran_src/VIZ_src/volume_rendering/ray_trace_4_each_image.f90
@@ -418,16 +418,17 @@
 !
       real(kind = kreal), intent(inout) :: rgba_ray(4)
 !
-      integer(kind = kint) :: i_fln, inum
+      integer(kind = kint) :: i_fln, inum, increment
       real(kind = kreal) :: grad_tgt(3), radius, distance
       real(kind = kreal) :: rgb_color(3), opacity
 !
       if(tracer_pvr_prm%num_pvr_tracer .le. 0) return
       do i_fln = 1, tracer_pvr_prm%num_pvr_tracer
+        increment =      tracer_pvr_prm%increment(i_fln)
         radius =         tracer_pvr_prm%rendering_radius(i_fln)
         opacity =        tracer_pvr_prm%tracer_opacity(i_fln)
         rgb_color(1:3) = tracer_pvr_prm%tracer_RGB(1:3,i_fln)
-        do inum = 1, particle_lc(i_fln)%nnod_line_l
+        do inum = 1, particle_lc(i_fln)%nnod_line_l, increment
           distance = distance_from_tracer(xx4_tgt, inum,                &
      &                                    particle_lc(i_fln))
           if(distance .ge. radius) cycle
@@ -472,15 +473,16 @@
 !
       real(kind = kreal), intent(inout) :: rgba_ray(4)
 !
-      integer(kind = kint) :: i_fln, iedge
+      integer(kind = kint) :: i_fln, iedge, increment
       real(kind = kreal) :: grad_tgt(3), radius, distance
       real(kind = kreal) :: rgb_color(3), opacity
 !
 !
       if(fline_pvr_prm%num_pvr_tracer .le. 0) return
-      do i_fln = 1, fline_pvr_prm%num_pvr_tracer
-        radius =  fline_pvr_prm%rendering_radius(i_fln)
-        opacity = fline_pvr_prm%tracer_opacity(i_fln)
+      do i_fln = 1, fline_pvr_prm%num_pvr_tracer, increment
+        increment = fline_pvr_prm%increment(i_fln)
+        radius =    fline_pvr_prm%rendering_radius(i_fln)
+        opacity =   fline_pvr_prm%tracer_opacity(i_fln)
         rgb_color(1:3) = fline_pvr_prm%tracer_RGB(1:3,i_fln)
         do iedge = 1, fline_lc(i_fln)%nele_line_l
           distance = distance_from_fline_segment(xx4_tgt, iedge,        &
