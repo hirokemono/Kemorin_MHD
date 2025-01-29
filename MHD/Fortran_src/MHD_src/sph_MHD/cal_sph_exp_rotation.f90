8b2c47881e4c8d3b1d5d053e59c4d29008600608
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2025-01-29 12:29:31 -0800

    Change FDM matrix for cal_sph_diff_pol_and_rot2

--- MHD/Fortran_src/MHD_src/sph_MHD/cal_sph_exp_rotation.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/cal_sph_exp_rotation.f90
@@ -8,7 +8,7 @@
 !!
 !!@verbatim
 !!      subroutine cal_sph_diff_pol_and_rot2(kr_in, kr_out, nidx_rj,    &
-!!     &          ar_1d_rj, g_sph_rj, d1nod_mat_fdm_2, d2nod_mat_fdm_2, &
+!!     &          ar_1d_rj, g_sph_rj, fdm2_d1_mat, fdm2_d2_mat,         &
 !!     &          is_fld, is_rot, nnod_rj, ntot_phys_rj, d_rj)
 !!        input:  d_rj(:,is_fld),   d_rj(:,is_fld+2)
 !!        output: d_rj(:,is_fld+1), d_rj(:,is_rot:is_rot+2)
@@ -75,7 +75,7 @@
 ! -----------------------------------------------------------------------
 !
       subroutine cal_sph_diff_pol_and_rot2(kr_in, kr_out, nidx_rj,      &
-     &          ar_1d_rj, g_sph_rj, d1nod_mat_fdm_2, d2nod_mat_fdm_2,   &
+     &          ar_1d_rj, g_sph_rj, fdm2_d1_mat, fdm2_d2_mat,           &
      &          is_fld, is_rot, nnod_rj, ntot_phys_rj, d_rj)
 !
       integer(kind = kint), intent(in) :: kr_in, kr_out
@@ -84,10 +84,8 @@
       integer(kind = kint), intent(in) ::  nnod_rj, ntot_phys_rj
       real(kind = kreal), intent(in) :: g_sph_rj(nidx_rj(2),13)
       real(kind = kreal), intent(in) :: ar_1d_rj(nidx_rj(1),3)
-      real(kind = kreal), intent(in)                                    &
-     &                   :: d1nod_mat_fdm_2(nidx_rj(1),-1:1)
-      real(kind = kreal), intent(in)                                    &
-     &                   :: d2nod_mat_fdm_2(nidx_rj(1),-1:1)
+      real(kind = kreal), intent(in) :: fdm2_d1_mat(-1:1,nidx_rj(1))
+      real(kind = kreal), intent(in) :: fdm2_d2_mat(-1:1,nidx_rj(1))
 !
       real(kind = kreal), intent(inout) :: d_rj(nnod_rj,ntot_phys_rj)
 !
@@ -115,15 +113,15 @@
         j = mod((inod-1),nidx_rj(2)) + 1
         k = 1 + (inod-j) / nidx_rj(2)
 !
-        d1s_dr1 =  d1nod_mat_fdm_2(k,-1) * d_rj(i_n1,is_fld  )          &
-     &           + d1nod_mat_fdm_2(k, 0) * d_rj(inod,is_fld  )          &
-     &           + d1nod_mat_fdm_2(k, 1) * d_rj(i_p1,is_fld  )
-        d2s_dr2 =  d2nod_mat_fdm_2(k,-1) * d_rj(i_n1,is_fld  )          &
-     &           + d2nod_mat_fdm_2(k, 0) * d_rj(inod,is_fld  )          &
-     &           + d2nod_mat_fdm_2(k, 1) * d_rj(i_p1,is_fld  )
-        d1t_dr1 =  d1nod_mat_fdm_2(k,-1) * d_rj(i_n1,is_fld+2)          &
-     &           + d1nod_mat_fdm_2(k, 0) * d_rj(inod,is_fld+2)          &
-     &           + d1nod_mat_fdm_2(k, 1) * d_rj(i_p1,is_fld+2)
+        d1s_dr1 =  fdm2_d1_mat(-1,k) * d_rj(i_n1,is_fld  )              &
+     &           + fdm2_d1_mat( 0,k) * d_rj(inod,is_fld  )              &
+     &           + fdm2_d1_mat( 1,k) * d_rj(i_p1,is_fld  )
+        d2s_dr2 =  fdm2_d2_mat(-1,k) * d_rj(i_n1,is_fld  )              &
+     &           + fdm2_d2_mat( 0,k) * d_rj(inod,is_fld  )              &
+     &           + fdm2_d2_mat( 1,k) * d_rj(i_p1,is_fld  )
+        d1t_dr1 =  fdm2_d1_mat(-1,k) * d_rj(i_n1,is_fld+2)              &
+     &           + fdm2_d1_mat( 0,k) * d_rj(inod,is_fld+2)              &
+     &           + fdm2_d1_mat( 1,k) * d_rj(i_p1,is_fld+2)
 !
         d_rj(inod,is_fld+1) =   d1s_dr1
         d_rj(inod,is_rot  ) =   d_rj(inod,is_fld+2)
--- MHD/Fortran_src/MHD_src/sph_MHD/sph_radial_grad_4_velocity.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/sph_radial_grad_4_velocity.f90
@@ -115,7 +115,7 @@
 !
       call cal_sph_diff_pol_and_rot2(sph_bc_U%kr_in, sph_bc_U%kr_out,   &
      &    sph_rj%nidx_rj, sph_rj%ar_1d_rj, g_sph_rj,                    &
-     &    r_2nd%fdm(1)%dmat, r_2nd%fdm(2)%dmat, is_velo, is_vort,       &
+     &    r_2nd%dmat(-1,1,1), r_2nd%dmat(-1,1,2), is_velo, is_vort,     &
      &    rj_fld%n_point, rj_fld%ntot_phys, rj_fld%d_fld)
 !
       end subroutine const_grad_vp_and_vorticity
