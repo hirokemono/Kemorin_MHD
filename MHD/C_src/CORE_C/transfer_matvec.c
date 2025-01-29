435ec4d8f13169f838a5d5781708a2a6ee521cf2
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2025-01-28 18:00:11 -0800

    Change matrix dimension

--- MHD/C_src/CORE_C/transfer_matvec.c
+++ MHD/C_src/CORE_C/transfer_matvec.c
@@ -4,7 +4,7 @@
 #include "transfer_matvec.h"
 
 
-void transform_frame_xyz(int num, double *xx_org, double coff_matrix[4][4], double *xx_new)
+void transform_frame_xyz(int num, double *xx_org, double coef_matrix[16], double *xx_new)
 {
 	int i;
 	double xx, yy, zz;
@@ -13,14 +13,14 @@ void transform_frame_xyz(int num, double *xx_org, double coff_matrix[4][4], doub
 		xx = xx_org[i*3  ];
 		yy = xx_org[i*3+1];
 		zz = xx_org[i*3+2];
-		xx_new[i*3  ] = xx*coff_matrix[0][0] + yy*coff_matrix[1][0]+zz*coff_matrix[2][0] + coff_matrix[3][0];
-		xx_new[i*3+1] = xx*coff_matrix[0][1] + yy*coff_matrix[1][1]+zz*coff_matrix[2][1] + coff_matrix[3][1];
-		xx_new[i*3+2] = xx*coff_matrix[0][2] + yy*coff_matrix[1][2]+zz*coff_matrix[2][2] + coff_matrix[3][2];
+		xx_new[i*3  ] = xx*coef_matrix[0] + yy*coef_matrix[4] + zz*coef_matrix[ 8] + coef_matrix[12];
+		xx_new[i*3+1] = xx*coef_matrix[1] + yy*coef_matrix[5] + zz*coef_matrix[ 9] + coef_matrix[13];
+		xx_new[i*3+2] = xx*coef_matrix[2] + yy*coef_matrix[6] + zz*coef_matrix[10] + coef_matrix[14];
 	}
 	return;
 }
 
-void transform_frame_xyzw(int num, double *xx_org, double coff_matrix[4][4], double *xx_new)
+void transform_frame_xyzw(int num, double *xx_org, double coef_matrix[16], double *xx_new)
 {
 	int i;
 	double xx, yy, zz, ww;
@@ -30,10 +30,10 @@ void transform_frame_xyzw(int num, double *xx_org, double coff_matrix[4][4], dou
 		yy = xx_org[i*4+1];
 		zz = xx_org[i*4+2];
 		ww = xx_org[i*4+3];
-		xx_new[i*4  ] = xx*coff_matrix[0][0] + yy*coff_matrix[1][0]+zz*coff_matrix[2][0] + coff_matrix[3][0];
-		xx_new[i*4+1] = xx*coff_matrix[0][1] + yy*coff_matrix[1][1]+zz*coff_matrix[2][1] + coff_matrix[3][1];
-		xx_new[i*4+2] = xx*coff_matrix[0][2] + yy*coff_matrix[1][2]+zz*coff_matrix[2][2] + coff_matrix[3][2];
-		xx_new[i*4+3] = xx*coff_matrix[0][3] + yy*coff_matrix[1][3]+zz*coff_matrix[2][3] + coff_matrix[3][3];
+		xx_new[i*4  ] = xx*coef_matrix[0] + yy*coef_matrix[4] + zz*coef_matrix[ 8] + coef_matrix[12];
+		xx_new[i*4+1] = xx*coef_matrix[1] + yy*coef_matrix[5] + zz*coef_matrix[ 9] + coef_matrix[13];
+		xx_new[i*4+2] = xx*coef_matrix[2] + yy*coef_matrix[6] + zz*coef_matrix[10] + coef_matrix[14];
+		xx_new[i*4+3] = xx*coef_matrix[3] + yy*coef_matrix[7] + zz*coef_matrix[11] + coef_matrix[15];
 	}
 	return;
 }
--- MHD/C_src/CORE_C/transfer_matvec.h
+++ MHD/C_src/CORE_C/transfer_matvec.h
@@ -6,7 +6,7 @@
 
 /* prototypes */
 
-void transform_frame_xyz(int num, double *xx_org, double coff_matrix[4][4], double *xx_new);
-void transform_frame_xyzw(int num, double *xx_org, double coff_matrix[4][4], double *xx_new);
+void transform_frame_xyz(int num, double *xx_org, double coef_matrix[16], double *xx_new);
+void transform_frame_xyzw(int num, double *xx_org, double coef_matrix[16], double *xx_new);
 
 #endif
