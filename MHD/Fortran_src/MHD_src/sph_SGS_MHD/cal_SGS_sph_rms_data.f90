2aee14378ea0960be6313665721792291a066a8c
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2025-08-15 23:43:02 +0900

    Add debug write around cal_mean_squre_w_SGS_in_shell

--- MHD/Fortran_src/MHD_src/sph_SGS_MHD/cal_SGS_sph_rms_data.f90
+++ MHD/Fortran_src/MHD_src/sph_SGS_MHD/cal_SGS_sph_rms_data.f90
@@ -106,10 +106,14 @@
       type(sph_mhd_monitor_data), intent(inout) :: monitor
 !
 !
+      write(*,*) 'cal_mean_squre_w_SGS_in_shell', &
+     &     monitor%pwr%v_spectr(1:2)%r_outside
       if(iflag_debug.gt.0)  write(*,*) 'cal_mean_squre_w_SGS_in_shell'
       call cal_mean_squre_w_SGS_in_shell                                &
      &   (sph%sph_params, sph%sph_rj, ipol, ipol_LES, rj_fld,           &
      &    trans_p%leg, monitor%pwr, monitor%WK_pwr)
+      write(*,*) 'cal_mean_squre_w_SGS_in_shell out', &
+     &     monitor%pwr%v_spectr(1:2)%r_outside
 !
       if(iflag_debug.gt.0)  write(*,*) 'cal_lorentz_spctr_in_shell'
       call cal_lorentz_spctr_in_shell                                   &
