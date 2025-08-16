55b2222efd0422e47d9bb97a44618d82cc282368
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2025-08-16 09:25:52 +0900

    Check boundary

--- MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/init_sph_spec_radial_param.f90
+++ MHD/Fortran_src/PARALLEL_src/SPH_SHELL_src/init_sph_spec_radial_param.f90
@@ -51,6 +51,10 @@
       integer(kind = kint) :: kr_st
 !
 !
+       write(*,*) my_rank, 'v_pwr%r_inside in',   v_pwr%r_inside
+       write(*,*) my_rank, 'v_pwr%kr_inside in',  v_pwr%kr_inside
+       write(*,*) my_rank, 'v_pwr%c_inter_in in', v_pwr%c_inter_in
+!
         if(v_pwr%r_inside .le. zero                                     &
      &                         .and. v_pwr%kr_inside(1) .eq. 0) then
           v_pwr%kr_inside(1:2) = sph_params%nlayer_ICB
@@ -71,6 +75,10 @@
      &        v_pwr%kr_inside(1), v_pwr%kr_inside(2), v_pwr%c_inter_in)
         end if
 !
+       write(*,*) my_rank, 'v_pwr%r_inside mid',   v_pwr%r_inside
+       write(*,*) my_rank, 'v_pwr%kr_inside mid',  v_pwr%kr_inside
+       write(*,*) my_rank, 'v_pwr%c_inter_in mid', v_pwr%c_inter_in
+!
         if(abs(v_pwr%c_inter_in) .lt. 1.0d-6) then
           kr_st = v_pwr%kr_inside(2)
           v_pwr%kr_inside(1) = kr_st
@@ -82,6 +90,10 @@
           v_pwr%r_inside =     sph_rj%radius_1d_rj_r(kr_st)
           v_pwr%c_inter_in =   one
         end if
+!
+       write(*,*) my_rank, 'v_pwr%r_inside out',   v_pwr%r_inside
+       write(*,*) my_rank, 'v_pwr%kr_inside out',  v_pwr%kr_inside
+       write(*,*) my_rank, 'v_pwr%c_inter_in out', v_pwr%c_inter_in
 !
         if(v_pwr%r_outside .le. zero) then
           v_pwr%kr_outside(1:2) = sph_params%nlayer_CMB
