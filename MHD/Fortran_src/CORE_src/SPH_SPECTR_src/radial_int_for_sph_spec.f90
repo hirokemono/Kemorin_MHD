f998528c7d87c15e4aa3800c7742a643f8700fba
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2025-08-16 12:29:13 +0900

    Add integration

--- MHD/Fortran_src/CORE_src/SPH_SPECTR_src/radial_int_for_sph_spec.f90
+++ MHD/Fortran_src/CORE_src/SPH_SPECTR_src/radial_int_for_sph_spec.f90
@@ -120,6 +120,11 @@
      &         + (one - c_inter_st) * f_org(1)
 !          f_int = half * (radius(1) - r_in) * (f_org(1) + d_in)
           f_int = zero
+!
+        do kr = kg_st(1)+1, kg_ed(1)-1
+          f_int = f_int + half * (radius(kr+1) - radius(kr))            &
+     &                         * (f_org(kr) + f_org(kr+1))
+        end do
           return
         else
           r_in =          c_inter_st * radius(kg_st(1))                 &
