2a7e651aed5498d8dddc50ab4ef2a4002692a446
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2025-09-26 00:05:22 +0900

    remove double buoyancy routine

--- MHD/Fortran_src/MHD_src/sph_MHD/div_r_const_buoyancies_sph.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/div_r_const_buoyancies_sph.f90
@@ -20,10 +20,6 @@
 !!        type(base_force_address), intent(in) :: ipol_div_frc
 !!        type(sph_boundary_type), intent(in) :: sph_bc_U
 !!        type(phys_data), intent(inout) :: rj_fld
-!!
-!!      subroutine cal_div_cst_buo_sph(kr_in, kr_out, coef,             &
-!!     &          is_fld, ids_fld, is_div, nidx_rj, a_r_1d_rj_r,        &
-!!     &          nnod_rj, ntot_phys_rj, d_rj)
 !!@endverbatim
 !!
 !!@param sph_bc_U  Structure for basic velocity
@@ -38,7 +34,6 @@
 !
       implicit  none
 !
-      private :: cal_div_double_cst_buo_sph
       private :: cal_div_cst_buo_sph
 !
 !-----------------------------------------------------------------------
@@ -70,50 +65,39 @@
       type(sph_boundary_type), intent(in) :: sph_bc_U
       type(phys_data), intent(inout) :: rj_fld
 !
-      integer(kind = kint) :: ipol_temp,  ipol_comp
-      integer(kind = kint) :: igrad_temp, igrad_comp
+      integer(kind = kint) :: ipol_scalar, igrad_scalar
 !
 !
-      if(ref_param_T%flag_ref_field) then
-        ipol_temp =  ipol_base%i_per_temp
-        igrad_temp = ipol_grd%i_grad_per_t
-      else
-        ipol_temp =  ipol_base%i_temp
-        igrad_temp = ipol_grd%i_grad_temp
-      end if
+      if (fl_prop%flag_thermal_buoyancy) then
+        if(ref_param_T%flag_ref_field) then
+          ipol_scalar =  ipol_base%i_per_temp
+          igrad_scalar = ipol_grd%i_grad_per_t
+        else
+          ipol_scalar =  ipol_base%i_temp
+          igrad_scalar = ipol_grd%i_grad_temp
+        end if
 !
-      if(ref_param_C%flag_ref_field) then
-        ipol_comp =  ipol_base%i_per_light
-        igrad_comp = ipol_grd%i_grad_per_c
-      else
-        ipol_comp =  ipol_base%i_light
-        igrad_comp = ipol_grd%i_grad_composit
-      end if
-!
-      if(fl_prop%flag_thermal_buoyancy                                  &
-     &   .and. fl_prop%flag_comp_buoyancy) then
-        if (iflag_debug.eq.1)                                           &
-     &      write(*,*)'cal_div_double_cst_buo_sph', ipol_temp
-        call cal_div_double_cst_buo_sph                                 &
-     &     (sph_bc_U%kr_in, sph_bc_U%kr_out, fl_prop%coef_buo,          &
-     &      ipol_temp, igrad_temp, fl_prop%coef_comp_buo,               &
-     &      ipol_comp, igrad_comp, ipol_div_frc%i_buoyancy,             &
-     &      sph_rj%nidx_rj, sph_rj%ar_1d_rj(1,1),                       &
-     &      rj_fld%n_point, rj_fld%ntot_phys, rj_fld%d_fld)
-!
-      else if (fl_prop%flag_thermal_buoyancy) then
         if (iflag_debug.eq.1) write(*,*) 'cal_div_cst_buo_sph'
         call cal_div_cst_buo_sph                                        &
      &     (sph_bc_U%kr_in, sph_bc_U%kr_out, fl_prop%coef_buo,          &
-     &      ipol_temp, igrad_temp, ipol_div_frc%i_buoyancy,             &
+     &      ipol_scalar, igrad_scalar, ipol_div_frc%i_buoyancy,         &
      &      sph_rj%nidx_rj, sph_rj%ar_1d_rj(1,1),                       &
      &      rj_fld%n_point, rj_fld%ntot_phys, rj_fld%d_fld)
+      end if
 !
-      else if (fl_prop%flag_comp_buoyancy) then
+      if (fl_prop%flag_comp_buoyancy) then
+        if(ref_param_C%flag_ref_field) then
+          ipol_scalar =  ipol_base%i_per_light
+          igrad_scalar = ipol_grd%i_grad_per_c
+        else
+          ipol_scalar =  ipol_base%i_light
+          igrad_scalar = ipol_grd%i_grad_composit
+        end if
+!
         if (iflag_debug.eq.1) write(*,*) 'cal_div_cst_buo_sph'
         call cal_div_cst_buo_sph                                        &
      &     (sph_bc_U%kr_in, sph_bc_U%kr_out, fl_prop%coef_comp_buo,     &
-     &      ipol_comp, igrad_comp, ipol_div_frc%i_comp_buo,             &
+     &      ipol_scalar, igrad_scalar, ipol_div_frc%i_comp_buo,         &
      &      sph_rj%nidx_rj, sph_rj%ar_1d_rj(1,1),                       &
      &      rj_fld%n_point, rj_fld%ntot_phys, rj_fld%d_fld)
       end if
@@ -121,44 +105,6 @@
       end subroutine cal_div_r_const_buo_sph_mhd
 !
 !-----------------------------------------------------------------------
-!-----------------------------------------------------------------------
-!
-      subroutine cal_div_double_cst_buo_sph(kr_in, kr_out,              &
-     &          coef_t_buo, is_t, ids_t, coef_c_buo,                    &
-     &          is_c, ids_c, is_div, nidx_rj, a_r_1d_rj_r,              &
-     &          nnod_rj, ntot_phys_rj, d_rj)
-!
-      integer(kind = kint), intent(in) :: kr_in, kr_out
-      integer(kind= kint), intent(in) :: is_t, ids_t
-      integer(kind= kint), intent(in) :: is_c, ids_c
-      integer(kind= kint), intent(in) :: is_div
-      integer(kind = kint), intent(in) :: nidx_rj(2)
-      integer(kind = kint), intent(in) :: nnod_rj, ntot_phys_rj
-      real(kind = kreal), intent(in) :: a_r_1d_rj_r(nidx_rj(1))
-      real(kind = kreal), intent(in) :: coef_t_buo, coef_c_buo
-!
-      real (kind=kreal), intent(inout) :: d_rj(nnod_rj,ntot_phys_rj)
-!
-      integer(kind= kint) :: ist, ied, inod, j, k
-!
-!
-        ist = (kr_in-1)*nidx_rj(2) + 1
-        ied = kr_out * nidx_rj(2)
-!$omp parallel do private (inod,j,k)
-        do inod = ist, ied
-          j = mod((inod-1),nidx_rj(2)) + 1
-          k = 1 + (inod- j) / nidx_rj(2)
-!
-          d_rj(inod,is_div) = two * a_r_1d_rj_r(k)                      &
-     &                       * (coef_t_buo * d_rj(inod,is_t)            &
-     &                        + coef_c_buo * d_rj(inod,is_c))           &
-     &                     +  ( coef_t_buo * d_rj(inod,ids_t)           &
-     &                        + coef_c_buo * d_rj(inod,ids_c))
-        end do
-!$omp end parallel do
-!
-      end subroutine cal_div_double_cst_buo_sph
-!
 !-----------------------------------------------------------------------
 !
       subroutine cal_div_cst_buo_sph(kr_in, kr_out, coef,               &
