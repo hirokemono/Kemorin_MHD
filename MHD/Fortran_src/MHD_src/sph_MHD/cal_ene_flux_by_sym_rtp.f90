ede73d0037067e90d603c939a5eed54c4739005a
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2025-10-07 11:04:40 +0900

    Add buoyancy flux evaluation for radial constant gravity

--- MHD/Fortran_src/MHD_src/sph_MHD/cal_ene_flux_by_sym_rtp.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/cal_ene_flux_by_sym_rtp.f90
@@ -52,7 +52,7 @@
      &          bs_trns, fs_trns, be_trns, fe_trns,                     &
      &          trns_b_snap, trns_f_snap, trns_b_eflux, trns_f_eflux)
 !
-      use cal_buoyancy_flux_sph
+      use cal_buoyancy_flux_rtp
 !
       type(sph_rtp_grid), intent(in) :: sph_rtp
       type(fluid_property), intent(in) :: fl_prop
@@ -93,27 +93,16 @@
      &    trns_f_eflux%ncomp, trns_f_eflux%fld_rtp)
 !
 !
-      call cal_buoyancy_flux_rtp                                        &
+      call s_cal_buoyancy_flux_rtp                                      &
      &   (sph_rtp, fl_prop, ref_param_T, ref_param_C,                   &
      &    bs_trns%sym_fld, bs_trns%sym_fld,                             &
      &    fe_trns%eflux_to_sym_by_sym_asym,                             &
      &    trns_b_snap, trns_b_snap, trns_f_eflux)
-      call pole_buoyancy_flux_rtp                                       &
-     &   (sph_rtp, fl_prop, ref_param_T, ref_param_C,                   &
-     &    bs_trns%sym_fld, bs_trns%sym_fld,                             &
-     &    fe_trns%eflux_to_sym_by_sym_asym,                             &
-     &    trns_b_snap, trns_b_snap, trns_f_eflux)
-!
-      call cal_buoyancy_flux_rtp                                        &
+      call s_cal_buoyancy_flux_rtp                                      &
      &   (sph_rtp, fl_prop, ref_param_T, ref_param_C,                   &
      &   bs_trns%asym_fld, bs_trns%asym_fld,                            &
      &   fe_trns%eflux_to_asym_by_sym_sym,                              &
      &   trns_b_snap, trns_b_snap, trns_f_eflux)
-      call pole_buoyancy_flux_rtp                                       &
-     &   (sph_rtp, fl_prop, ref_param_T, ref_param_C,                   &
-     &    bs_trns%asym_fld, bs_trns%asym_fld,                           &
-     &    fe_trns%eflux_to_asym_by_sym_sym,                             &
-     &    trns_b_snap, trns_b_snap, trns_f_eflux)
 !
 !
      call cal_work_of_inertia_on_node                                   &
