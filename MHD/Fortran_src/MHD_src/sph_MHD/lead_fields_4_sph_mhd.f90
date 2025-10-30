3a4e5564a1f80c3143eb4cdb20cf5bf6c2d6f5f0
Takumi Kera <takumi.kera.r1@dc.tohoku.ac.jp>
2025-10-30 22:57:19 +0900

    Move the call for decomposition routine of coriolis force after forward trans and before backward trans

--- MHD/Fortran_src/MHD_src/sph_MHD/lead_fields_4_sph_mhd.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/lead_fields_4_sph_mhd.f90
@@ -153,10 +153,6 @@
      &    SPH_MHD%ipol%base, SPH_MHD%ipol%sym_fld,                      &
      &    SPH_MHD%ipol%asym_fld, SPH_MHD%fld)
 ! 
-      call s_decomp_w_sym_rj_Coriolis_force(SPH_MHD%sph%sph_rj,             &
-     &    SPH_MHD%ipol%forces, SPH_MHD%ipol%forces_by_sym_asym,                      &
-     &    SPH_MHD%ipol%forces_by_sym_sym, SPH_MHD%fld)
-!
       call sel_field_address_for_buoyancies(SPH_MHD%ipol%sym_fld,       &
      &    MHD_prop%ref_param_T, MHD_prop%ref_param_C,                   &
      &    ibuo_temp, ibuo_comp)
@@ -178,7 +174,12 @@
       call lead_fields_by_sph_trans(SPH_MHD%sph, SPH_MHD%comms,         &
      &    MHD_prop, trans_p, WK%trns_MHD, WK%trns_snap,                 &
      &    WK%WK_leg, WK%WK_FFTs, SPH_MHD%fld, SR_sig, SR_r)
-      call gradients_of_vectors_sph                                     &
+!
+      call s_decomp_w_sym_rj_Coriolis_force(SPH_MHD%sph%sph_rj,         &
+     &    SPH_MHD%ipol%forces, SPH_MHD%ipol%forces_by_sym_asym,         &
+     &    SPH_MHD%ipol%forces_by_sym_sym, SPH_MHD%fld)
+!
+     call gradients_of_vectors_sph                                     &
      &   (SPH_MHD%sph, SPH_MHD%comms, r_2nd, sph_MHD_bc, trans_p,       &
      &    SPH_MHD%ipol, WK%trns_snap, WK%trns_difv,                     &
      &    WK%WK_leg, WK%WK_FFTs, SPH_MHD%fld, SR_sig, SR_r)
