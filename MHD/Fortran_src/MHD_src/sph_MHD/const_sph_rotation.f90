663f80031f09738e52fde0337e5c317304a15460
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2026-01-08 13:13:00 +0900

    Change valuables for subroutines in sph_exp_no_condition_ICB/CMB

--- MHD/Fortran_src/MHD_src/sph_MHD/const_sph_rotation.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/const_sph_rotation.f90
@@ -220,12 +220,12 @@
 !
       call cal_sph_nod_nobc_in_rot2(sph_rj%nidx_rj(2), g_sph_rj,        &
      &    sph_bc%kr_in, sph_bc%r_ICB, sph_bc%fdm2_fix_fld_ICB,          &
-     &    is_fld, is_rot, rj_fld%n_point, rj_fld%ntot_phys,             &
-     &    rj_fld%d_fld)
+     &    rj_fld%n_point, rj_fld%d_fld(1,is_fld),                       &
+     &    rj_fld%d_fld(1,is_rot))
       call cal_sph_nod_nobc_out_rot2(sph_rj%nidx_rj(2), g_sph_rj,       &
      &    sph_bc%kr_out, sph_bc%r_CMB, sph_bc%fdm2_fix_fld_CMB,         &
-     &    is_fld, is_rot, rj_fld%n_point, rj_fld%ntot_phys,             &
-     &    rj_fld%d_fld)
+     &    rj_fld%n_point, rj_fld%d_fld(1,is_fld),                       &
+     &    rj_fld%d_fld(1,is_rot))
 !
       call cal_sph_nod_vect_rot2(sph_bc%kr_in, sph_bc%kr_out,           &
      &    sph_rj%nidx_rj, sph_rj%ar_1d_rj, g_sph_rj,                    &
