e32a3c63cfdfd69d483b19633c18ff7c54214ffc
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2026-01-08 16:54:00 +0900

    Change valuables for subroutines in sph_exp_rigid_ICB_s4t2

--- MHD/Fortran_src/MHD_src/radial_FDM/sph_exp_rigid_ICB_s4t2.f90
+++ MHD/Fortran_src/MHD_src/radial_FDM/sph_exp_rigid_ICB_s4t2.f90
@@ -11,16 +11,47 @@
 !!      subroutine cal_sph_icb_rigid_v_and_w_s4t2(nri, jmax,            &
 !!     &          g_sph_rj, kr_in, r_ICB, r_ICB1, d1nod_mat_fdm_2,      &
 !!     &          fdm2_fix_fld_ICB, fdm4_noslip_ICB, fdm4_noslip_ICB1,  &
-!!     &          Vt_ICB, is_fld, is_rot, n_point, ntot_phys_rj, d_rj)
+!!     &          Vt_ICB, n_point, d_rj_fld, d_rj_rot)
+!!        integer(kind = kint), intent(in) :: nri, jmax, kr_in
+!!        real(kind = kreal), intent(in) :: g_sph_rj(jmax,13)
+!!        real(kind = kreal), intent(in) :: r_ICB(0:2), r_ICB1(0:2)
+!!        real(kind = kreal), intent(in) :: d1nod_mat_fdm_2(nri,-1:1)
+!!        real(kind = kreal), intent(in) :: fdm2_fix_fld_ICB(0:2,3)
+!!        real(kind = kreal), intent(in) :: fdm4_noslip_ICB(0:2,2:4)
+!!        real(kind = kreal), intent(in) :: fdm4_noslip_ICB1(-1:2,5)
+!!        real(kind = kreal), intent(in) :: Vt_ICB(jmax)
+!!        integer(kind = kint), intent(in) :: n_point
+!!        real(kind = kreal), intent(inout) :: d_rj_fld(n_point,3)
+!!        real(kind = kreal), intent(inout) :: d_rj_rot(n_point,3)
 !!      subroutine cal_sph_icb_rigid_rot_s4t2(nri, jmax,                &
 !!     &          g_sph_rj, kr_in, r_ICB1, d1nod_mat_fdm_2,             &
 !!     &          fdm2_fix_fld_ICB, fdm4_noslip_ICB, fdm4_noslip_ICB1,  &
-!!     &          is_fld, is_rot, n_point, ntot_phys_rj, d_rj)
+!!     &          n_point, d_rj_fld, d_rj_rot)
+!!        integer(kind = kint), intent(in) :: nri, jmax, kr_in
+!!        real(kind = kreal), intent(in) :: g_sph_rj(jmax,13)
+!!        real(kind = kreal), intent(in) :: r_ICB1(0:2)
+!!        real(kind = kreal), intent(in) :: d1nod_mat_fdm_2(nri,-1:1)
+!!        real(kind = kreal), intent(in) :: fdm2_fix_fld_ICB(0:2,3)
+!!        real(kind = kreal), intent(in) :: fdm4_noslip_ICB(0:2,2:4)
+!!        real(kind = kreal), intent(in) :: fdm4_noslip_ICB1(-1:2,5)
+!!        integer(kind = kint), intent(in) :: n_point
+!!        real(kind = kreal), intent(in) :: d_rj_fld(n_point,3)
+!!        real(kind = kreal), intent(inout) :: d_rj_rot(n_point,3)
 !!      subroutine cal_sph_icb_rigid_diffuse_s4t2(nri, jmax,            &
 !!     &          g_sph_rj, kr_in, r_ICB, r_ICB1, d2nod_mat_fdm_2,      &
 !!     &          fdm2_fix_fld_ICB, fdm4_noslip_ICB, fdm4_noslip_ICB1,  &
-!!     &          coef_d, is_fld, is_diffuse,                           &
-!!     &          n_point, ntot_phys_rj, d_rj)
+!!     &          coef_d, n_point, d_rj_fld, d_rj_diffuse)
+!!        integer(kind = kint), intent(in) :: nri, jmax, kr_in
+!!        real(kind = kreal), intent(in) :: g_sph_rj(jmax,13)
+!!        real(kind = kreal), intent(in) :: r_ICB(0:2), r_ICB1(0:2)
+!!        real(kind = kreal), intent(in) :: coef_d
+!!        real(kind = kreal), intent(in) :: d2nod_mat_fdm_2(nri,-1:1)
+!!        real(kind = kreal), intent(in) :: fdm2_fix_fld_ICB(0:2,3)
+!!        real(kind = kreal), intent(in) :: fdm4_noslip_ICB(0:2,2:4)
+!!        real(kind = kreal), intent(in) :: fdm4_noslip_ICB1(-1:2,5)
+!!        integer(kind = kint), intent(in) :: n_point, ntot_phys_rj
+!!        real(kind = kreal), intent(in) :: d_rj_fld(n_point,3)
+!!        real(kind = kreal), intent(inout) :: d_rj_diffuse(n_point,3)
 !!@endverbatim
 !!
 !!@n @param n_point  Number of points for spectrum data
@@ -39,12 +70,6 @@
 !!
 !!@n @param Vt_ICB(jmax) Spectr data for toroidal velocity ICB
 !!@n @param coef_d     Coefficient for diffusion term
-!!@n @param is_fld     Address of poloidal velocity in d_rj
-!!@n @param is_rot     Address of poloidal vorticity in d_rj
-!!@n @param is_diffuse Address of poloidal viscousity in d_rj
-!!
-!!@n @param ntot_phys_rj   Total number of components
-!!@n @param d_rj           Spectrum data
 !!
 !
 !
@@ -65,10 +90,9 @@
       subroutine cal_sph_icb_rigid_v_and_w_s4t2(nri, jmax,              &
      &          g_sph_rj, kr_in, r_ICB, r_ICB1, d1nod_mat_fdm_2,        &
      &          fdm2_fix_fld_ICB, fdm4_noslip_ICB, fdm4_noslip_ICB1,    &
-     &          Vt_ICB, is_fld, is_rot, n_point, ntot_phys_rj, d_rj)
+     &          Vt_ICB, n_point, d_rj_fld, d_rj_rot)
 !
       integer(kind = kint), intent(in) :: nri, jmax, kr_in
-      integer(kind = kint), intent(in) :: is_fld, is_rot
       real(kind = kreal), intent(in) :: g_sph_rj(jmax,13)
       real(kind = kreal), intent(in) :: r_ICB(0:2), r_ICB1(0:2)
       real(kind = kreal), intent(in) :: d1nod_mat_fdm_2(nri,-1:1)
@@ -77,8 +101,9 @@
       real(kind = kreal), intent(in) :: fdm4_noslip_ICB1(-1:2,5)
       real(kind = kreal), intent(in) :: Vt_ICB(jmax)
 !
-      integer(kind = kint), intent(in) :: n_point, ntot_phys_rj
-      real (kind=kreal), intent(inout) :: d_rj(n_point,ntot_phys_rj)
+      integer(kind = kint), intent(in) :: n_point
+      real(kind = kreal), intent(inout) :: d_rj_fld(n_point,3)
+      real(kind = kreal), intent(inout) :: d_rj_rot(n_point,3)
 !
       integer(kind = kint) :: inod, j, i_p1, i_p2, i_p3
       real(kind = kreal) :: d1s_dr1, d2s_dr2, d1t_dr1
@@ -91,38 +116,38 @@
         i_p2 = i_p1 + jmax
         i_p3 = i_p2 + jmax
 !
-        d_rj(inod,is_fld  ) = zero
-        d_rj(inod,is_fld+1) = zero
-        d_rj(inod,is_fld+2) = Vt_ICB(j)
+        d_rj_fld(inod,1) = zero
+        d_rj_fld(inod,2) = zero
+        d_rj_fld(inod,3) = Vt_ICB(j)
 !
-        d2s_dr2 =  fdm4_noslip_ICB( 2,3) * d_rj(i_p2,is_fld  )          &
-     &           + fdm4_noslip_ICB( 1,3) * d_rj(i_p1,is_fld  )
-        d1t_dr1 =  fdm2_fix_fld_ICB(2,2) * d_rj(i_p2,is_fld+2)          &
-     &           + fdm2_fix_fld_ICB(1,2) * d_rj(i_p1,is_fld+2)          &
-     &           + fdm2_fix_fld_ICB(0,2) * d_rj(inod,is_fld+2)
+        d2s_dr2 =  fdm4_noslip_ICB( 2,3) * d_rj_fld(i_p2,1)             &
+     &           + fdm4_noslip_ICB( 1,3) * d_rj_fld(i_p1,1)
+        d1t_dr1 =  fdm2_fix_fld_ICB(2,2) * d_rj_fld(i_p2,3)             &
+     &           + fdm2_fix_fld_ICB(1,2) * d_rj_fld(i_p1,3)             &
+     &           + fdm2_fix_fld_ICB(0,2) * d_rj_fld(inod,3)
 !
-        d_rj(inod,is_rot  ) =  d_rj(inod,is_fld+2)
-        d_rj(inod,is_rot+1) =  d1t_dr1
-        d_rj(inod,is_rot+2) = -d2s_dr2
+        d_rj_rot(inod,1) =  d_rj_fld(inod,3)
+        d_rj_rot(inod,2) =  d1t_dr1
+        d_rj_rot(inod,3) = -d2s_dr2
 !
 !
-        d1s_dr1 =  fdm4_noslip_ICB1( 2,2) * d_rj(i_p3,is_fld  )         &
-     &           + fdm4_noslip_ICB1( 1,2) * d_rj(i_p2,is_fld  )         &
-     &           + fdm4_noslip_ICB1( 0,2) * d_rj(i_p1,is_fld  )         &
-     &           + fdm4_noslip_ICB1(-1,2) * d_rj(inod,is_fld  )
-        d2s_dr2 =  fdm4_noslip_ICB1( 2,3) * d_rj(i_p3,is_fld  )         &
-     &           + fdm4_noslip_ICB1( 1,3) * d_rj(i_p2,is_fld  )         &
-     &           + fdm4_noslip_ICB1( 0,3) * d_rj(i_p1,is_fld  )         &
-     &           + fdm4_noslip_ICB1(-1,3) * d_rj(inod,is_fld  )
-        d1t_dr1 =  d1nod_mat_fdm_2(kr_in+1,-1) * d_rj(inod,is_fld+2)    &
-     &           + d1nod_mat_fdm_2(kr_in+1, 0) * d_rj(i_p1,is_fld+2)    &
-     &           + d1nod_mat_fdm_2(kr_in+1, 1) * d_rj(i_p2,is_fld+2)
+        d1s_dr1 =  fdm4_noslip_ICB1( 2,2) * d_rj_fld(i_p3,1)            &
+     &           + fdm4_noslip_ICB1( 1,2) * d_rj_fld(i_p2,1)            &
+     &           + fdm4_noslip_ICB1( 0,2) * d_rj_fld(i_p1,1)            &
+     &           + fdm4_noslip_ICB1(-1,2) * d_rj_fld(inod,1)
+        d2s_dr2 =  fdm4_noslip_ICB1( 2,3) * d_rj_fld(i_p3,1)            &
+     &           + fdm4_noslip_ICB1( 1,3) * d_rj_fld(i_p2,1)            &
+     &           + fdm4_noslip_ICB1( 0,3) * d_rj_fld(i_p1,1)            &
+     &           + fdm4_noslip_ICB1(-1,3) * d_rj_fld(inod,1)
+        d1t_dr1 =  d1nod_mat_fdm_2(kr_in+1,-1) * d_rj_fld(inod,3)       &
+     &           + d1nod_mat_fdm_2(kr_in+1, 0) * d_rj_fld(i_p1,3)       &
+     &           + d1nod_mat_fdm_2(kr_in+1, 1) * d_rj_fld(i_p2,3)
 !
-        d_rj(i_p1,is_fld+1) =  d1s_dr1
-        d_rj(i_p1,is_rot  ) =  d_rj(i_p1,is_fld+2)
-        d_rj(i_p1,is_rot+1) =  d1t_dr1
-        d_rj(i_p1,is_rot+2) = - ( d2s_dr2 - g_sph_rj(j,3)               &
-     &                        * r_ICB1(2)*d_rj(i_p1,is_fld  ) )
+        d_rj_fld(i_p1,2) =  d1s_dr1
+        d_rj_rot(i_p1,1) =  d_rj_fld(i_p1,3)
+        d_rj_rot(i_p1,2) =  d1t_dr1
+        d_rj_rot(i_p1,3) = - (d2s_dr2 - g_sph_rj(j,3)                   &
+     &                      * r_ICB1(2) * d_rj_fld(i_p1,1))
       end do
 !$omp end parallel do
 !
@@ -133,10 +158,9 @@
       subroutine cal_sph_icb_rigid_rot_s4t2(nri, jmax,                  &
      &          g_sph_rj, kr_in, r_ICB1, d1nod_mat_fdm_2,               &
      &          fdm2_fix_fld_ICB, fdm4_noslip_ICB, fdm4_noslip_ICB1,    &
-     &          is_fld, is_rot, n_point, ntot_phys_rj, d_rj)
+     &          n_point, d_rj_fld, d_rj_rot)
 !
       integer(kind = kint), intent(in) :: nri, jmax, kr_in
-      integer(kind = kint), intent(in) :: is_fld, is_rot
       real(kind = kreal), intent(in) :: g_sph_rj(jmax,13)
       real(kind = kreal), intent(in) :: r_ICB1(0:2)
       real(kind = kreal), intent(in) :: d1nod_mat_fdm_2(nri,-1:1)
@@ -144,8 +168,9 @@
       real(kind = kreal), intent(in) :: fdm4_noslip_ICB(0:2,2:4)
       real(kind = kreal), intent(in) :: fdm4_noslip_ICB1(-1:2,5)
 !
-      integer(kind = kint), intent(in) :: n_point, ntot_phys_rj
-      real (kind=kreal), intent(inout) :: d_rj(n_point,ntot_phys_rj)
+      integer(kind = kint), intent(in) :: n_point
+      real(kind = kreal), intent(in) :: d_rj_fld(n_point,3)
+      real(kind = kreal), intent(inout) :: d_rj_rot(n_point,3)
 !
       integer(kind = kint) :: inod, j, i_p1, i_p2, i_p3
       real(kind = kreal) :: d2s_dr2, d1t_dr1
@@ -158,29 +183,29 @@
         i_p2 = i_p1 + jmax
         i_p3 = i_p2 + jmax
 !
-        d2s_dr2 =  fdm4_noslip_ICB( 2,3) * d_rj(i_p2,is_fld  )          &
-     &           + fdm4_noslip_ICB( 1,3) * d_rj(i_p1,is_fld  )
-        d1t_dr1 =  fdm2_fix_fld_ICB(2,2) * d_rj(i_p2,is_fld+2)          &
-     &           + fdm2_fix_fld_ICB(1,2) * d_rj(i_p1,is_fld+2)          &
-     &           + fdm2_fix_fld_ICB(0,2) * d_rj(inod,is_fld+2)
+        d2s_dr2 =  fdm4_noslip_ICB( 2,3) * d_rj_fld(i_p2,1)            &
+     &           + fdm4_noslip_ICB( 1,3) * d_rj_fld(i_p1,1)
+        d1t_dr1 =  fdm2_fix_fld_ICB(2,2) * d_rj_fld(i_p2,3)            &
+     &           + fdm2_fix_fld_ICB(1,2) * d_rj_fld(i_p1,3)            &
+     &           + fdm2_fix_fld_ICB(0,2) * d_rj_fld(inod,3)
 !
-        d_rj(inod,is_rot  ) =  d_rj(inod,is_fld+2)
-        d_rj(inod,is_rot+1) =  d1t_dr1
-        d_rj(inod,is_rot+2) = -d2s_dr2
+        d_rj_rot(inod,1) =  d_rj_fld(inod,3)
+        d_rj_rot(inod,2) =  d1t_dr1
+        d_rj_rot(inod,3) = -d2s_dr2
 !
 !
-        d2s_dr2 =  fdm4_noslip_ICB1( 2,3) * d_rj(i_p3,is_fld  )         &
-     &           + fdm4_noslip_ICB1( 1,3) * d_rj(i_p2,is_fld  )         &
-     &           + fdm4_noslip_ICB1( 0,3) * d_rj(i_p1,is_fld  )         &
-     &           + fdm4_noslip_ICB1(-1,3) * d_rj(inod,is_fld  )
-        d1t_dr1 =  d1nod_mat_fdm_2(kr_in+1,-1) * d_rj(inod,is_fld+2)    &
-     &           + d1nod_mat_fdm_2(kr_in+1, 0) * d_rj(i_p1,is_fld+2)    &
-     &           + d1nod_mat_fdm_2(kr_in+1, 1) * d_rj(i_p2,is_fld+2)
+        d2s_dr2 =  fdm4_noslip_ICB1( 2,3) * d_rj_fld(i_p3,1)           &
+     &           + fdm4_noslip_ICB1( 1,3) * d_rj_fld(i_p2,1)           &
+     &           + fdm4_noslip_ICB1( 0,3) * d_rj_fld(i_p1,1)           &
+     &           + fdm4_noslip_ICB1(-1,3) * d_rj_fld(inod,1)
+        d1t_dr1 =  d1nod_mat_fdm_2(kr_in+1,-1) * d_rj_fld(inod,3)      &
+     &           + d1nod_mat_fdm_2(kr_in+1, 0) * d_rj_fld(i_p1,3)      &
+     &           + d1nod_mat_fdm_2(kr_in+1, 1) * d_rj_fld(i_p2,3)
 !
-        d_rj(i_p1,is_rot  ) =  d_rj(i_p1,is_fld+2)
-        d_rj(i_p1,is_rot+1) =  d1t_dr1
-        d_rj(i_p1,is_rot+2) = - ( d2s_dr2 - g_sph_rj(j,3)               &
-     &                       * r_ICB1(2) * d_rj(i_p1,is_fld  ) )
+        d_rj_rot(i_p1,1) =  d_rj_fld(i_p1,3)
+        d_rj_rot(i_p1,2) =  d1t_dr1
+        d_rj_rot(i_p1,3) = - ( d2s_dr2 - g_sph_rj(j,3)                  &
+     &                                * r_ICB1(2) * d_rj_fld(i_p1,1))
       end do
 !$omp end parallel do
 !
@@ -191,11 +216,9 @@
       subroutine cal_sph_icb_rigid_diffuse_s4t2(nri, jmax,              &
      &          g_sph_rj, kr_in, r_ICB, r_ICB1, d2nod_mat_fdm_2,        &
      &          fdm2_fix_fld_ICB, fdm4_noslip_ICB, fdm4_noslip_ICB1,    &
-     &          coef_d, is_fld, is_diffuse,                             &
-     &          n_point, ntot_phys_rj, d_rj)
+     &          coef_d, n_point, d_rj_fld, d_rj_diffuse)
 !
       integer(kind = kint), intent(in) :: nri, jmax, kr_in
-      integer(kind = kint), intent(in) :: is_fld, is_diffuse
       real(kind = kreal), intent(in) :: g_sph_rj(jmax,13)
       real(kind = kreal), intent(in) :: r_ICB(0:2), r_ICB1(0:2)
       real(kind = kreal), intent(in) :: coef_d
@@ -204,8 +227,9 @@
       real(kind = kreal), intent(in) :: fdm4_noslip_ICB(0:2,2:4)
       real(kind = kreal), intent(in) :: fdm4_noslip_ICB1(-1:2,5)
 !
-      integer(kind = kint), intent(in) :: n_point, ntot_phys_rj
-      real (kind=kreal), intent(inout) :: d_rj(n_point,ntot_phys_rj)
+      integer(kind = kint), intent(in) :: n_point
+      real(kind = kreal), intent(in) :: d_rj_fld(n_point,3)
+      real(kind = kreal), intent(inout) :: d_rj_diffuse(n_point,3)
 !
       integer(kind = kint) :: inod, j, i_p1, i_p2, i_p3
       real(kind = kreal) :: d2s_dr2, d2t_dr2
@@ -218,29 +242,29 @@
         i_p2 = i_p1 + jmax
         i_p3 = i_p2 + jmax
 !
-        d2s_dr2 =  fdm4_noslip_ICB( 2,3) * d_rj(i_p2,is_fld  )          &
-     &           + fdm4_noslip_ICB( 1,3) * d_rj(i_p1,is_fld  )
-        d2t_dr2 =  fdm2_fix_fld_ICB(2,3) * d_rj(i_p2,is_fld+2)          &
-     &           + fdm2_fix_fld_ICB(1,3) * d_rj(i_p1,is_fld+2)          &
-     &           + fdm2_fix_fld_ICB(0,3) * d_rj(inod,is_fld+2)
+        d2s_dr2 =  fdm4_noslip_ICB( 2,3) * d_rj_fld(i_p2,1)             &
+     &           + fdm4_noslip_ICB( 1,3) * d_rj_fld(i_p1,1)
+        d2t_dr2 =  fdm2_fix_fld_ICB(2,3) * d_rj_fld(i_p2,3)             &
+     &           + fdm2_fix_fld_ICB(1,3) * d_rj_fld(i_p1,3)             &
+     &           + fdm2_fix_fld_ICB(0,3) * d_rj_fld(inod,3)
 !
-        d_rj(inod,is_diffuse  ) =  coef_d * (d2s_dr2                    &
-     &               - g_sph_rj(j,3)*r_ICB(2)*d_rj(inod,is_fld  ) )
-        d_rj(inod,is_diffuse+2) =  coef_d * (d2t_dr2                    &
-     &               - g_sph_rj(j,3)*r_ICB(2)*d_rj(inod,is_fld+2) )
+        d_rj_diffuse(inod,1) =  coef_d * (d2s_dr2                       &
+     &               - g_sph_rj(j,3) * r_ICB(2) * d_rj_fld(inod,1))
+        d_rj_diffuse(inod,3) =  coef_d * (d2t_dr2                       &
+     &               - g_sph_rj(j,3) * r_ICB(2) * d_rj_fld(inod,3))
 !
-        d2s_dr2 =  fdm4_noslip_ICB1( 2,3) * d_rj(i_p3,is_fld  )         &
-     &           + fdm4_noslip_ICB1( 1,3) * d_rj(i_p2,is_fld  )         &
-     &           + fdm4_noslip_ICB1( 0,3) * d_rj(i_p1,is_fld  )         &
-     &           + fdm4_noslip_ICB1(-1,3) * d_rj(inod,is_fld  )
-        d2t_dr2 =  d2nod_mat_fdm_2(kr_in+1,-1) * d_rj(inod,is_fld+2)    &
-     &           + d2nod_mat_fdm_2(kr_in+1, 0) * d_rj(i_p1,is_fld+2)    &
-     &           + d2nod_mat_fdm_2(kr_in+1, 1) * d_rj(i_p2,is_fld+2)
+        d2s_dr2 =  fdm4_noslip_ICB1( 2,3) * d_rj_fld(i_p3,1)            &
+     &           + fdm4_noslip_ICB1( 1,3) * d_rj_fld(i_p2,1)            &
+     &           + fdm4_noslip_ICB1( 0,3) * d_rj_fld(i_p1,1)            &
+     &           + fdm4_noslip_ICB1(-1,3) * d_rj_fld(inod,1)
+        d2t_dr2 =  d2nod_mat_fdm_2(kr_in+1,-1) * d_rj_fld(inod,3)       &
+     &           + d2nod_mat_fdm_2(kr_in+1, 0) * d_rj_fld(i_p1,3)       &
+     &           + d2nod_mat_fdm_2(kr_in+1, 1) * d_rj_fld(i_p2,3)
 !
-        d_rj(i_p1,is_diffuse  ) =  coef_d * (d2s_dr2                    &
-     &               - g_sph_rj(j,3)*r_ICB1(2)*d_rj(i_p1,is_fld  ) )
-        d_rj(i_p1,is_diffuse+2) =  coef_d * (d2t_dr2                    &
-     &               - g_sph_rj(j,3)*r_ICB1(2)*d_rj(i_p1,is_fld+2) )
+        d_rj_diffuse(i_p1,1) =  coef_d * (d2s_dr2                       &
+     &               - g_sph_rj(j,3) * r_ICB1(2) * d_rj_fld(i_p1,1))
+        d_rj_diffuse(i_p1,3) =  coef_d * (d2t_dr2                       &
+     &               - g_sph_rj(j,3) * r_ICB1(2) * d_rj_fld(i_p1,3))
       end do
 !$omp end parallel do
 !
