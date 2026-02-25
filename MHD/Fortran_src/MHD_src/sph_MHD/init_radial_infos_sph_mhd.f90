c04113f2166ee507da98cd08f2039f71751e0c51
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2026-02-25 17:09:40 +0900

    Fix spacing

--- MHD/Fortran_src/MHD_src/sph_MHD/init_radial_infos_sph_mhd.f90
+++ MHD/Fortran_src/MHD_src/sph_MHD/init_radial_infos_sph_mhd.f90
@@ -266,22 +266,22 @@
       end if
 !
       if(my_rank .eq. refs%irank_reference) then
-      if(MHD_prop%ref_param_T%iflag_reference                           &
+        if(MHD_prop%ref_param_T%iflag_reference                         &
+     &                    .eq. id_ref_field_file) then
+          call load_sph_reference_two_field                             &
+     &       (MHD_prop%ref_param_T%ref_file_IO, refs%iref_radius,       &
+     &        temperature%name, heat_source%name,                       &
+     &        refs%iref_base%i_temp, refs%iref_base%i_heat_source,      &
+     &        n_scalar, refs%r_itp, refs%ref_field)
+        end if
+        if(MHD_prop%ref_param_C%iflag_reference                         &
      &                  .eq. id_ref_field_file) then
-        call load_sph_reference_two_field                               &
-     &     (MHD_prop%ref_param_T%ref_file_IO, refs%iref_radius,         &
-     &      temperature%name, heat_source%name,                         &
-     &      refs%iref_base%i_temp, refs%iref_base%i_heat_source,        &
-     &      n_scalar, refs%r_itp, refs%ref_field)
-      end if
-      if(MHD_prop%ref_param_C%iflag_reference                           &
-     &                  .eq. id_ref_field_file) then
-        call load_sph_reference_two_field                               &
-     &     (MHD_prop%ref_param_C%ref_file_IO, refs%iref_radius,         &
-     &      composition%name, composition_source%name,                  &
-     &      refs%iref_base%i_light, refs%iref_base%i_light_source,      &
-     &      n_scalar, refs%r_itp, refs%ref_field)
-      end if
+          call load_sph_reference_two_field                             &
+     &       (MHD_prop%ref_param_C%ref_file_IO, refs%iref_radius,       &
+     &        composition%name, composition_source%name,                &
+     &        refs%iref_base%i_light, refs%iref_base%i_light_source,    &
+     &        n_scalar, refs%r_itp, refs%ref_field)
+        end if
       end if
 !
       call s_init_reference_scalar(refs%irank_reference,                &
